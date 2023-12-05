8178ad041ad79d7d142ec50e296810b73bced95d
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2023-12-05 22:59:08 +0100

    Remove unnecessary subscription from context.subscriptions
    NDISC-65

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -57,7 +57,7 @@ export function activate(context: vscode.ExtensionContext) {
     },
   );
 
-  context.subscriptions.push(disposable, openFilesInLayout);
+  context.subscriptions.push(openFilesInLayout);
 }
 
 export function deactivate() {}
