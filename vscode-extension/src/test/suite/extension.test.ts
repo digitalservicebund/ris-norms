bc9e44885318abd0ed55813504928fb76f235d18
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2023-12-07 13:51:46 +0100

    Adds test for the extension
    NDISC-65

--- vscode-extension/src/test/suite/extension.test.ts
+++ vscode-extension/src/test/suite/extension.test.ts
@@ -1,15 +1,93 @@
 import * as assert from "assert";
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from "vscode";
-// import * as myExtension from '../../extension';
+import * as sinon from "sinon";
 
 suite("Extension Test Suite", () => {
   vscode.window.showInformationMessage("Start all tests.");
 
-  test("Sample test", () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  let openTextDocumentStub: sinon.SinonStub;
+
+  setup(() => {
+    // Mock workspace folder
+    const mockedWorkspaceFolder = {
+      uri: vscode.Uri.file("/mocked/workspace/folder"),
+      name: "MockedWorkspace",
+      index: 0,
+    };
+
+    sinon
+      .stub(vscode.workspace, "workspaceFolders")
+      .value([mockedWorkspaceFolder]);
+
+    // Define mock file paths
+    const filePaths = [
+      `${mockedWorkspaceFolder.uri.fsPath}/07_01_änderungsgesetz.xml`,
+      `${mockedWorkspaceFolder.uri.fsPath}/07_01_geändertesGesetz_V1.1_Metadatenaenderung.xml`,
+      `${mockedWorkspaceFolder.uri.fsPath}/07_01_geändertesGesetz_V1.2_konsolidierte_Fassung.xml`,
+    ];
+
+    // Mock openTextDocument to simulate opening the files
+    openTextDocumentStub = sinon.stub(vscode.workspace, "openTextDocument");
+    filePaths.forEach((path) => {
+      openTextDocumentStub
+        .withArgs(vscode.Uri.file(path))
+        .resolves({ uri: vscode.Uri.file(path) });
+    });
+  });
+
+  teardown(() => {
+    sinon.restore();
+  });
+
+  test("Extension Activation", async () => {
+    // The unique identifier for your extension
+    const extensionId = "digitalservicebund.normendokumentation";
+    const extension = vscode.extensions.getExtension(extensionId);
+
+    assert.ok(extension, `Extension "${extensionId}" should be present`);
+
+    // Check that the extension is initially not active
+    assert.strictEqual(
+      extension.isActive,
+      false,
+      `Extension "${extensionId}" should initially be inactive`,
+    );
+
+    // Activate the extension and check if it's active
+    await extension.activate();
+    assert.strictEqual(
+      extension.isActive,
+      true,
+      `Extension "${extensionId}" should be active after activation`,
+    );
+  });
+
+  test("openFilesInLayout Command", async () => {
+    await vscode.commands.executeCommand(
+      "digitalservicebund.openFilesInLayout",
+    );
+
+    assert.ok(
+      openTextDocumentStub.calledWith(
+        vscode.Uri.file("/mocked/workspace/folder/07_01_änderungsgesetz.xml"),
+      ),
+      "07_01_änderungsgesetz.xml should be opened",
+    );
+    assert.ok(
+      openTextDocumentStub.calledWith(
+        vscode.Uri.file(
+          "/mocked/workspace/folder/07_01_geändertesGesetz_V1.1_Metadatenaenderung.xml",
+        ),
+      ),
+      "07_01_geändertesGesetz_V1.1_Metadatenaenderung.xml should be opened",
+    );
+    assert.ok(
+      openTextDocumentStub.calledWith(
+        vscode.Uri.file(
+          "/mocked/workspace/folder/07_01_geändertesGesetz_V1.2_konsolidierte_Fassung.xml",
+        ),
+      ),
+      "07_01_geändertesGesetz_V1.2_konsolidierte_Fassung.xml should be opened",
+    );
   });
 });
