9b7b323bb0730ff548676f0c49b4166050e30b25
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2023-12-08 08:40:45 +0100

    Adds tests for extension coverage
    NDISC-65

--- vscode-extension/src/test/suite/extension.test.ts
+++ vscode-extension/src/test/suite/extension.test.ts
@@ -1,14 +1,12 @@
 import * as assert from "assert";
 import * as vscode from "vscode";
 import * as sinon from "sinon";
+import { activate } from "../../extension";
 
-suite("Extension Test Suite", () => {
-  vscode.window.showInformationMessage("Start all tests.");
-
+suite("Extension Unit Test Suite", () => {
   let openTextDocumentStub: sinon.SinonStub;
 
   setup(() => {
-    // Mock workspace folder
     const mockedWorkspaceFolder = {
       uri: vscode.Uri.file("/mocked/workspace/folder"),
       name: "MockedWorkspace",
@@ -19,15 +17,14 @@ suite("Extension Test Suite", () => {
       .stub(vscode.workspace, "workspaceFolders")
       .value([mockedWorkspaceFolder]);
 
-    // Define mock file paths
     const filePaths = [
       `${mockedWorkspaceFolder.uri.fsPath}/07_01_änderungsgesetz.xml`,
       `${mockedWorkspaceFolder.uri.fsPath}/07_01_geändertesGesetz_V1.1_Metadatenaenderung.xml`,
       `${mockedWorkspaceFolder.uri.fsPath}/07_01_geändertesGesetz_V1.2_konsolidierte_Fassung.xml`,
     ];
 
-    // Mock openTextDocument to simulate opening the files
     openTextDocumentStub = sinon.stub(vscode.workspace, "openTextDocument");
+
     filePaths.forEach((path) => {
       openTextDocumentStub
         .withArgs(vscode.Uri.file(path))
@@ -39,30 +36,10 @@ suite("Extension Test Suite", () => {
     sinon.restore();
   });
 
-  test("Extension Activation", async () => {
-    // The unique identifier for your extension
-    const extensionId = "digitalservicebund.normendokumentation";
-    const extension = vscode.extensions.getExtension(extensionId);
+  test("openFilesInLayout command executes correctly", async () => {
+    const context = { subscriptions: [] } as unknown as vscode.ExtensionContext;
+    activate(context);
 
-    assert.ok(extension, `Extension "${extensionId}" should be present`);
-
-    // Check that the extension is initially not active
-    assert.strictEqual(
-      extension.isActive,
-      false,
-      `Extension "${extensionId}" should initially be inactive`,
-    );
-
-    // Activate the extension and check if it's active
-    await extension.activate();
-    assert.strictEqual(
-      extension.isActive,
-      true,
-      `Extension "${extensionId}" should be active after activation`,
-    );
-  });
-
-  test("openFilesInLayout Command", async () => {
     await vscode.commands.executeCommand(
       "digitalservicebund.openFilesInLayout",
     );
