e4adf825c078a5f621ca226db6884c32af868e81
Thore Strassburg <thore@weilbier.net>
2023-12-20 11:26:11 +0100

    Feat(system-tests): setup basic system tests with Playwright
    As the whole user experience is intended to be within the web-browser,
    using an end-to-end test framework for system tests seems like an
    obvious solution.
    
    The new folder `system-tests` is intentionally not within any of the
    system components as it acts independently on all of them together as
    system.
    
    Playwright is a sophisticated framework to test with web-browsers in
    user centric way. The team has experience with it and uses it for other
    systems of the NeuRIS project too.
    
    NDISC-80

---
+++ system-tests/playwright.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    trace: "on-first-retry",
+  },
+
+  /* Run your local dev server before starting the tests */
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
