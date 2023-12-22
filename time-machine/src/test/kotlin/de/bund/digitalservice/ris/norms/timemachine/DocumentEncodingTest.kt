1431cfcee5a20ef46138da44fc35f9c674bb6c20
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2023-12-22 20:38:26 +0100

    Wrap tests in class
    They're not picked up by './gradlew test' otherwise
    
    NDISC-0000

--- time-machine/src/test/kotlin/de/bund/digitalservice/ris/norms/timemachine/DocumentEncodingTest.kt
+++ time-machine/src/test/kotlin/de/bund/digitalservice/ris/norms/timemachine/DocumentEncodingTest.kt
@@ -4,45 +4,49 @@ import javax.xml.parsers.DocumentBuilderFactory
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.Test
 
-@Test
-fun `documentToString() should return an XML document as a string`() {
-  // Create a sample document with a root element
-  val document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument()
-  val root = document.createElement("root")
-  document.appendChild(root)
-  val result = documentToString(document)
+class DocumentEncodingTest {
 
-  assertThat(result).contains("<?xml")
-}
+  @Test
+  fun `documentToString() should return an XML document as a string`() {
+    // Create a sample document with a root element
+    val document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument()
+    val root = document.createElement("root")
+    document.appendChild(root)
+    val result = documentToString(document)
 
-@Test
-fun `writeDocumentToFile() should write an XML document to a file`() {
-  // Create a sample document with a root element
-  val document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument()
-  val root = document.createElement("root")
-  document.appendChild(root)
+    assertThat(result).contains("<?xml")
+  }
 
-  // Write the document to a file
-  val fileName = "test.xml"
-  writeDocumentToFile(document, fileName)
+  @Test
+  fun `writeDocumentToFile() should write an XML document to a file`() {
+    // Create a sample document with a root element
+    val document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument()
+    val root = document.createElement("root")
+    document.appendChild(root)
 
-  // Check that the file exists and has the expected content
-  val file = File(fileName)
-  assertThat(file.exists()).isTrue()
-  assertThat(file.readText())
-      .contains(
-          "<root/>",
-      )
-}
+    // Write the document to a file
+    val fileName = "test.xml"
+    writeDocumentToFile(document, fileName)
 
-@Test
-fun `fileToDocument() result contains modification node with specific eID`() {
-  //    val filePath = this.javaClass.classLoader.getResource("07_01_änderungsgesetz.xml")?.toURI()
-  val filePath = object {}.javaClass.classLoader.getResource("07_01_änderungsgesetz.xml")?.toURI()
-  val amendingLawFile = File(filePath!!)
-  val amendingLaw = readDocumentFromFile(amendingLawFile)
-  val modNode = getNode("//*[local-name()='mod']", amendingLaw)
+    // Check that the file exists and has the expected content
+    val file = File(fileName)
+    assertThat(file.exists()).isTrue()
+    assertThat(file.readText())
+        .contains(
+            "<root/>",
+        )
+  }
 
-  assertThat(modNode?.attributes?.getNamedItem("eId")?.nodeValue)
-      .isEqualTo("art-1_abs-1_untergl-1_listenelem-1_inhalt-1_text-1_ändbefehl-1")
+  @Test
+  fun `fileToDocument() result contains modification node with specific eID`() {
+    //    val filePath =
+    // this.javaClass.classLoader.getResource("07_01_änderungsgesetz.xml")?.toURI()
+    val filePath = object {}.javaClass.classLoader.getResource("07_01_änderungsgesetz.xml")?.toURI()
+    val amendingLawFile = File(filePath!!)
+    val amendingLaw = readDocumentFromFile(amendingLawFile)
+    val modNode = getNode("//*[local-name()='mod']", amendingLaw)
+
+    assertThat(modNode?.attributes?.getNamedItem("eId")?.nodeValue)
+        .isEqualTo("art-1_abs-1_untergl-1_listenelem-1_inhalt-1_text-1_ändbefehl-1")
+  }
 }
