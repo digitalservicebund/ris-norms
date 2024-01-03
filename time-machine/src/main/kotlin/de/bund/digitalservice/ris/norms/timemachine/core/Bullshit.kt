306a8b4aef628c9f06bc680ed7e6f369cb24599e
Thore Strassburg <thore@weilbier.net>
2024-01-03 10:56:54 +0100

    Chore: try fail code quality gate
    This introduces a bunch of duplicated code.
    
    NDISC-0000

---
+++ time-machine/src/main/kotlin/de/bund/digitalservice/ris/norms/timemachine/core/Bullshit.kt
@@ -0,0 +1,43 @@
+package de.bund.digitalservice.ris.norms.timemachine.core
+
+fun foo() {
+  for (index in 1..100) {
+    println("index: $index")
+  }
+}
+
+fun bar() {
+  for (index in 1..100) {
+    println("index: $index")
+  }
+}
+
+fun baz() {
+  for (index in 1..100) {
+    println("index: $index")
+  }
+}
+
+fun cab() {
+  for (index in 1..100) {
+    println("index: $index")
+  }
+}
+
+fun caz() {
+  for (index in 1..100) {
+    println("index: $index")
+  }
+}
+
+fun dar() {
+  for (index in 1..100) {
+    println("index: $index")
+  }
+}
+
+fun daz() {
+  for (index in 1..100) {
+    println("index: $index")
+  }
+}
