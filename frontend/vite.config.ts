77bd2fc0d1c6c837b0bd071ef07840b85155dfb3
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-01-16 14:41:56 +0100

    Consolidate tsconfig.json
    RISDEV-3140

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,15 @@
-// <reference types="vitest" />
 import { fileURLToPath, URL } from "node:url"
 import vue from "@vitejs/plugin-vue"
 import { defineConfig } from "vite"
-// https://vitejs.dev/config/
 
 export default defineConfig({
   plugins: [vue()],
   test: {
     global: true,
     coverage: {
-      provider: "v8", // or 'istanbul'
+      provider: "v8",
     },
+    environment: "jsdom",
   },
   resolve: {
     alias: {
