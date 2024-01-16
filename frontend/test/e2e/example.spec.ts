4b3e6f60e30832eec06dac8f9ce3b306d2ea153f
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-01-16 09:23:46 +0100

    Delete test templates
    RISDEV-3140

--- frontend/test/e2e/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test"
-
-test.describe("basic example test", () => {
-  test("basic test", async ({ page }) => {
-    await page.goto("/")
-    await expect(page.locator("text=DigitalService")).toBeVisible()
-  })
-})
