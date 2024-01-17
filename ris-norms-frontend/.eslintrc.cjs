c79abd6d3bf477f9d529b67b353b9de759b4a2e1
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-01-17 08:59:57 +0100

    Use eslint-plugin-testing-library
    RISDEV-3140

--- ris-norms-frontend/.eslintrc.cjs
+++ ris-norms-frontend/.eslintrc.cjs
@@ -115,5 +115,10 @@ module.exports = {
       extends: ["eslint:recommended", "plugin:import/recommended"],
       rules: { ...moduleImportRules },
     },
+    {
+      files: ["**/*.spec.ts"],
+      extends: ["plugin:testing-library/vue"],
+      rules: { ...moduleImportRules },
+    },
   ],
 }
