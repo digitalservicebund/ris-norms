c6706cc21a2a9ac6605d0468bbcf9218ba35141d
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-01-17 09:22:44 +0100

    Use c8 for coverage reporting
    RISDEV-3140

--- ris-norms-frontend/vite.config.ts
+++ ris-norms-frontend/vite.config.ts
@@ -8,26 +8,5 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    coverage: {
-      provider: "v8",
-      exclude: [
-        // Configuration and generated outputs
-        "**/[.]**",
-        "coverage/**/*",
-        "dist/**/*",
-        "**/.*rc.{?(c|m)js,yml}",
-        "*.config.{js,ts}",
-
-        // Types
-        "**/*.d.ts",
-
-        // Tests
-        "**/*.spec.*",
-
-        // App content we're not interested in covering with unit tests
-        "src/App.vue",
-        "src/main.ts",
-      ],
-    },
   },
 })
