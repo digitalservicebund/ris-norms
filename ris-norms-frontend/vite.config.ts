3d46a792b4b85ce5d36a9734d8d6ee0289bdb827
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-01-17 07:48:07 +0100

    Add v8 coverage config
    RISDEV-3140

--- ris-norms-frontend/vite.config.ts
+++ ris-norms-frontend/vite.config.ts
@@ -7,5 +7,26 @@ export default defineConfig({
   plugins: [vue()],
   test: {
     globals: true,
+    environment: "jsdom",
+    coverage: {
+      provider: "v8",
+      exclude: [
+        // Configuration and generated outputs
+        "**/[.]**",
+        "coverage/**/*",
+        "dist/**/*",
+        "**/.*rc.{?(c|m)js,yml}",
+        "*.config.{js,ts}",
+
+        // Types
+        "**/*.d.ts",
+
+        // Tests
+        "**/*.spec.*",
+
+        // App content we're not interested in covering with unit tests
+        "src/App.vue",
+      ],
+    },
   },
 })
