c14a2e2f83ab5d0e81ae917c3785f4da3b6d5575
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-01-18 07:13:03 +0100

    Add procedure component and display two
    RISDEV-3149

--- frontend/src/views/Procedures.vue
+++ frontend/src/views/Procedures.vue
@@ -1,7 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue"
+import Procedure from "@/components/Procedure.vue"
+
+const procedures = ref([
+  {
+    eli: "eli/bund/bgbl-1/1964/s593/regelungstext-1",
+    printAnnouncementGazette: "BGBl I",
+    printAnnouncementYear: 1964,
+    printAnnouncementNumber: 7,
+    printAnnouncementPage: 593,
+    publicationDate: new Date("2023-03-12"),
+  },
+  {
+    eli: "eli/bund/bgbl-1/1982/s22/regelungstext-1",
+    printAnnouncementGazette: "BGBl II",
+    printAnnouncementYear: 1982,
+    printAnnouncementNumber: 3,
+    printAnnouncementPage: 22,
+    publicationDate: new Date("2024-01-01"),
+  },
+])
+</script>
 
 <template>
-  <div class="flex flex-col p-40">
+  <div class="flex min-h-screen flex-col bg-gray-100 p-40">
     <h1 class="ds-heading-02-reg mb-40">Vorgänge</h1>
+    <div class="flex flex-col gap-8">
+      <Procedure
+        v-for="procedure in procedures"
+        :key="procedure.eli"
+        :eli="procedure.eli"
+        :print-announcement-gazette="procedure.printAnnouncementGazette"
+        :print-announcement-number="procedure.printAnnouncementNumber"
+        :print-announcement-page="procedure.printAnnouncementPage"
+        :print-announcement-year="procedure.printAnnouncementYear"
+        :publication-date="procedure.publicationDate"
+      />
+    </div>
   </div>
 </template>
