8225dfb850493c7201313a95ec13c36fb1076b3a
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2024-01-19 15:36:56 +0100

    Feat: set up Histoire, add example story
    RISDEV-3195

---
+++ frontend/src/components/ProcedureCard.story.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ProcedureCard from "./ProcedureCard.vue"
+</script>
+
+<template>
+  <Story>
+    <ProcedureCard
+      eli="4711"
+      print-announcement-gazette="Test"
+      :print-announcement-year="2024"
+      :print-announcement-number="815"
+      :print-announcement-page="12"
+      :publication-date="new Date(2024, 1, 19)"
+    />
+  </Story>
+</template>
