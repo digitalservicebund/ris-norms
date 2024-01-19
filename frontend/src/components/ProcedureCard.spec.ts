89956cc2de329875bdb574f5648ea28089796e9d
Hannes <hannes.michalek@digitalservice.bund.de>
2024-01-19 09:23:57 +0100

    Add procedure component and displays two (RISDEV-3149) (#27)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Andreas Philippi <andreas.philippi@digitalservice.bund.de>
    Co-authored-by: Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>

---
+++ frontend/src/components/ProcedureCard.spec.ts
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/vue"
+import { describe, expect, test } from "vitest"
+import ProcedureCard from "./ProcedureCard.vue"
+
+describe("YourComponent", () => {
+  test("should render overview of open procedures", () => {
+    render(ProcedureCard, {
+      props: {
+        eli: "someEli",
+        printAnnouncementGazette: "GazetteName",
+        printAnnouncementNumber: "123",
+        printAnnouncementYear: "2021",
+        printAnnouncementPage: "456",
+        // publicationDate: "2021-01-01",
+        publicationDate: new Date("2021-01-01"),
+      },
+    })
+    expect(screen.getByText("GazetteName 2021 Nr. 123")).toBeInTheDocument()
+    expect(screen.getByText(/Jan 1, 2021/)).toBeInTheDocument()
+  })
+})
