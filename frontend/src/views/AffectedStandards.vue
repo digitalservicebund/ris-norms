d037f8034537d8cc699cb0f2bae1b65490665180
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-01-25 08:34:55 +0100

    Add 2 subpages (views) for the Affected Standards & Article Overview
    RISDEV-3117

---
+++ frontend/src/views/AffectedStandards.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from "vue"
+const identifier = "123"
+const vscodeLink = ref(`vscode://file?fileId=${identifier}`)
+</script>
+
+<template>
+  <div>
+    <H1>Affected Standards</H1>
+    <a :href="vscodeLink">Meta data management</a>
+  </div>
+</template>
---
+++ frontend/src/views/ArticleOverview.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from "vue"
+const identifier = "123"
+const vscodeLink = ref(`vscode://file?fileId=${identifier}`)
+</script>
+
+<template>
+  <div>
+    <H1>Article overview</H1>
+    <a :href="vscodeLink">Text consolidation</a>
+  </div>
+</template>
