31b6a8152a416a7caafdab7d9d78d0ddb750a1cf
Sebastian Rossa <sebastian.rossa@digitalservice.bund.de>
2024-01-26 15:26:49 +0100

    Fix license report naming and add editorconfig
    RISDEV-0000

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,5 +1,8 @@
 import com.adarshr.gradle.testlogger.theme.ThemeType
+import com.github.jk1.license.filter.DependencyFilter
 import com.github.jk1.license.filter.LicenseBundleNormalizer
+import com.github.jk1.license.render.CsvReportRenderer
+import com.github.jk1.license.render.ReportRenderer
 
 buildscript { repositories { mavenCentral() } }
 
@@ -167,13 +170,7 @@ sonar {
 }
 
 licenseReport {
-    // If there's a new dependency with a yet unknown license causing this task to fail
-    // the license(s) will be listed in build/reports/dependency-license/dependencies-without-allowed-license.json
-    allowedLicensesFile = File("$projectDir/allowed-licenses.json")
-    filters =
-        arrayOf(
-            // With second arg true we get the default transformations:
-            // https://github.com/jk1/Gradle-License-Report/blob/7cf695c38126b63ef9e907345adab84dfa92ea0e/src/main/resources/default-license-normalizer-bundle.json
-            LicenseBundleNormalizer(null, true),
-        )
+    allowedLicensesFile = File("$projectDir/../allowed-licenses.json")
+    renderers = arrayOf<ReportRenderer>(CsvReportRenderer("time-machine-licence-report.csv"))
+    filters = arrayOf<DependencyFilter>(LicenseBundleNormalizer(null, true))
 }
