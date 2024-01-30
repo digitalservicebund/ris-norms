9a779cc1396b7ae6fceda4fd7782d70a3606a468
Victor del Campo <victor.delcampo@digitalservice.bund.de>
2024-01-30 09:24:54 +0100

    Add spring data and validation as libs
    RISDEV-0000

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -40,9 +40,8 @@ dependencies {
     implementation(libs.spring.boot.starter.actuator)
     implementation(libs.spring.boot.starter.security)
     implementation(libs.spring.boot.starter.web)
-
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.2")
-    implementation("org.springframework.boot:spring-boot-starter-validation:3.2.2")
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.spring.boot.starter.validation)
 
     compileOnly(libs.lombok)
 
