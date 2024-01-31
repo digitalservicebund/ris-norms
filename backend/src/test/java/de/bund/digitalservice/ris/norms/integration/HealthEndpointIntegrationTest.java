a4aa5578426d953f990db2384020c71186a773e6
Sebastian Rossa <sebastian.rossa@digitalservice.bund.de>
2024-01-31 12:57:04 +0100

    Extract test setup to test base class RISDEV-3260

--- backend/src/test/java/de/bund/digitalservice/ris/norms/integration/HealthEndpointIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/integration/HealthEndpointIntegrationTest.java
@@ -3,45 +3,11 @@ package de.bund.digitalservice.ris.norms.integration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-@Testcontainers(disabledWithoutDocker = true)
-@Tag("integration")
-class HealthEndpointIntegrationTest {
-
-  @Container
-  static PostgreSQLContainer<?> postgreSQLContainer =
-      new PostgreSQLContainer<>("postgres:16")
-          .withExposedPorts(5432)
-          .withDatabaseName("postgres")
-          .withUsername("postgres")
-          .withPassword("pass");
-
-  @DynamicPropertySource
-  static void registerDynamicProperties(DynamicPropertyRegistry registry) {
-    registry.add("spring.datasource.username", () -> postgreSQLContainer.getUsername());
-    registry.add("spring.datasource.password", () -> postgreSQLContainer.getPassword());
-    registry.add(
-        "spring.datasource.url",
-        () ->
-            String.format(
-                "jdbc:postgresql://localhost:%d/postgres",
-                postgreSQLContainer.getFirstMappedPort()));
-    registry.add("database.database", () -> postgreSQLContainer.getDatabaseName());
-  }
+class HealthEndpointIntegrationTest extends BaseIntegrationTest {
 
   @Test
   void actuatorHealthShouldBeReachable(@Autowired MockMvc mvc) throws Exception {
