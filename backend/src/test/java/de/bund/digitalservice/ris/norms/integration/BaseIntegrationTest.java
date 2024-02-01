7f228923cc72287327e8106e50ff7181c8224036
Victor del Campo <victor.delcampo@digitalservice.bund.de>
2024-02-01 15:25:28 +0100

    Added @DirtiesContext for integration tests
    RISDEV-3260

--- backend/src/test/java/de/bund/digitalservice/ris/norms/integration/BaseIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/integration/BaseIntegrationTest.java
@@ -5,12 +5,23 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+/**
+ * This class is a base class for integration tests which require a PostgreSQL database
+ * testcontainer. The usage of {@code @DirtiesContext} needed because {@code @SpringBootTest} is
+ * reusing Spring context between tests, meaning there is a common Hikari Pool between tests. But
+ * testcontainer is killing previous container and creating new one. SpringBootTest is not aware of
+ * that change what results in a new postgres container with the same Hikari Pool as the previous
+ * test, which is pointing to the already used an unavailable port. Using {@code @DirtiesContext}
+ * tells the test framework to close and recreate the context.
+ */
+@DirtiesContext
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureDataJpa
 @AutoConfigureMockMvc
@@ -19,7 +30,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 public abstract class BaseIntegrationTest {
 
   @Container
-  static PostgreSQLContainer<?> postgreSQLContainer =
+  static final PostgreSQLContainer<?> postgreSQLContainer =
       new PostgreSQLContainer<>("postgres:14")
           .withDatabaseName("postgres")
           .withUsername("postgres")
@@ -27,13 +38,14 @@ public abstract class BaseIntegrationTest {
 
   @DynamicPropertySource
   static void registerDynamicProperties(DynamicPropertyRegistry registry) {
-    registry.add("spring.datasource.username", () -> postgreSQLContainer.getUsername());
-    registry.add("spring.datasource.password", () -> postgreSQLContainer.getPassword());
+    registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+    registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
     registry.add(
         "spring.datasource.url",
         () ->
             String.format(
                 "jdbc:postgresql://localhost:%d/%s",
-                postgreSQLContainer.getFirstMappedPort(), postgreSQLContainer.getDatabaseName()));
+                postgreSQLContainer.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT),
+                postgreSQLContainer.getDatabaseName()));
   }
 }
