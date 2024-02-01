43a2de98b78e4fa32628e777678a8a4f1c7ba04c
Sebastian Rossa <sebastian.rossa@digitalservice.bund.de>
2024-02-01 17:04:13 +0100

    Add build script for whole project
    RISDEV-3260

---
+++ buildProject.sh
@@ -0,0 +1,13 @@
+#!   /bin/bash -e
+
+npm --prefix ./vscode-extension/ install
+npm --prefix ./vscode-extension/ run vscode:pack
+
+./time-machine/gradlew -p ./time-machine spotlessApply
+./time-machine/gradlew -p ./time-machine  build
+./time-machine/gradlew -p ./time-machine  install
+./time-machine/gradlew -p ./time-machine  checkLicense
+
+docker build -t ris-norms-vs-code:001 -f DockerfileVsCode .
+
+docker build -t ris-norms-app:001 -f DockerfileApp .
