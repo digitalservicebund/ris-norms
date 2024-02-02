892356ebcb2cf40a16889ffbb8bf1c135d4e1638
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-02-02 12:04:34 +0100

    Integrate pinia in our app
    RISDEV-3117

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from "vue"
+import { createPinia } from "pinia"
 import "./style.css"
 import App from "./App.vue"
 import router from "./router"
 
-createApp(App).use(router).mount("#app")
+const storeManager = createPinia()
+
+createApp(App).use(storeManager).use(router).mount("#app")
