da1c04fa70ca3c8acd0ee14ae8b6306ec07a0655
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-02-15 11:35:03 +0100

    Adjusts details page to include only data points in the db
    RISDEV-3275

--- frontend/src/components/procedures/RisProcedureCard.vue
+++ frontend/src/components/procedures/RisProcedureCard.vue
@@ -6,28 +6,20 @@ const props = defineProps<{
   eli: string
   printAnnouncementGazette: string
   printAnnouncementYear: number
-  printAnnouncementNumber: number
   printAnnouncementPage: number
-  publicationDate: Date
 }>()
 
-const formattedDate = computed(() => {
-  const day = String(props.publicationDate.getDate()).padStart(2, "0")
-  const month = String(props.publicationDate.getMonth() + 1).padStart(2, "0")
-  const year = props.publicationDate.getFullYear()
-
-  return `${day}.${month}.${year}`
-})
+const printAnnouncementGazetteUpper = computed(() =>
+  props.printAnnouncementGazette.toUpperCase(),
+)
 </script>
 
 <template>
-  <div class="flex gap-8 bg-white p-16 hover:bg-blue-200">
-    <span class="ds-label-02-bold w-128 flex-none">
-      {{ printAnnouncementGazette }} {{ printAnnouncementYear }} Nr.
-      {{ printAnnouncementNumber }}
+  <div class="flex justify-between gap-8 bg-white p-16 hover:bg-blue-200">
+    <span class="ds-label-02-bold w-128 flex-none flex-grow">
+      {{ printAnnouncementGazetteUpper }} {{ printAnnouncementYear }} Nr.
+      {{ printAnnouncementPage }}
     </span>
-
-    <span class="flex-auto">{{ formattedDate }}</span>
 
     <button class="text-blue-800">
       <ExpandMoreIcon class="h-24 w-24" />
