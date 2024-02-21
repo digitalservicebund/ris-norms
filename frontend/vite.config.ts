f25320c444f964f4d036874465dccd88cf5fb27a
Sebastian Rossa <sebastian.rossa@digitalservice.bund.de>
2024-02-21 17:15:35 +0100

    Configure proxy in frontend to reach backend
    - an backend run at localhost:8080 can be reached now
    
    RISDEV-0000

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -65,4 +65,14 @@ export default defineConfig({
       "@e2e": fileURLToPath(new URL("./e2e", import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
 })
