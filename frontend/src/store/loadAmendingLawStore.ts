9ba5d0652e5014c9126abd676b82c68987d24283
Sebastian Rossa <sebastian.rossa@digitalservice.bund.de>
2024-02-21 18:04:08 +0100

    Load one specific law with articles when needed
    RISDEV-3404

--- frontend/src/store/loadAmendingLawStore.ts
+++ frontend/src/store/loadAmendingLawStore.ts
@@ -1,19 +1,27 @@
 import { defineStore } from "pinia"
 import { ComputedRef, ref } from "vue"
-import { getAmendingLaws, AmendingLaw } from "@/services/amendingLawsService"
+import {
+  getAmendingLaws,
+  AmendingLaw,
+  getAmendingLawByEli,
+} from "@/services/amendingLawsService"
 
 export const useAmendingLawsStore = defineStore("amendingLaws", () => {
   const amendingLaws = ref<AmendingLaw[]>([])
+  const selectedAmendingLaw = ref<AmendingLaw>()
 
   async function loadAmendingLaws() {
     amendingLaws.value = await getAmendingLaws()
   }
 
-  function loadAmendingLawByEli(eli: ComputedRef<string>): AmendingLaw {
-    return amendingLaws.value.filter(
-      (amendingLaw) => amendingLaw.eli == eli.value,
-    )[0]
+  async function loadAmendingLawByEli(eli: ComputedRef<string>) {
+    selectedAmendingLaw.value = await getAmendingLawByEli(eli.value)
   }
 
-  return { amendingLaws, loadAmendingLaws, loadAmendingLawByEli }
+  return {
+    amendingLaws,
+    selectedAmendingLaw,
+    loadAmendingLaws,
+    loadAmendingLawByEli,
+  }
 })
--- frontend/src/views/ArticleOverview.vue
+++ frontend/src/views/ArticleOverview.vue
@@ -1,20 +1,30 @@
 <script setup lang="ts">
 import RisInfoModal from "@/components/controls/RisInfoModal.vue"
 import { useRoute } from "vue-router"
-import { computed } from "vue"
+import { computed, onMounted } from "vue"
 import { useAmendingLawsStore } from "@/store/loadAmendingLawStore"
+import { storeToRefs } from "pinia"
 
 const route = useRoute()
 const eli = computed(() => decodeURIComponent(route.params.id?.toString()))
+
 const amendingLawsStore = useAmendingLawsStore()
-const amendingLaw = amendingLawsStore.loadAmendingLawByEli(eli)
+const { selectedAmendingLaw } = storeToRefs(amendingLawsStore)
+
+onMounted(async () => {
+  try {
+    await amendingLawsStore.loadAmendingLawByEli(eli)
+  } catch (error) {
+    //TODO: handle error
+  }
+})
 </script>
 
 <template>
   <div>
     <h1 class="ds-heading-02-reg mb-40">Enthaltene Artikel</h1>
     <RisInfoModal
-      v-for="(article, index) in amendingLaw.articles"
+      v-for="(article, index) in selectedAmendingLaw.articles"
       :key="index"
       :title="`Artikel ${article.enumeration}`"
       :description="article.title"
