2bd330368e10f92d5623b112829eb3d7632d2781
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-02-28 15:15:37 +0100

    Return Document from valid xml string
    - Allow for "functions" in hexagonal "core"
    
    RISDEV-3446

--- backend/src/test/java/de/bund/digitalservice/ris/norms/domain/functions/TimeMachineFunctionsTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/domain/functions/TimeMachineFunctionsTest.java
@@ -2,21 +2,23 @@ package de.bund.digitalservice.ris.norms.domain.functions;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Optional;
 import org.junit.Test;
 import org.w3c.dom.Document;
 
 public class TimeMachineFunctionsTest {
-    // @Test
-    // public void XmlDocumentsGoInAndOut() {}
+  // @Test
+  // public void XmlDocumentsGoInAndOut() {}
 
-    @Test
-    public void DocumentGeneratedFromString() {
-        // given
-        final String input = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><root/>";
-        // when
-        final Document result = TimeMachineFunctions.readXmlDocumentFromString(input);
-        // then
-        assertNotNull(result);
+  final String minimalXmlString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><root/>";
 
-    }
+  @Test
+  public void DocumentGeneratedFromStringMustNotBeEmpty() {
+    // given
+    final String input = minimalXmlString;
+    // when
+    final Optional<Document> result = TimeMachineFunctions.loadXMLFromString(input);
+    // then
+    assertNotNull(result);
+  }
 }
