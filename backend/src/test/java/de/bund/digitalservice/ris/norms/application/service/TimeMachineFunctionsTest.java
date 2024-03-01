a8b68bcd5e56a46fd7d6d3d5b146864d01164684
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-03-01 15:03:15 +0100

    Rename test file (typo), add todo
    RISDEV-3446

--- backend/src/test/java/de/bund/digitalservice/ris/norms/application/service/TimeMachineFunctionsTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/application/service/TimeMachineFunctionsTest.java
@@ -3,19 +3,17 @@ package de.bund.digitalservice.ris.norms.application.service;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Optional;
 import org.junit.Test;
 import org.w3c.dom.Document;
 
 public class TimeMachineFunctionsTest {
 
-  final String minimalXmlString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><root/>";
-
   @Test
   public void xmlDocumentsGoInAndOut() {
     // given
-    final Document amendingLaw = TimeMachineFunctions.loadXMLFromString(minimalXmlString).get();
-    final Document targetLaw = TimeMachineFunctions.loadXMLFromString(minimalXmlString).get();
+    final String minimalXmlString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><root/>";
+    final Document amendingLaw = XmlFunctions.loadXMLFromString(minimalXmlString).get();
+    final Document targetLaw = XmlFunctions.loadXMLFromString(minimalXmlString).get();
     // when
     final Document result = TimeMachineFunctions.applyTimeMachine(amendingLaw, targetLaw);
     // then
@@ -26,36 +24,13 @@ public class TimeMachineFunctionsTest {
   public void targetLawStaysUnchangedIfAmendingLawHasNoModifications() {
     // given
     final Document amendingLaw =
-        TimeMachineFunctions.loadXMLFromString(
-                "<?xml version=\"1.0\" encoding=\"UTF-8\"?><amending/>")
+        XmlFunctions.loadXMLFromString("<?xml version=\"1.0\" encoding=\"UTF-8\"?><amending/>")
             .get();
     final Document targetLaw =
-        TimeMachineFunctions.loadXMLFromString(
-                "<?xml version=\"1.0\" encoding=\"UTF-8\"?><target/>")
-            .get();
+        XmlFunctions.loadXMLFromString("<?xml version=\"1.0\" encoding=\"UTF-8\"?><target/>").get();
     // when
     final Document result = TimeMachineFunctions.applyTimeMachine(amendingLaw, targetLaw);
     // then
     assertTrue(result.equals(targetLaw));
-  }
-
-  @Test
-  public void documentGeneratedFromValidXmlStringMustNotBeEmpty() {
-    // given
-    final String input = minimalXmlString;
-    // when
-    final Optional<Document> result = TimeMachineFunctions.loadXMLFromString(input);
-    // then
-    assertTrue(result.isPresent());
-  }
-
-  @Test
-  public void documentGeneratedFromInValidXmlStringMustBeEmpty() {
-    // given
-    final String input = "invalid XML as it does not start with the XML declaration";
-    // when
-    final Optional<Document> result = TimeMachineFunctions.loadXMLFromString(input);
-    // then
-    assertTrue(result.isEmpty());
   }
 }
