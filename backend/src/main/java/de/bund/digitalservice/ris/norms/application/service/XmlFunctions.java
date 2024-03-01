f23361bf8adb223418ab21c851080d0ffd7d3e17
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-03-01 16:01:51 +0100

    Return empty if node not found

--- backend/src/main/java/de/bund/digitalservice/ris/norms/application/service/XmlFunctions.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/application/service/XmlFunctions.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.xml.sax.InputSource;
 
 /** TODO */
@@ -20,13 +21,23 @@ public class XmlFunctions {
   public static Optional<Document> loadXMLFromString(String xml) {
     try {
 
-      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-      DocumentBuilder builder = factory.newDocumentBuilder();
-      InputSource is = new InputSource(new StringReader(xml));
-      Document document = builder.parse(is);
+      final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+      final DocumentBuilder builder = factory.newDocumentBuilder();
+      final InputSource is = new InputSource(new StringReader(xml));
+      final Document document = builder.parse(is);
       return Optional.of(document);
     } catch (Exception e) {
       // TODO: do something with e. Maybe log it?
+    }
+
+    return Optional.empty();
+  }
+
+  static Optional<Node> getNode(String expression, Document document) {
+    try {
+      throw new Exception("not implemented, yet");
+    } catch (Exception e) {
+      // TODO: do something with e?
     }
 
     return Optional.empty();
