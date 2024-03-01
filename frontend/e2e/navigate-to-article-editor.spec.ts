f0639fc72d86da63bf114b580439b3c46a428bc0
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2024-03-01 14:36:19 +0100

    Fix e2e test
    It still had the old route for the edit page
    
    RISDEV-3463

--- frontend/e2e/navigate-to-article-editor.spec.ts
+++ frontend/e2e/navigate-to-article-editor.spec.ts
@@ -2,12 +2,12 @@ import { test, expect } from "@playwright/test"
 
 test(`navigate to article editor`, async ({ page }) => {
   await page.goto(
-    "/amendinglaws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1",
+    "/amending-laws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1",
   )
   await page.getByText("Änderungsbefehl prüfen").click()
 
   await expect(page).toHaveURL(
-    `/amendinglaws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/hauptteil-1_art-1/edit`,
+    `/amending-laws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/articles/hauptteil-1_art-1/edit`,
   )
 
   await expect(page.getByRole("heading", { name: "Artikel 1" })).toBeVisible()
