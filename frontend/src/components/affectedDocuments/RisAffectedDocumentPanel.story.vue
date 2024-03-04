bff8173c801ba36e182dca3cf9dcbcf2d62093a3
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2024-03-04 16:15:39 +0100

    Feat: add WIP of affected document list item
    Currently not integrated in the application, but will be used for displaying
    affected documents and linking to the metadata editor. Unit tests to be done.
    
    RISDEV-3126

--- frontend/src/components/affectedDocuments/RisAffectedDocumentPanel.story.vue
+++ frontend/src/components/affectedDocuments/RisAffectedDocumentPanel.story.vue
@@ -3,5 +3,13 @@ import RisAffectedDocumentPanel from "./RisAffectedDocumentPanel.vue"
 </script>
 
 <template>
-  <RisAffectedDocumentPanel />
+  <Story>
+    <RisAffectedDocumentPanel
+      :fna="'210-5'"
+      :short-title="'BVerfSchG'"
+      :title="'Gesetz über die Zusammenarbeit des Bundes und der Länder in Angelegenheiten des Verfassungsschutzes und über das Bundesamt für Verfassungsschutz'"
+      :eli="'eli/bund/bgbl-1/1968/s537/1968-05-19/18/deu/regelungstext-1'"
+      :as-list-item="true"
+    />
+  </Story>
 </template>
