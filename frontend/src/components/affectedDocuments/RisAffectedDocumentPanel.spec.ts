455d6afc9f1ffd073c4b720b45b87059db77e3d4
Andreas Ganske <a.ganske@chaosspace.de>
2024-03-04 18:14:32 +0100

    Add tests for RisAffectedDocumentPanel
    RISDEV-3126

--- frontend/src/components/affectedDocuments/RisAffectedDocumentPanel.spec.ts
+++ frontend/src/components/affectedDocuments/RisAffectedDocumentPanel.spec.ts
@@ -1,5 +1,61 @@
-import { describe, test } from "vitest"
+import { describe, expect, test } from "vitest"
+import { render, screen } from "@testing-library/vue"
+import RisAffectedDocumentPanel from "@/components/affectedDocuments/RisAffectedDocumentPanel.vue"
 
 describe("RisAffectedDocumentPanel", () => {
-  test.todo("initializes correctly", () => {})
+  const eli = "eli/bund/bgbl-1/1968/s537/1968-05-19/18/deu/regelungstext-1"
+  const title = "Some title"
+
+  test("should show title, eli and edit metadata button", () => {
+    render(RisAffectedDocumentPanel, {
+      props: {
+        eli,
+        title,
+        asListItem: true,
+      },
+    })
+    // question: How to check, if the element is a <li>? Should we check?
+    expect(screen.getByText(title)).toBeInTheDocument()
+    expect(screen.getByText(eli)).toBeInTheDocument()
+    expect(screen.getByText("Metadaten bearbeiten")).toBeInTheDocument()
+  })
+
+  test("should render fna", () => {
+    render(RisAffectedDocumentPanel, {
+      props: {
+        eli,
+        title,
+        fna: "1234",
+        asListItem: true,
+      },
+    })
+    expect(document.body).toHaveTextContent("FNA 1234")
+    expect(document.body).not.toHaveTextContent("FNA 1234-")
+  })
+
+  test("should render fna and shortTitle", () => {
+    render(RisAffectedDocumentPanel, {
+      props: {
+        eli,
+        title,
+        fna: "1234",
+        shortTitle: "SomeShortTitle",
+        asListItem: true,
+      },
+    })
+    expect(document.body).toHaveTextContent("FNA 1234-SomeShortTitle")
+  })
+
+  test("should render fna and shortTitle", () => {
+    render(RisAffectedDocumentPanel, {
+      props: {
+        eli,
+        title,
+        shortTitle: "SomeShortTitle",
+        asListItem: true,
+      },
+    })
+    expect(document.body).toHaveTextContent("SomeShortTitle")
+    expect(document.body).not.toHaveTextContent("FNA")
+  })
 })
