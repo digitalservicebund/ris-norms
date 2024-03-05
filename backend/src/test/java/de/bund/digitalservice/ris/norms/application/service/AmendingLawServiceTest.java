86eb43ad7d2c964b9c87ef8b26112963d89c5e6e
Victor del Campo <victor.delcampo@digitalservice.bund.de>
2024-03-05 15:06:59 +0100

    Add unit test to amending service
    RISDEV-0000

--- backend/src/test/java/de/bund/digitalservice/ris/norms/application/service/AmendingLawServiceTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/application/service/AmendingLawServiceTest.java
@@ -9,8 +9,11 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import de.bund.digitalservice.ris.norms.application.port.input.LoadAmendingLawUseCase;
+import de.bund.digitalservice.ris.norms.application.port.input.LoadArticleUseCase;
+import de.bund.digitalservice.ris.norms.application.port.input.LoadArticlesUseCase;
 import de.bund.digitalservice.ris.norms.application.port.output.*;
 import de.bund.digitalservice.ris.norms.domain.entity.AmendingLaw;
+import de.bund.digitalservice.ris.norms.domain.entity.Article;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
@@ -143,5 +146,62 @@ class AmendingLawServiceTest {
     // Then
     assertThat(amendingLaw).hasSize(2).containsExactlyElementsOf(expectedAmendingLaws);
     verify(loadAllAmendingLawsAdapter, times(1)).loadAllAmendingLaws();
+  }
+
+  @Test
+  void canLoadArticleByEliAndEid() {
+    // Given
+    final String eli = "someEli";
+    final String eId = "someEid";
+
+    final LoadArticleUseCase.Query query = new LoadArticleUseCase.Query(eli, eId);
+
+    final String title = "Article Title";
+    final String enumeration = "Article Enumeration";
+
+    final Article article =
+        Article.builder().title(title).enumeration(enumeration).eid(eId).build();
+
+    when(loadArticlePort.loadArticleByEliAndEid(any())).thenReturn(Optional.of(article));
+
+    // When
+    final Optional<Article> loadedArticle = service.loadArticle(query);
+
+    // Then
+    assertThat(loadedArticle).isPresent().contains(article);
+    verify(loadArticlePort, times(1))
+        .loadArticleByEliAndEid(
+            argThat(command -> command.eli().equals(eli) && command.eId().equals(eId)));
+  }
+
+  @Test
+  void canLoadArticlesOfAmendingLawByEli() {
+    // Given
+    final String eli = "someEli";
+    final LoadArticlesUseCase.Query query = new LoadArticlesUseCase.Query(eli);
+
+    final String title1 = "Article 1 Title";
+    final String enumeration1 = "Article 1 Enumeration";
+    final String eid1 = "article-eid-1";
+    final Article article1 =
+        Article.builder().title(title1).enumeration(enumeration1).eid(eid1).build();
+
+    final String title2 = "Article 2 Title";
+    final String enumeration2 = "Article 2 Enumeration";
+    final String eid2 = "article-eid-2";
+    final Article article2 =
+        Article.builder().eid(eid2).title(title2).enumeration(enumeration2).build();
+
+    final List<Article> expectedArticles = List.of(article1, article2);
+
+    when(loadArticlesPort.loadArticlesByAmendingLaw(any())).thenReturn(expectedArticles);
+
+    // When
+    List<Article> loadedArticles = service.loadArticlesOfAmendingLaw(query);
+
+    // Then
+    assertThat(loadedArticles).hasSize(2).containsExactlyElementsOf(expectedArticles);
+    verify(loadArticlesPort, times(1))
+        .loadArticlesByAmendingLaw(argThat(command -> command.eli().equals(eli)));
   }
 }
