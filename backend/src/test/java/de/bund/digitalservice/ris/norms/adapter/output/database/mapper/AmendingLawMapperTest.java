9ddbf64cc96e92841ee9add9c73a0f9af63c4939
Victor del Campo <victor.delcampo@digitalservice.bund.de>
2024-03-05 11:36:34 +0100

    Rename test
    RISDEV-0000

--- backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/output/database/mapper/AmendingLawMapperTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/output/database/mapper/AmendingLawMapperTest.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
-class AmendingLawResponseMapperTest {
+class AmendingLawMapperTest {
 
   @Test
   void testMapToDomain() {
