ef5015d393c15cbc65e03cd134f4680ab96eecf5
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2024-03-11 15:02:11 +0100

    feat: support multiple ELIs in routes
    This commit:
    
    - Replaces the constant ELI path parameter with a function that returns
      an ELI path parameter (optionally with a prefix to support multiple
      ELIs in the same route through different parameter names)
    - Updates the ELI path composable to also support the optional prefix
    - Updates the router to use the new function instead of the constant
    
    RISDEV-3126

--- frontend/src/composables/useEliPathParameter.ts
+++ frontend/src/composables/useEliPathParameter.ts
@@ -2,29 +2,68 @@ import { ComputedRef, computed } from "vue"
 import { useRoute } from "vue-router"
 
 /**
- * Provides a reference to the eli of the current route
+ * Returns a string containing a set of named path params that can be used
+ * for creating a part of a route that specifies an ELI.
  *
- * @returns a reference to the eli of the current route
+ * Use in combination with `useEliPathParameter` to get the ELI from the
+ * current route.
+ *
+ * @param prefix If provided, prefixes each named path param with the value.
+ *  This allows you to have multiple ELIs in a single route.
  */
-export function useEliPathParameter(): ComputedRef<string> {
+export function createEliPathParameter(prefix?: string) {
+  const name = prefix ? `${prefix}Eli` : "eli"
+
+  // The regular expressions for the parts of the ELI are based on the definitions
+  // from LDML.de 1.6 for ELIs for "Verkündungsfassungen und Neufassungen". It was
+  // not possible to put the ELI in just one combined path parameter as vue-router
+  // replaces slashes in path parameters automatically by %2F when routing to a new
+  // route.
+  return [
+    "eli",
+    `:${name}Jurisdiction(bund)`,
+    `:${name}Agent(bgbl-1|bgbl-2|banz-at)`,
+    `:${name}Year([12][0-9]{3})`,
+    `:${name}NaturalIdentifier(s[0-9]+[a-zäöüß]*|[0-9]+)`,
+    `:${name}PointInTime([12][0-9]{3}-[0-9]{2}-[0-9]{2})`,
+    `:${name}Version([0-9]+)`,
+    `:${name}Language(deu)`,
+    `:${name}Subtype(regelungstext-[0-9]+|offenestruktur-[0-9]+|vereinbarung-[0-9]+|bekanntmachungstext-[0-9]+|externesdokument-[0-9]+|rechtsetzungsdokument-[0-9]+)`,
+  ].join("/")
+}
+
+/**
+ * Provides a reference to the ELI of the current route. This is reconstructed
+ * from the named path params created by `createEliPathParameter`.
+ *
+ * @param prefix If provided, only returns the path parameters that use the
+ *  specified prefix. This allows you to get multiple ELIs from a single route.
+ * @returns A reference to the ELI of the current route
+ */
+export function useEliPathParameter(prefix?: string): ComputedRef<string> {
   const { params } = useRoute()
+  const name = prefix ? `${prefix}Eli` : "eli"
+
+  const parameterNames = [
+    `${name}Jurisdiction`,
+    `${name}Agent`,
+    `${name}Year`,
+    `${name}NaturalIdentifier`,
+    `${name}PointInTime`,
+    `${name}Version`,
+    `${name}Language`,
+    `${name}Subtype`,
+  ]
 
   return computed(() => {
-    if (
-      params.eliJurisdiction == undefined ||
-      params.eliAgent == undefined ||
-      params.eliYear == undefined ||
-      params.eliNaturalIdentifier == undefined ||
-      params.eliPointInTime == undefined ||
-      params.eliVersion == undefined ||
-      params.eliLanguage == undefined ||
-      params.eliSubtype == undefined
-    ) {
+    const parameterValues = parameterNames.map((name) => params[name])
+
+    if (parameterValues.some((value) => value === undefined)) {
       throw new Error(
-        "useEliPathParameter: You can only use this composable on pages which have an eli in their route",
+        `useEliPathParameter: You can only use this composable on pages which have an ELI${prefix ? " prefixed with " + prefix : ""} in their route`,
       )
     }
 
-    return `eli/${params.eliJurisdiction}/${params.eliAgent}/${params.eliYear}/${params.eliNaturalIdentifier}/${params.eliPointInTime}/${params.eliVersion}/${params.eliLanguage}/${params.eliSubtype}`
+    return ["eli", ...parameterValues].join("/")
   })
 }
