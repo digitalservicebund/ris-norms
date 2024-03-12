e7ce95322b57b01eb9865eb93701a99a898e53ac
Victor del Campo <victor.delcampo@digitalservice.bund.de>
2024-03-12 08:33:22 +0100

    Removed missing parts of old time machine
    RISDEV-3527

--- buildProject.sh
+++ buildProject.sh
@@ -1,8 +1,3 @@
 #!   /bin/bash -e
 
-./time-machine/gradlew -p ./time-machine spotlessApply
-./time-machine/gradlew -p ./time-machine  build
-./time-machine/gradlew -p ./time-machine  install
-./time-machine/gradlew -p ./time-machine  checkLicense
-
 docker build -t ris-norms-app:001 -f DockerfileApp .
