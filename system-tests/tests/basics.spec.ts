5dd963c5918cde7537e3a2e0b9a8470b4d693015
Victor del Campo <victor.delcampo@digitalservice.bund.de>
2024-03-15 10:30:39 +0100

    remove system-tests folder
    RISDEV-0000

--- system-tests/tests/basics.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("it shows the intial view of the VSCode UI", async ({ page }) => {
-  await page.goto("/");
-
-  const welcomeLabel = page.getByLabel("Welcome, preview").getByText("Welcome");
-
-  await expect(welcomeLabel).toBeVisible();
-});
