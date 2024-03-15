4309d4e655c574a926dc82b5f09acec47ce538e7
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2024-03-15 14:09:37 +0100

    feat: add service for saving target law XMLs
    RISDEV-3306

--- frontend/src/services/targetLawsService.spec.ts
+++ frontend/src/services/targetLawsService.spec.ts
@@ -64,6 +64,36 @@ describe("targetLawsService", () => {
     })
   })
 
+  describe("putTargetLawXml(eli, xml)", () => {
+    it("sends the data to the API", async () => {
+      const fetchMock = vi
+        .fn()
+        .mockResolvedValueOnce('<?xml version="1.0" encoding="UTF-8"?></xml>')
+
+      vi.doMock("./apiService.ts", () => ({ apiFetch: fetchMock }))
+
+      const { putTargetLawXml } = await import("./targetLawsService")
+
+      const result = await putTargetLawXml(
+        "eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1",
+        '<?xml version="1.0" encoding="UTF-8"?></xml>',
+      )
+      expect(result).toBe('<?xml version="1.0" encoding="UTF-8"?></xml>')
+
+      expect(fetchMock).toHaveBeenCalledWith(
+        "/target-laws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1",
+        expect.objectContaining({
+          method: "PUT",
+          headers: expect.objectContaining({
+            "Content-Type": "application/xml",
+            Accept: "application/xml",
+          }),
+          body: '<?xml version="1.0" encoding="UTF-8"?></xml>',
+        }),
+      )
+    })
+  })
+
   describe("previewTargetLaw(eli, xml)", () => {
     it("calls the api correctly", async () => {
       const fetchMock = vi
