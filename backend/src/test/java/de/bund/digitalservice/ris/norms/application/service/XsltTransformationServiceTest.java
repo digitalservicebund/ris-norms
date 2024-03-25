3cf6b91dc293b6e2ee4156dd35e08cd1a0570f4f
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2024-03-25 13:51:51 +0100

    Add test for xslt transformations
    RISDEV-3647

--- backend/src/test/java/de/bund/digitalservice/ris/norms/application/service/XsltTransformationServiceTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/application/service/XsltTransformationServiceTest.java
@@ -10,7 +10,15 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.stream.Stream;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.core.io.FileUrlResource;
 import org.springframework.core.io.Resource;
 
 class XsltTransformationServiceTest {
@@ -173,5 +181,43 @@ class XsltTransformationServiceTest {
             """
             SXXP0003   Error reported by XML parser: Attribute name "xml" associated with an element type "invalid" must be followed by the ' = ' character.
             """);
+  }
+
+  @ParameterizedTest(name = "{0}")
+  @MethodSource("shouldTransformXmlArgumentsProvider")
+  void shouldTransformXml(String name, String xml, Boolean showMetadata, String expectedHtml)
+      throws TransformLegalDocMlToHtmlUseCase.XmlTransformationException {
+    var xsltResource =
+        new FileUrlResource(
+            Objects.requireNonNull(
+                XsltTransformationService.class.getResource("/XSLT/html/legislation.xslt")));
+    var xsltTransformationService = new XsltTransformationService(xsltResource);
+
+    var result =
+        xsltTransformationService.transformLegalDocMlToHtml(
+            new TransformLegalDocMlToHtmlUseCase.Query(xml, showMetadata));
+
+    assertThat(result).isEqualToIgnoringWhitespace(expectedHtml);
+  }
+
+  static String loadTestResource(String fileName) throws IOException {
+    var resource =
+        XsltTransformationServiceTest.class.getResource("xsltTransformationService/" + fileName);
+    assert resource != null;
+    return IOUtils.toString(resource, StandardCharsets.UTF_8);
+  }
+
+  static Stream<Arguments> shouldTransformXmlArgumentsProvider() throws IOException {
+    return Stream.of(
+        Arguments.arguments(
+            "Bundesverfassungsschutzgesetz",
+            loadTestResource("Bundesverfassungsschutzgesetz.xml"),
+            false,
+            loadTestResource("Bundesverfassungsschutzgesetz.html")),
+        Arguments.arguments(
+            "Bundesverfassungsschutzgesetz with metadata",
+            loadTestResource("Bundesverfassungsschutzgesetz.xml"),
+            true,
+            loadTestResource("Bundesverfassungsschutzgesetz-with-metadata.html")));
   }
 }
