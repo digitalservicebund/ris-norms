271bb8c781d16499a0c79e384bf204b4bad4cf7d
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-03-26 00:09:09 +0100

    Adds new services for GET & PUT calls for publishing
    RISDEV-3624

---
+++ frontend/src/services/amendingLawReleaseService.ts
@@ -0,0 +1,34 @@
+import { apiFetch } from "@/services/apiService"
+import { AmendingLawReleaseResponse } from "@/types/amendingLawReleaseResponse"
+
+/**
+ * Release the XML of a specific amending law by eli.
+ *
+ * @param eli Eli of the amending law
+ * @returns the publishing timestamp
+ * @returns the eli of the amending law
+ * @returns the elis of all ZF0
+ */
+export async function putReleaseAmendingLawXml(
+  eli: string,
+): Promise<AmendingLawReleaseResponse> {
+  return await apiFetch(`/amending-laws/${eli}/release`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/xml",
+      Accept: "application/json",
+    },
+  })
+}
+
+export async function getAmendingLawReleaseStatus(
+  eli: string,
+): Promise<AmendingLawReleaseResponse> {
+  return await apiFetch(`/amending-laws/${eli}/release`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/xml",
+      Accept: "application/json",
+    },
+  })
+}
