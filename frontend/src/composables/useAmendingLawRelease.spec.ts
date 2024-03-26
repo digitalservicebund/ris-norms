d03f8764335dd80e0ba5876e9e160caab46f4346
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-03-26 09:19:48 +0100

    Adds unit tests for release composable
    RISDEV-3624

---
+++ frontend/src/composables/useAmendingLawRelease.spec.ts
@@ -0,0 +1,68 @@
+import { describe, it, expect, vi, beforeEach } from "vitest"
+import { ref } from "vue"
+import { useAmendingLawRelease } from "./useAmendingLawRelease"
+import * as amendingLawReleaseService from "@/services/amendingLawReleaseService"
+
+describe("useAmendingLawRelease", () => {
+  beforeEach(() => {
+    vi.resetAllMocks()
+  })
+
+  describe("fetchReleaseStatus", () => {
+    it("should fetch the release status and update state", async () => {
+      const mockReleaseStatus = {
+        releaseAt: "2024-03-25T10:37:29.658954Z",
+        amendingLawEli:
+          "eli/bund/bgbl-1/2023/413/2023-12-29/1/deu/regelungstext-1",
+        zf0Elis: [
+          "eli/bund/bgbl-1/1990/s2954/2023-12-29/1/deu/regelungstext-1",
+        ],
+      }
+
+      vi.spyOn(
+        amendingLawReleaseService,
+        "getAmendingLawReleaseStatus",
+      ).mockResolvedValue(mockReleaseStatus)
+
+      const eli = ref(
+        "eli/bund/bgbl-1/2023/413/2023-12-29/1/deu/regelungstext-1",
+      )
+      const { releasedAt, releasedElis, fetchReleaseStatus } =
+        useAmendingLawRelease(eli)
+
+      await fetchReleaseStatus()
+
+      expect(releasedAt.value).toBeInstanceOf(Date)
+      expect(releasedElis.value).toEqual(mockReleaseStatus)
+    })
+  })
+
+  describe("releaseAmendingLaw", () => {
+    it("should send a release request and update state with the new release info", async () => {
+      const mockReleaseResponse = {
+        releaseAt: "2024-03-25T11:00:00.000Z",
+        amendingLawEli:
+          "eli/bund/bgbl-1/2023/414/2023-12-30/1/deu/regelungstext-1",
+        zf0Elis: [
+          "eli/bund/bgbl-1/1991/s2955/2023-12-30/1/deu/regelungstext-2",
+        ],
+      }
+
+      vi.spyOn(
+        amendingLawReleaseService,
+        "putReleaseAmendingLawXml",
+      ).mockResolvedValue(mockReleaseResponse)
+
+      const eli = ref(
+        "eli/bund/bgbl-1/2023/414/2023-12-30/1/deu/regelungstext-1",
+      )
+      const { releasedAt, releasedElis, releaseAmendingLaw } =
+        useAmendingLawRelease(eli)
+
+      await releaseAmendingLaw()
+
+      expect(releasedAt.value).toBeInstanceOf(Date)
+      expect(releasedElis.value).toEqual(mockReleaseResponse)
+    })
+  })
+})
