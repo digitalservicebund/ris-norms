c235fd1fb95e6907d3407d538f32bd2da0a2d2df
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2024-03-28 12:33:55 +0100

    Create composable for getting rendered amending law html
    RISDEV-3696

---
+++ frontend/src/composables/useAmendingLawHtml.spec.ts
@@ -0,0 +1,50 @@
+import { beforeEach, describe, expect, test, vi } from "vitest"
+import { nextTick, ref } from "vue"
+
+describe("useAmendingLawHtml", () => {
+  beforeEach(() => {
+    vi.resetModules()
+    vi.resetAllMocks()
+  })
+
+  test("should provide the amending law html", async () => {
+    const getAmendingLawHtmlByEli = vi.fn().mockResolvedValue("<div></div>")
+
+    vi.doMock("@/services/amendingLawsService", () => ({
+      getAmendingLawHtmlByEli,
+    }))
+
+    const { useAmendingLawHtml } = await import("./useAmendingLawHtml")
+
+    const eli = ref(
+      "eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1",
+    )
+    const html = useAmendingLawHtml(eli)
+    await vi.waitUntil(() => html.value)
+
+    expect(html.value).toBe("<div></div>")
+  })
+
+  test("should load the amending law html when the eli changes", async () => {
+    const getAmendingLawHtmlByEli = vi.fn()
+
+    vi.doMock("@/services/amendingLawsService", () => ({
+      getAmendingLawHtmlByEli,
+    }))
+
+    const { useAmendingLawHtml } = await import("./useAmendingLawHtml")
+
+    const eli = ref(
+      "eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1",
+    )
+    useAmendingLawHtml(eli)
+
+    eli.value = "eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/regelungstext-1"
+    await nextTick()
+
+    eli.value = "eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/regelungstext-1"
+    await nextTick()
+
+    expect(getAmendingLawHtmlByEli).toBeCalledTimes(2)
+  })
+})
---
+++ frontend/src/composables/useAmendingLawHtml.ts
@@ -0,0 +1,28 @@
+import { MaybeRefOrGetter, Ref, readonly, ref, toValue, watch } from "vue"
+import { getAmendingLawHtmlByEli } from "@/services/amendingLawsService"
+
+/**
+ * Get the rendered HTML of an amending law.
+ *
+ * @param eli a reference to the eli for which the law xml will be returned.
+ *  Changing the value of the reference will load the data for the new eli.
+ *
+ * @returns A reference to the amending law rendered HTML or undefined if it is not available (or still loading).
+ */
+export function useAmendingLawHtml(
+  eli: MaybeRefOrGetter<string | undefined>,
+): Readonly<Ref<string | undefined>> {
+  const amendingLawHtml = ref<string>()
+
+  watch(
+    () => toValue(eli),
+    async (eli) => {
+      if (eli) {
+        amendingLawHtml.value = await getAmendingLawHtmlByEli(eli)
+      }
+    },
+    { immediate: true },
+  )
+
+  return readonly(amendingLawHtml)
+}
