9f930e0289f594adb3e4d7123b10dda7912ef0bc
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-04-05 15:35:24 +0200

    xml should just be a string, no need to be reactive
    RISDEV-3698

--- frontend/src/services/lawService.ts
+++ frontend/src/services/lawService.ts
@@ -1,8 +1,6 @@
 import { apiFetch } from "@/services/apiService"
-import { MaybeRefOrGetter } from "vue"
-
 export async function renderHtmlLaw(
-  xml: MaybeRefOrGetter<string | undefined>,
+  xml: string | undefined,
   showMetadata: boolean = true,
 ): Promise<string> {
   return await apiFetch(`laws/rendering?showMetadata=${showMetadata}`, {
