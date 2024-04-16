1d3c3d77955955d271296b4cb9d63abb06da286c
Sebastian Rossa <sebastian.rossa@digitalservice.bund.de>
2024-04-16 17:17:33 +0200

    Cleanup
    RISDEV-3652

--- backend/src/main/java/de/bund/digitalservice/ris/norms/domain/entity/Norm.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/domain/entity/Norm.java
@@ -4,8 +4,6 @@ import de.bund.digitalservice.ris.norms.domain.exceptions.XmlProcessingException
 import java.io.StringWriter;
 import java.time.LocalDate;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
@@ -50,7 +48,7 @@ public class Norm {
     return getValueFromExpression(
             "//FRBRExpression/FRBRalias[@name='aktuelle-version-id']/@value", document)
         .flatMap(
-            (guid) -> {
+            guid -> {
               try {
                 return Optional.of(UUID.fromString(guid));
               } catch (IllegalArgumentException e) {
@@ -69,10 +67,11 @@ public class Norm {
     Optional<String> announcementGazetteRaw =
         getValueFromExpression("//FRBRWork/FRBRname/@value", document);
 
-    Optional<String> result = announcementGazetteRaw.map(s -> s.replace("bgbl-", "BGBl. "));
-
-    result = replaceEditionNumberWithRomanNumber(result);
-    return result;
+    return announcementGazetteRaw.map(
+        s ->
+            s.replace("bgbl-1", "BGBl. I")
+                .replace("bgbl-2", "BGBl. II")
+                .replace("bgbl-3", "BGBl. III"));
   }
 
   //  public String getDigitalAnnouncementMedium() {
@@ -202,84 +201,6 @@ public class Norm {
       throw new XmlProcessingException(e.getMessage(), e);
     }
     return Optional.of(result);
-  }
-
-  private Optional<String> replaceEditionNumberWithRomanNumber(
-      Optional<String> stringWithNumberAtEnd) {
-    Optional<Integer> extractedNumber = stringWithNumberAtEnd.flatMap(this::extractNumberAtEnd);
-    Optional<String> romanNumber = extractedNumber.map(this::arabicToRoman);
-    if (romanNumber.isPresent()) {
-      stringWithNumberAtEnd =
-          stringWithNumberAtEnd.map(
-              s -> s.replace(extractedNumber.get().toString(), romanNumber.get()));
-    }
-    return stringWithNumberAtEnd;
-  }
-
-  private Optional<Integer> extractNumberAtEnd(String str) {
-    Pattern pattern = Pattern.compile("\\d{1,3}$");
-    Matcher matcher = pattern.matcher(str);
-
-    if (matcher.find()) {
-      return Optional.of(Integer.parseInt(matcher.group()));
-    } else {
-      return Optional.empty();
-    }
-  }
-
-  private String arabicToRoman(int number) {
-    if ((number <= 0) || (number > 4000)) {
-      throw new IllegalArgumentException(number + " is not in range (0,4000]");
-    }
-
-    List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
-
-    int i = 0;
-    StringBuilder sb = new StringBuilder();
-
-    while ((number > 0) && (i < romanNumerals.size())) {
-      RomanNumeral currentSymbol = romanNumerals.get(i);
-      if (currentSymbol.getValue() <= number) {
-        sb.append(currentSymbol.name());
-        number -= currentSymbol.getValue();
-      } else {
-        i++;
-      }
-    }
-
-    return sb.toString();
-  }
-
-  private enum RomanNumeral {
-    I(1),
-    IV(4),
-    V(5),
-    IX(9),
-    X(10),
-    XL(40),
-    L(50),
-    XC(90),
-    C(100),
-    CD(400),
-    D(500),
-    CM(900),
-    M(1000);
-
-    private final int value;
-
-    RomanNumeral(int value) {
-      this.value = value;
-    }
-
-    public int getValue() {
-      return value;
-    }
-
-    public static List<RomanNumeral> getReverseSortedValues() {
-      return Arrays.stream(values())
-          .sorted(Comparator.comparing((RomanNumeral e) -> e.value).reversed())
-          .toList();
-    }
   }
 
   @Override
