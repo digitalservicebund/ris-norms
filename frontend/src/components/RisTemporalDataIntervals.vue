2af1e13a2fceeb3a9d1d7d3d805dc85673d545f6
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-04-17 16:35:23 +0200

    Adds sort button
    RISDEV-3750

--- frontend/src/components/RisTemporalDataIntervals.vue
+++ frontend/src/components/RisTemporalDataIntervals.vue
@@ -3,6 +3,7 @@ import { defineModel, ref, watch } from "vue"
 import RisDateInput from "@/components/controls/RisDateInput.vue"
 import RisTextButton from "@/components/controls/RisTextButton.vue"
 import DeleteOutlineIcon from "~icons/ic/outline-delete"
+import SortOutlineIcon from "~icons/ic/outline-arrow-downward"
 import dayjs from "dayjs"
 
 const dates = defineModel("dates", {
@@ -31,45 +32,53 @@ watch(
 </script>
 
 <template>
-  <form class="p-10">
-    <div class="flex flex-col space-y-4">
-      <div v-for="(date, index) in dates" :key="date" class="mb-4">
-        <div class="p-1o flex items-center justify-between">
+  <div class="p-10">
+    <div class="mr-[2rem] flex justify-end">
+      <RisTextButton
+        :icon="SortOutlineIcon"
+        :label="`Nach Datum sortieren`"
+        variant="ghost"
+      />
+    </div>
+    <form class="p-10">
+      <div class="flex flex-col space-y-4">
+        <div v-for="(date, index) in dates" :key="date" class="mb-4">
+          <div class="p-1o flex items-center justify-between">
+            <!-- eslint-disable-next-line vuejs-accessibility/label-has-for -->
+            <label :for="'date-' + index" class="w-96 flex-none"
+              >Zeitgrenze {{ index + 1 }}</label
+            >
+            <RisDateInput
+              :id="'date-' + index"
+              v-model="dates[index]"
+              size="small"
+            />
+            <RisTextButton
+              :icon="DeleteOutlineIcon"
+              variant="ghost"
+              class="shrink-0"
+              label="Löschen"
+              type="button"
+              icon-only
+              @click="removeDateInput(index, $event)"
+            />
+          </div>
+        </div>
+        <div class="flex flex-1 items-center">
           <!-- eslint-disable-next-line vuejs-accessibility/label-has-for -->
-          <label :for="'date-' + index" class="w-96 flex-none"
-            >Zeitgrenze {{ index + 1 }}</label
+          <label :for="'new-date'" class="w-96 flex-none"
+            >Zeitgrenze hinzufügen</label
           >
           <RisDateInput
-            :id="'date-' + index"
-            v-model="dates[index]"
-            :aria-label="'Date Input'"
+            id="new-date"
+            v-model="newDate"
             size="small"
-            is-read-only
-          />
-          <RisTextButton
-            :icon="DeleteOutlineIcon"
-            variant="ghost"
-            class="shrink-0"
-            label="Löschen"
-            type="button"
-            icon-only
-            @click="removeDateInput(index, $event)"
+            class="w-[calc(100%-9rem)]"
           />
         </div>
       </div>
-      <div class="flex flex-1 items-center">
-        <!-- eslint-disable-next-line vuejs-accessibility/label-has-for -->
-        <label :for="'new-date'" class="w-96 flex-none"
-          >Zeitgrenze hinzufügen</label
-        >
-        <RisDateInput
-          id="new-date"
-          v-model="newDate"
-          :aria-label="`Enter new date`"
-          size="small"
-          class="w-[calc(100%-9rem)]"
-        />
-      </div>
-    </div>
-  </form>
+    </form>
+  </div>
 </template>
+
+// norms eid article refert
