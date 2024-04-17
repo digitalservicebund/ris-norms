ed7029bc8c0c068116204b0d247c46453e6a7527
Hannes <hannes.michalek@digitalservice.bund.de>
2024-04-17 11:10:01 +0200

    Upgrade spring web (# CVE-2024-22262) (#267), upload trivy result as artifact
    * Update spring-web
    
    # CVE-2024-2226
    
    * Add trivy result upload
    
    RISDEV-0000

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.spring.boot.starter.actuator)
     implementation(libs.spring.boot.starter.security)
     implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.web)
     implementation(libs.spring.boot.starter.data.jpa)
     implementation(libs.spring.boot.starter.validation)
     implementation(libs.spring.cloud.starter.kubernetes.client.config)
