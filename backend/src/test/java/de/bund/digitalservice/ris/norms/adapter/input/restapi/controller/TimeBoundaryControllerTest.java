a56f33dc2c7b37d9d32731b617e59d27453ec064
Sebastian Reck <sebastian.reck@digitalservice.bund.de>
2024-04-29 08:53:31 +0200

    Add tests for updateTimeBoundaries
    RISDEV-3743

--- backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/TimeBoundaryControllerTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/TimeBoundaryControllerTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -70,5 +71,127 @@ class TimeBoundaryControllerTest {
       verify(loadTimeBoundariesUseCase, times(1))
           .loadTimeBoundariesOfNorm(any(LoadTimeBoundariesUseCase.Query.class));
     }
+  }
+
+  @Test
+  void updateTimeBoundariesReturnsCorrectDataNoChange() throws Exception {
+    // Given
+    final String eli = "eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1";
+    final String eventRef1 =
+        """
+                  <akn:eventRef eId="meta-1_lebzykl-1_ereignis-2"
+                        GUID="7174df87-6418-4d47-ac4e-5fb87c2caa50"
+                        date="1964-09-21"
+                        source="attributsemantik-noch-undefiniert"
+                        type="generation"
+                        refersTo="inkrafttreten"/>
+            """
+            .strip();
+
+    final String timeInterval1 =
+        """
+                    <akn:timeInterval eId="meta-1_geltzeiten-1_geltungszeitgr-1_gelzeitintervall-1"
+                         GUID="6fc42c43-6598-4c95-ac57-da132b047ec1"
+                         refersTo="geltungszeit"
+                         start="#meta-1_lebzykl-1_ereignis-2"/>
+            """
+            .strip();
+
+    List<TimeBoundary> timeBoundaries =
+        List.of(new TimeBoundary(XmlMapper.toNode(timeInterval1), XmlMapper.toNode(eventRef1)));
+
+    when(updateTimeBoundariesUseCase.updateTimeBoundariesOfNorm(any())).thenReturn(timeBoundaries);
+
+    // When // Then
+    mockMvc
+        .perform(
+            put("/api/v1/norms/{eli}/timeBoundaries", eli)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("[{\"date\": \"1964-09-21\", \"eid\": \"meta-1_lebzykl-1_ereignis-2\"}]"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$", hasSize(1)))
+        .andExpect(jsonPath("$[0].date", is("1964-09-21")))
+        .andExpect(jsonPath("$[0].eid", is("meta-1_lebzykl-1_ereignis-2")));
+
+    verify(updateTimeBoundariesUseCase, times(1))
+        .updateTimeBoundariesOfNorm(any(UpdateTimeBoundariesUseCase.Query.class));
+  }
+
+  @Test
+  void updateTimeBoundariesReturnsCorrectDataAddTimeBoundary() throws Exception {
+    // Given
+    final String eli = "eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1";
+
+    // eventRef1 + timeInterval1 stay the same in this example. Frontend is not requesting any
+    // change.
+    final String eventRef1 =
+        """
+                  <akn:eventRef eId="meta-1_lebzykl-1_ereignis-2"
+                        GUID="7174df87-6418-4d47-ac4e-5fb87c2caa50"
+                        date="1964-09-21"
+                        source="attributsemantik-noch-undefiniert"
+                        type="generation"
+                        refersTo="inkrafttreten"/>
+            """
+            .strip();
+
+    final String timeInterval1 =
+        """
+                    <akn:timeInterval eId="meta-1_geltzeiten-1_geltungszeitgr-1_gelzeitintervall-1"
+                         GUID="6fc42c43-6598-4c95-ac57-da132b047ec1"
+                         refersTo="geltungszeit"
+                         start="#meta-1_lebzykl-1_ereignis-2"/>
+            """
+            .strip();
+
+    // eventRef2 + timeInterval2 is being newly created due to frontend added the new date
+    // 1964-09-01
+    // you can tell by the dates that it's not sorted yet
+    final String eventRef2 =
+        """
+                  <akn:eventRef eId="meta-1_lebzykl-1_ereignis-3"
+                        GUID="08308b59-4541-4643-8d55-f7950e515553"
+                        date="1964-09-01"
+                        source="attributsemantik-noch-undefiniert"
+                        type="generation"
+                        refersTo="inkrafttreten"/>
+            """
+            .strip();
+
+    final String timeInterval2 =
+        """
+                    <akn:timeInterval eId="meta-1_geltzeiten-1_geltungszeitgr-2_gelzeitintervall-1"
+                         GUID="b382ce1b-7b4f-4bb6-a878-7a37c11c3854"
+                         refersTo="geltungszeit"
+                         start="#meta-1_lebzykl-1_ereignis-3"/>
+            """
+            .strip();
+
+    List<TimeBoundary> timeBoundaries =
+        List.of(
+            new TimeBoundary(XmlMapper.toNode(timeInterval1), XmlMapper.toNode(eventRef1)),
+            new TimeBoundary(XmlMapper.toNode(timeInterval2), XmlMapper.toNode(eventRef2)));
+
+    when(updateTimeBoundariesUseCase.updateTimeBoundariesOfNorm(any())).thenReturn(timeBoundaries);
+
+    // When // Then
+    mockMvc
+        .perform(
+            put("/api/v1/norms/{eli}/timeBoundaries", eli)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    "[{\"date\": \"1964-09-21\", \"eid\": \"meta-1_lebzykl-1_ereignis-2\"},{\"date\": \"1964-09-01\", \"eid\": null}]"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$", hasSize(2)))
+        .andExpect(jsonPath("$[0].date", is("1964-09-21")))
+        .andExpect(jsonPath("$[0].eid", is("meta-1_lebzykl-1_ereignis-2")))
+        .andExpect(jsonPath("$[1].date", is("1964-09-01")))
+        .andExpect(jsonPath("$[1].eid", is("meta-1_lebzykl-1_ereignis-3")));
+    ;
+
+    verify(updateTimeBoundariesUseCase, times(1))
+        .updateTimeBoundariesOfNorm(any(UpdateTimeBoundariesUseCase.Query.class));
   }
 }
