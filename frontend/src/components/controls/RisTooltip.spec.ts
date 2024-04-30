4d1251c6b42adf98f09c5e0449be8d1646ea9b47
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2024-04-30 06:56:10 +0200

    feat: implement tooltip component
    RISDEV-3800

---
+++ frontend/src/components/controls/RisTooltip.spec.ts
@@ -0,0 +1,87 @@
+import { describe, test } from "vitest"
+
+describe("RisTooltip", () => {
+  test.todo("renders", () => {
+    // TODO: Implement
+  })
+
+  test.todo("renders with an embedded element", () => {
+    // TODO: Implement
+  })
+
+  test.todo("displays the title", () => {
+    // TODO: Implement
+  })
+
+  test.todo("shows as neutral variant", () => {
+    // TODO: Implement
+  })
+
+  test.todo("shows as error variant", () => {
+    // TODO: Implement
+  })
+
+  test.todo("shows as warning variant", () => {
+    // TODO: Implement
+  })
+
+  test.todo("shows as success variant", () => {
+    // TODO: Implement
+  })
+
+  test.todo("shows as neutral variant by default", () => {
+    // TODO: Implement
+  })
+
+  test.todo("aligns left", () => {
+    // TODO: Implement
+  })
+
+  test.todo("aligns right", () => {
+    // TODO: Implement
+  })
+
+  test.todo("aligns left by default", () => {
+    // TODO: Implement
+  })
+
+  test.todo("attaches top", () => {
+    // TODO: Implement
+  })
+
+  test.todo("attaches bottom", () => {
+    // TODO: Implement
+  })
+
+  test.todo("attaches top by default", () => {
+    // TODO: Implement
+  })
+
+  test.todo("shows a dimiss button", () => {
+    // TODO: Implement
+  })
+
+  test.todo("doesn't show the dismiss button", () => {
+    // TODO: Implement
+  })
+
+  test.todo("doesn't show the dismiss button by default", () => {
+    // TODO: Implement
+  })
+
+  test.todo("is hidden on dismiss", () => {
+    // TODO: Implement
+  })
+
+  test.todo("is visible based on the model value", () => {
+    // TODO: Implement
+  })
+
+  test.todo("is hidden based on the model value", () => {
+    // TODO: Implement
+  })
+
+  test.todo("is visible by default", () => {
+    // TODO: Implement
+  })
+})
