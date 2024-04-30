b21947c29df93e7d3345340c6d729e56fe830142
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2024-04-30 08:36:18 +0200

    test: missing unit tests
    RISDEV-3800

--- frontend/src/components/controls/RisTooltip.spec.ts
+++ frontend/src/components/controls/RisTooltip.spec.ts
@@ -1,87 +1,167 @@
-import { describe, test } from "vitest"
+import { userEvent } from "@testing-library/user-event"
+import { render, screen } from "@testing-library/vue"
+import { describe, expect, test, vi } from "vitest"
+import { defineComponent } from "vue"
+import RisTooltip from "./RisTooltip.vue"
 
 describe("RisTooltip", () => {
-  test.todo("renders", () => {
-    // TODO: Implement
+  test("renders", () => {
+    render(RisTooltip, { props: { title: "Foo" } })
+    expect(screen.getByRole("tooltip")).toBeInTheDocument()
   })
 
-  test.todo("renders with an embedded element", () => {
-    // TODO: Implement
+  test("renders with an embedded element", () => {
+    const component = defineComponent({
+      components: { RisTooltip },
+      template: `
+        <RisTooltip title="Foo">
+          <button>Bar</button>
+        </RisTooltip>
+      `,
+    })
+    render(component)
+
+    expect(screen.getByRole("button", { name: "Bar" })).toBeInTheDocument()
   })
 
-  test.todo("displays the title", () => {
-    // TODO: Implement
+  test("displays the title", () => {
+    render(RisTooltip, { props: { title: "Foo" } })
+    expect(screen.getByRole("tooltip", { name: "Foo" })).toBeInTheDocument()
   })
 
-  test.todo("shows as neutral variant", () => {
-    // TODO: Implement
+  test("shows as neutral variant", () => {
+    render(RisTooltip, { props: { title: "Foo", variant: "neutral" } })
+    expect(screen.getByTestId("callout-wrapper")).toHaveAttribute(
+      "data-variant",
+      "neutral",
+    )
   })
 
-  test.todo("shows as error variant", () => {
-    // TODO: Implement
+  test("shows as error variant", () => {
+    render(RisTooltip, { props: { title: "Foo", variant: "error" } })
+    expect(screen.getByTestId("callout-wrapper")).toHaveAttribute(
+      "data-variant",
+      "error",
+    )
   })
 
-  test.todo("shows as warning variant", () => {
-    // TODO: Implement
+  test("shows as warning variant", () => {
+    render(RisTooltip, { props: { title: "Foo", variant: "warning" } })
+    expect(screen.getByTestId("callout-wrapper")).toHaveAttribute(
+      "data-variant",
+      "warning",
+    )
   })
 
-  test.todo("shows as success variant", () => {
-    // TODO: Implement
+  test("shows as success variant", () => {
+    render(RisTooltip, { props: { title: "Foo", variant: "success" } })
+    expect(screen.getByTestId("callout-wrapper")).toHaveAttribute(
+      "data-variant",
+      "success",
+    )
   })
 
-  test.todo("shows as neutral variant by default", () => {
-    // TODO: Implement
+  test("shows as neutral variant by default", () => {
+    render(RisTooltip, { props: { title: "Foo" } })
+    expect(screen.getByTestId("callout-wrapper")).toHaveAttribute(
+      "data-variant",
+      "neutral",
+    )
   })
 
-  test.todo("aligns left", () => {
-    // TODO: Implement
+  test("aligns left", () => {
+    render(RisTooltip, { props: { title: "Foo", alignment: "left" } })
+    expect(screen.getByRole("tooltip")).toHaveAttribute(
+      "data-alignment",
+      "left",
+    )
   })
 
-  test.todo("aligns right", () => {
-    // TODO: Implement
+  test("aligns right", () => {
+    render(RisTooltip, { props: { title: "Foo", alignment: "right" } })
+    expect(screen.getByRole("tooltip")).toHaveAttribute(
+      "data-alignment",
+      "right",
+    )
   })
 
-  test.todo("aligns left by default", () => {
-    // TODO: Implement
+  test("aligns left by default", () => {
+    render(RisTooltip, { props: { title: "Foo" } })
+    expect(screen.getByRole("tooltip")).toHaveAttribute(
+      "data-alignment",
+      "left",
+    )
   })
 
-  test.todo("attaches top", () => {
-    // TODO: Implement
+  test("attaches top", () => {
+    render(RisTooltip, { props: { title: "Foo", attachment: "top" } })
+    expect(screen.getByRole("tooltip")).toHaveAttribute(
+      "data-attachment",
+      "top",
+    )
   })
 
-  test.todo("attaches bottom", () => {
-    // TODO: Implement
+  test("attaches bottom", () => {
+    render(RisTooltip, { props: { title: "Foo", attachment: "bottom" } })
+    expect(screen.getByRole("tooltip")).toHaveAttribute(
+      "data-attachment",
+      "bottom",
+    )
   })
 
-  test.todo("attaches top by default", () => {
-    // TODO: Implement
+  test("attaches top by default", () => {
+    render(RisTooltip, { props: { title: "Foo" } })
+    expect(screen.getByRole("tooltip")).toHaveAttribute(
+      "data-attachment",
+      "top",
+    )
   })
 
-  test.todo("shows a dimiss button", () => {
-    // TODO: Implement
+  test("shows a dimiss button", () => {
+    render(RisTooltip, { props: { title: "Foo", allowDismiss: true } })
+    expect(
+      screen.getByRole("button", { name: "Hinweis schließen" }),
+    ).toBeInTheDocument()
   })
 
-  test.todo("doesn't show the dismiss button", () => {
-    // TODO: Implement
+  test("doesn't show the dismiss button", () => {
+    render(RisTooltip, { props: { title: "Foo", allowDismiss: false } })
+    expect(
+      screen.queryByRole("button", { name: "Hinweis schließen" }),
+    ).not.toBeInTheDocument()
   })
 
-  test.todo("doesn't show the dismiss button by default", () => {
-    // TODO: Implement
+  test("doesn't show the dismiss button by default", () => {
+    render(RisTooltip, { props: { title: "Foo", allowDismiss: false } })
+    expect(
+      screen.queryByRole("button", { name: "Hinweis schließen" }),
+    ).not.toBeInTheDocument()
   })
 
-  test.todo("is hidden on dismiss", () => {
-    // TODO: Implement
+  test("is hidden on dismiss", async () => {
+    const update = vi.fn()
+    render(RisTooltip, {
+      props: { title: "Foo", allowDismiss: true, "onUpdate:visible": update },
+    })
+
+    const user = userEvent.setup()
+    await user.click(screen.getByRole("button", { name: "Hinweis schließen" }))
+
+    expect(update).toHaveBeenCalled()
   })
 
-  test.todo("is visible based on the model value", () => {
-    // TODO: Implement
+  test("is visible based on the model value", () => {
+    render(RisTooltip, { props: { title: "Foo", visible: true } })
+    expect(screen.getByRole("tooltip")).toBeVisible()
   })
 
-  test.todo("is hidden based on the model value", () => {
-    // TODO: Implement
+  test("is hidden based on the model value", () => {
+    render(RisTooltip, { props: { title: "Foo", visible: false } })
+    expect(screen.queryByRole("tooltip")).not.toBeInTheDocument()
   })
 
-  test.todo("is visible by default", () => {
-    // TODO: Implement
+  test("is visible by default", () => {
+    render(RisTooltip, { props: { title: "Foo" } })
+    expect(screen.getByRole("tooltip")).toBeVisible()
   })
 })
