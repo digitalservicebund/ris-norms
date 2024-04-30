81a0911a5a2bff30d17388dfc598609281955f60
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2024-04-30 06:56:10 +0200

    feat: implement tooltip component
    RISDEV-3800

---
+++ frontend/src/components/controls/RisTooltip.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import { computed, useSlots } from "vue"
+import RisCallout from "./RisCallout.vue"
+
+withDefaults(
+  defineProps<{
+    /** Title of the tooltip. */
+    title: string
+
+    /**
+     * Visual variant of the tooltip.
+     * @default "neutral"
+     */
+    variant: "neutral" | "error" | "warning" | "success"
+
+    /**
+     * Whether the tooltip is aligned to the left or the right of the available
+     * container. This manages the positioning of the arrow and the tooltip box.
+     * @default "left"
+     */
+    alignment: "left" | "right"
+
+    /**
+     * Whether the tooltip is positioned above or below the element in the slot.
+     * This also manages the direction of the arrow.
+     * @default "top"
+     */
+    attachment: "top" | "bottom"
+
+    /**
+     * Shows an icon to hide the callout.
+     * @default false
+     */
+    allowDismiss: boolean
+  }>(),
+  {
+    variant: "neutral",
+    alignment: "left",
+    attachment: "top",
+    allowDismiss: false,
+  },
+)
+
+const slots = useSlots()
+
+const hasChildren = computed(() => Boolean(slots.default))
+
+const visible = defineModel<boolean>("visible", { default: true })
+</script>
+
+<template>
+  <span>
+    <span
+      v-if="visible"
+      :data-with-children="hasChildren || undefined"
+      :data-alignment="alignment"
+      :data-attachment="attachment"
+      class="group absolute inline-block data-[alignment=left]:left-0 data-[alignment=right]:right-0 data-[attachment=bottom]:translate-y-full data-[attachment=top]:-translate-y-[calc(100%+8px)]"
+    >
+      <RisCallout
+        :title
+        :variant
+        :allow-dismiss
+        visible
+        @update:visible="visible = false"
+      />
+      <span
+        :data-variant="variant"
+        class="absolute inline-block border-8 border-x-transparent data-[variant=error]:border-y-red-200 data-[variant=neutral]:border-y-blue-200 data-[variant=success]:border-y-green-200 data-[variant=warning]:border-y-yellow-200 group-data-[alignment=left]:left-24 group-data-[alignment=right]:right-20 group-data-[attachment=bottom]:-top-8 group-data-[attachment=bottom]:border-t-0 group-data-[attachment=top]:border-b-0"
+      ></span>
+    </span>
+
+    <template v-if="$slots.default">
+      <slot />
+    </template>
+  </span>
+</template>
