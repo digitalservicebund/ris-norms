d2f9775317304adc898548397520d2c8f8f9d8a3
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-05-06 08:54:18 +0200

    Update e2e tests
    Adds test for editing
    Joins tests for adding, editing, deleting together (for now)
    
    RISDEV-3848

--- frontend/e2e/navigate-to-temporal-data-page-and-test-content.spec.ts
+++ frontend/e2e/navigate-to-temporal-data-page-and-test-content.spec.ts
@@ -1,20 +1,19 @@
 import { test, expect } from "@playwright/test"
 
-test("navigate to temporal data page for an amending law", async ({ page }) => {
-  await page.goto(
-    "/amending-laws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/temporal-data",
-  )
-  await expect(
-    page.locator('[data-testid="temporalDataHeading"]'),
-  ).toBeVisible()
-})
-
 test.describe("Temporal Data for an amending law", () => {
   const BASE_URL =
     "/amending-laws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/temporal-data"
 
   test.beforeEach(async ({ page }) => {
     await page.goto(BASE_URL)
+  })
+
+  test("navigate to temporal data page for an amending law", async ({
+    page,
+  }) => {
+    await expect(
+      page.locator('[data-testid="temporalDataHeading"]'),
+    ).toBeVisible()
   })
 
   test(`contents of entry into force article html rendering`, async ({
@@ -32,29 +31,36 @@ test.describe("Temporal Data for an amending law", () => {
     await expect(inputValue).toBe("16.03.2017")
   })
 
-  test("can add and save multiple new time boundaries", async ({ page }) => {
+  test("can add, edit, delete and save time boundaries", async ({ page }) => {
+    // adding new time boundaries
     const newDateInput = page.locator('[data-testid="new-date-input-field"]')
     await newDateInput.fill("01-05-2023")
     await newDateInput.fill("02-06-2023")
-    await newDateInput.fill("03-07-2023")
 
     const saveButton = page.locator("text=Speichern")
+
+    await saveButton.click()
+
+    await page.reload()
+
+    let dateInputs = page.locator('[data-testid="date-input-field"]')
+    await expect(dateInputs).toHaveCount(3)
+
+    // Editing the time boundaries
+    const dateInputToEdit = page
+      .locator('[data-testid="date-input-field"]')
+      .nth(1)
+    await dateInputToEdit.fill("")
+    await dateInputToEdit.fill("03.06.2023")
     await saveButton.click()
     await page.reload()
 
-    const dateInputs = page.locator('[data-testid="date-input-field"]')
-    await expect(dateInputs).toHaveCount(4)
-  })
+    // Verifying the edited value
+    const editedValue = await dateInputs.nth(1).inputValue()
+    await expect(editedValue).toBe("03.06.2023")
 
-  test("can delete a date input and verify the remaining inputs", async ({
-    page,
-  }) => {
-    let dateInputs = page.locator('[data-testid="date-input-field"]')
-    const saveButton = page.locator("text=Speichern")
-
-    await expect(dateInputs).toHaveCount(4)
-
-    for (let i = 3; i > 0; i--) {
+    // deletion of the time boundaries
+    for (let i = 2; i > 0; i--) {
       const deleteButton = page.locator(`[data-testid="delete-button-${i}"]`)
       await deleteButton.click()
       await saveButton.click()
