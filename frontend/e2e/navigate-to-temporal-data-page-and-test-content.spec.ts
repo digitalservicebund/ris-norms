0f9ade9b9054db9073e5dc8a02d026832de80103
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-05-09 21:23:58 +0200

    Adjusts e2e
    RISDEV-3848

--- frontend/e2e/navigate-to-temporal-data-page-and-test-content.spec.ts
+++ frontend/e2e/navigate-to-temporal-data-page-and-test-content.spec.ts
@@ -1,34 +1,36 @@
 import { test, expect } from "@playwright/test"
 
-test.describe("Navigate to temporal data page and test its rendered content", () => {
-  test.beforeEach(async ({ page }) => {
+test.describe("Navigate to temporal data page", () => {
+  test("navigate to temporal data page for an amending law using side navigation", async ({
+    page,
+  }) => {
     await page.goto(
-      "/amending-laws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/temporal-data",
+      "/amending-laws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1",
     )
-  })
 
-  test("navigate to temporal data page for an amending law", async ({
-    page,
-  }) => {
+    await page.getByText("Zeitgrenzen anlegen").click()
+
+    await expect(page).toHaveURL(
+      `/amending-laws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/temporal-data`,
+    )
+
     await expect(
-      page.locator('[data-testid="temporalDataHeading"]'),
+      page.getByRole("heading", { level: 1, name: "Zeitgrenzen anlegen" }),
     ).toBeVisible()
-  })
-
-  test(`contents of entry into force article html rendering`, async ({
-    page,
-  }) => {
-    await expect(page.getByText("Artikel 3Inkrafttreten")).toBeVisible()
   })
 })
 
 test.describe("management of Temporal Data for an amending law", () => {
-  test(`renders correct number of date inputs for the time boundaries`, async ({
+  test(`adding, editing, deleting and saving time boundaries`, async ({
     page,
   }) => {
     await page.goto(
       "/amending-laws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/temporal-data",
     )
+
+    // check contents of entry into force article html rendering
+    await expect(page.getByText("Artikel 3Inkrafttreten")).toBeVisible()
+
     const saveButton = page.locator("text=Speichern")
     let dateInputs = page.locator('[data-testid="date-input-field"]')
 
@@ -47,53 +49,18 @@ test.describe("management of Temporal Data for an amending law", () => {
         response.url().includes("timeBoundaries") && response.status() === 200,
     )
 
-    const addTimeBoundariesResponse = await page.request.get(
-      `/api/v1/norms/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/timeBoundaries`,
-      {
-        headers: {
-          Accept: "application/json",
-        },
-      },
-    )
+    await page.reload()
 
-    const addedTimeBoundaries = await addTimeBoundariesResponse.json()
-
-    expect(addedTimeBoundaries).toEqual([
-      { date: "2017-03-16", eventRefEid: "meta-1_lebzykl-1_ereignis-2" },
-      { date: "2023-05-01", eventRefEid: "meta-1_lebzykl-1_ereignis-3" },
-      { date: "2023-06-02", eventRefEid: "meta-1_lebzykl-1_ereignis-4" },
-    ])
-
+    await expect(dateInputs).toHaveCount(3)
     //edit time boundaries
 
     await expect(dateInputs).toHaveCount(3)
     const dateInputToEdit = page
       .locator('[data-testid="date-input-field"]')
       .nth(1)
-    await dateInputToEdit.fill("")
     await dateInputToEdit.fill("03.06.2023")
     await saveButton.click()
-
-    await page.waitForResponse(
-      (response) =>
-        response.url().includes("timeBoundaries") && response.status() === 200,
-    )
-
-    const editedTimeboundariesResponse = await page.request.get(
-      `/api/v1/norms/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/timeBoundaries`,
-      {
-        headers: {
-          Accept: "application/json",
-        },
-      },
-    )
-    const editedTimeboundaries = await editedTimeboundariesResponse.json()
-
-    expect(editedTimeboundaries).toEqual([
-      { date: "2017-03-16", eventRefEid: "meta-1_lebzykl-1_ereignis-2" },
-      { date: "2023-06-03", eventRefEid: "meta-1_lebzykl-1_ereignis-3" },
-      { date: "2023-06-02", eventRefEid: "meta-1_lebzykl-1_ereignis-4" },
-    ])
+    await page.reload()
 
     const editedValue = await dateInputs.nth(1).inputValue()
     await expect(editedValue).toBe("03.06.2023")
@@ -108,20 +75,6 @@ test.describe("management of Temporal Data for an amending law", () => {
       dateInputs = page.locator('[data-testid="date-input-field"]')
       await expect(dateInputs).toHaveCount(i)
     }
-
-    const deletedTimeBoundariesResponse = await page.request.get(
-      `/api/v1/norms/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/timeBoundaries`,
-      {
-        headers: {
-          Accept: "application/json",
-        },
-      },
-    )
-    const timeboundaries = await deletedTimeBoundariesResponse.json()
-
-    expect(timeboundaries).toEqual([
-      { date: "2017-03-16", eventRefEid: "meta-1_lebzykl-1_ereignis-2" },
-    ])
 
     const deleteButton = page.locator(`[data-testid="delete-button-0"]`)
     const isDisabled = await deleteButton.isDisabled()
