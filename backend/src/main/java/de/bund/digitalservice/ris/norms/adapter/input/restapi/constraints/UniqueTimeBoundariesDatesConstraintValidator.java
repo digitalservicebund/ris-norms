652deff7f86e390f7389b4fd289fdb61e90c12fe
Sebastian Reck <sebastian.reck@digitalservice.bund.de>
2024-05-10 13:41:19 +0200

    Simplify TB Unique constraint to not use a HashMap
    RISDEV-3923

--- backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/constraints/UniqueTimeBoundariesDatesConstraintValidator.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/constraints/UniqueTimeBoundariesDatesConstraintValidator.java
@@ -3,7 +3,6 @@ package de.bund.digitalservice.ris.norms.adapter.input.restapi.constraints;
 import de.bund.digitalservice.ris.norms.adapter.input.restapi.schema.TimeBoundarySchema;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import java.util.HashSet;
 import java.util.List;
 
 /** Validates if TimeBoundary dates are unique */
@@ -12,7 +11,7 @@ public class UniqueTimeBoundariesDatesConstraintValidator
   @Override
   public boolean isValid(
       List<TimeBoundarySchema> timeBoundaries, ConstraintValidatorContext context) {
-    return new HashSet<>(timeBoundaries.stream().map(TimeBoundarySchema::getDate).toList()).size()
+    return timeBoundaries.stream().map(TimeBoundarySchema::getDate).distinct().count()
         == timeBoundaries.size();
   }
 }
