368e1484bb63a74a541f19847f710be9f8b3d35b
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-05-13 10:23:18 +0200

    Updates e2e tests
    - removes await with toBe assertion as does not return a promise
    
    - removed the waitForResponse as not needed
    
    - created a setup function that to be used at the start and finish of the test
    
    RISDEV-3848

--- frontend/e2e/navigate-to-temporal-data-page-and-test-content.spec.ts
+++ frontend/e2e/navigate-to-temporal-data-page-and-test-content.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test"
+import { test, expect, Page } from "@playwright/test"
 
 test.describe("Navigate to temporal data page", () => {
   test("navigate to temporal data page for an amending law using side navigation", async ({
@@ -21,12 +21,25 @@ test.describe("Navigate to temporal data page", () => {
 })
 
 test.describe("management of Temporal Data for an amending law", () => {
+  async function setupInitialData(page: Page) {
+    await page.request.put(
+      "/api/v1/norms/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/timeBoundaries",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        data: JSON.stringify([
+          { date: "2017-03-16", eventRefEid: "meta-1_lebzykl-1_ereignis-2" },
+        ]),
+      },
+    )
+  }
+
   test(`adding, editing, deleting and saving time boundaries`, async ({
     page,
   }) => {
-    await page.goto(
-      "/amending-laws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/temporal-data",
-    )
+    await setupInitialData(page)
 
     // check contents of entry into force article html rendering
     await expect(page.getByText("Artikel 3Inkrafttreten")).toBeVisible()
@@ -36,25 +49,27 @@ test.describe("management of Temporal Data for an amending law", () => {
 
     await expect(dateInputs).toHaveCount(1)
     const inputValue = await dateInputs.inputValue()
-    await expect(inputValue).toBe("16.03.2017")
+    expect(inputValue).toBe("16.03.2017")
 
     //   add new time boundaries
     const newDateInput = page.locator('[data-testid="new-date-input-field"]')
-    await newDateInput.fill("01-05-2023")
-    await newDateInput.fill("02-06-2023")
+    await newDateInput.fill("01.05.2023")
+    await newDateInput.fill("02.06.2023")
     await saveButton.click()
 
-    await page.waitForResponse(
-      (response) =>
-        response.url().includes("timeBoundaries") && response.status() === 200,
-    )
-
     await page.reload()
-
     await expect(dateInputs).toHaveCount(3)
+
+    const firstDateValue = await dateInputs.nth(0).inputValue()
+    const secondDateValue = await dateInputs.nth(1).inputValue()
+    const thirdDateValue = await dateInputs.nth(2).inputValue()
+
+    // validate each date input contains the correct data
+    expect(firstDateValue).toBe("16.03.2017")
+    expect(secondDateValue).toBe("01.05.2023")
+    expect(thirdDateValue).toBe("02.06.2023")
+
     //edit time boundaries
-
-    await expect(dateInputs).toHaveCount(3)
     const dateInputToEdit = page
       .locator('[data-testid="date-input-field"]')
       .nth(1)
@@ -63,10 +78,9 @@ test.describe("management of Temporal Data for an amending law", () => {
     await page.reload()
 
     const editedValue = await dateInputs.nth(1).inputValue()
-    await expect(editedValue).toBe("03.06.2023")
+    expect(editedValue).toBe("03.06.2023")
 
     //   delete time boundaries
-
     for (let i = 2; i > 0; i--) {
       const deleteButton = page.locator(`[data-testid="delete-button-${i}"]`)
       await deleteButton.click()
@@ -79,18 +93,8 @@ test.describe("management of Temporal Data for an amending law", () => {
     const deleteButton = page.locator(`[data-testid="delete-button-0"]`)
     const isDisabled = await deleteButton.isDisabled()
     expect(isDisabled).toBe(true)
+    expect(inputValue).toBe("16.03.2017")
 
-    await page.request.put(
-      `/api/v1/norms/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/timeBoundaries`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        data: JSON.stringify([
-          { date: "2017-03-16", eventRefEid: "meta-1_lebzykl-1_ereignis-2" },
-        ]),
-      },
-    )
+    await setupInitialData(page)
   })
 })
