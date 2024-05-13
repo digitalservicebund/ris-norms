f2c43757f3c652d0cf9fc27f8439fa9ecc45b403
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-05-13 11:59:20 +0200

    Adds unit test for RisTextInput
    RISDEV-3920

---
+++ frontend/src/components/controls/RisTextInput.spec.ts
@@ -0,0 +1,108 @@
+import { userEvent } from "@testing-library/user-event"
+import { render, screen } from "@testing-library/vue"
+import { describe, expect, test, vi } from "vitest"
+import { defineComponent } from "vue"
+import RisTextInput from "@/components/controls/RisTextInput.vue"
+
+function renderComponent(options?: {
+  modelValue?: string
+  placeholder?: string
+  readOnly?: boolean
+  size?: string
+}) {
+  const user = userEvent.setup()
+  const props = {
+    id: "identifier",
+    modelValue: options?.modelValue,
+    placeholder: options?.placeholder,
+    readOnly: options?.readOnly,
+    size: options?.size,
+  }
+  const utils = render(RisTextInput, { props })
+  return { user, props, ...utils }
+}
+
+describe("TextInput", () => {
+  test("shows an text input element", () => {
+    renderComponent()
+    const input = screen.queryByRole("textbox")
+    expect(input).toBeInTheDocument()
+  })
+
+  test("shows the value", () => {
+    renderComponent({ modelValue: "test" })
+    const input: HTMLInputElement = screen.getByRole("textbox")
+    expect(input).toHaveValue("test")
+  })
+
+  test("shows input with a placeholder", () => {
+    renderComponent({ placeholder: "Test Placeholder" })
+    const input = screen.queryByPlaceholderText("Test Placeholder")
+    expect(input).toBeInTheDocument()
+  })
+
+  test("emits input events when user types into input", async () => {
+    const handleInput = vi.fn()
+
+    const withInputEvent = defineComponent({
+      components: { RisTextInput },
+      data: () => ({ value: "" }),
+      methods: { handleInput },
+      template: `
+        <RisTextInput
+          id="identifier"
+          v-model="value"
+          @input="handleInput"
+        />`,
+    })
+
+    render(withInputEvent)
+    const input = screen.getByRole("textbox")
+
+    await userEvent.type(input, "ab")
+    expect(handleInput).toHaveBeenCalledTimes(2)
+    expect(handleInput).toHaveBeenNthCalledWith(1, expect.any(InputEvent))
+    expect(handleInput).toHaveBeenNthCalledWith(2, expect.any(InputEvent))
+  })
+
+  test("emits model update event when user types into input", async () => {
+    const { emitted } = renderComponent()
+    const input = screen.getByRole("textbox")
+    await userEvent.type(input, "a")
+    expect(emitted("update:modelValue")).toEqual([["a"]])
+  })
+
+  test("renders a read-only input", () => {
+    renderComponent({ readOnly: true })
+    const input = screen.getByRole("textbox")
+    expect(input).toHaveAttribute("readonly")
+  })
+
+  test("renders the regular variant by default", () => {
+    renderComponent()
+    const input = screen.getByRole("textbox")
+    expect(input).not.toHaveClass("ds-input-medium")
+    expect(input).not.toHaveClass("ds-input-small")
+  })
+
+  test("renders the regular variant", () => {
+    renderComponent({ size: "regular" })
+    const input = screen.getByRole("textbox")
+    expect(input).not.toHaveClass("ds-input-medium")
+    expect(input).not.toHaveClass("ds-input-small")
+  })
+
+  test("renders the medium variant", () => {
+    renderComponent({ size: "medium" })
+    const input = screen.getByRole("textbox")
+    expect(input).toHaveClass("ds-input-medium")
+    expect(input).not.toHaveClass("ds-input-small")
+  })
+
+  test("renders the small variant", () => {
+    renderComponent({ size: "small" })
+    const input = screen.getByRole("textbox")
+    expect(input).not.toHaveClass("ds-input-medium")
+    expect(input).toHaveClass("ds-input-small")
+  })
+})
