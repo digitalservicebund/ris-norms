d0b54d0dd3382d82962b21fdf4bfd7e269c24289
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-05-14 08:18:57 +0200

    Red test on returning JSON
    RISDEV-3890

--- backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ListControllerIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ListControllerIntegrationTest.java
@@ -27,5 +27,21 @@ class ListControllerIntegrationTest extends BaseIntegrationTest {
           // then
           .andExpect(status().isOk());
     }
+
+    @Test
+    void itReturnsJson() throws Exception {
+      // given
+      var norm = NormFixtures.loadFromDisk("NormWithMultiplePassiveModifications.xml");
+      // when
+      mockMvc
+              .perform(
+                      get("/api/v1/norms/eli/bund/bgbl-1/2023/413/2023-12-29/1/deu/regelungstext-1/list"))
+              // then
+              .andExpect(status().isOk()) // TODO Hannnes: Once this test works, we can remove the one on returning 200 OK
+              .andExpect(jsonPath("$[0]").exists());
+    }
   }
+
+  // @Nested
+  // class getListWithAmendedBy{}
 }
