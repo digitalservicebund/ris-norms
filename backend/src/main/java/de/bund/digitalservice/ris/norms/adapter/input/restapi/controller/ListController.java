a4fc92c70359b5aeadd176deef02634b132f674e
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-05-14 08:25:33 +0200

    Return a JSON list
    RISDEV-3890

--- backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ListController.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ListController.java
@@ -5,13 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 /** Controller for norm-related list actions. */
 @RestController
 @RequestMapping(
         "/api/v1/norms/eli/bund/{agent}/{year}/{naturalIdentifier}/{pointInTime}/{version}/{language}/{subtype}/list")
 public class ListController {
-    @GetMapping
+    @GetMapping(produces = {APPLICATION_JSON_VALUE})
     public ResponseEntity getList(){
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(List.of("not implemented, yet"));
     }
 }
