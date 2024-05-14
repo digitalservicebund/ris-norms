2d3efae4146c90ed05ddf247e4c33cb18f9d86d5
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2024-05-14 15:18:34 +0200

    Create method to get activeModification by akn:mod eid
    RISDEV-3932

--- frontend/src/services/ldmldeService.ts
+++ frontend/src/services/ldmldeService.ts
@@ -1,5 +1,18 @@
 import { evaluateXPath } from "@/services/xmlService"
 
+/**
+ * Get the node with the given eId.
+ */
 export function getNodeByEid(xml: Document, eid: string) {
   return evaluateXPath(`//*[@eId="${eid}"]`, xml)
+}
+
+/**
+ * Get the node of the active modification associated with the akn:mod element of the given eId.
+ */
+export function getActiveModificationByModEid(xml: Document, modEid: string) {
+  return evaluateXPath(
+    `//akn:activeModifications/akn:textualMod[akn:source[@href="#${modEid}"]]`,
+    xml,
+  )
 }
