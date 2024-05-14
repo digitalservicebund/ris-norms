3b0d8a8be62c27c28d0abdeaf8efb25bbd596206
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-05-14 22:29:12 +0200

    Refactor: Code style
    RISDEV-0000

--- backend/src/test/java/de/bund/digitalservice/ris/norms/utils/EliBuilderTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/utils/EliBuilderTest.java
@@ -1,35 +1,28 @@
 package de.bund.digitalservice.ris.norms.utils;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-
 public class EliBuilderTest {
-    @Test
-    void itShouldReturnEli() {
-        // given
-        String agent = "agent";
-        String year = "year";
-        String naturalIdentifier = "naturalIdentifier";
-        String pointInTime = "pointInTime";
-        String version = "version";
-        String language = "language";
-        String subtype = "subtype";
+  @Test
+  void itShouldReturnEli() {
+    // given
+    String agent = "agent";
+    String year = "year";
+    String naturalIdentifier = "naturalIdentifier";
+    String pointInTime = "pointInTime";
+    String version = "version";
+    String language = "language";
+    String subtype = "subtype";
 
-        // when
-        String eli = EliBuilder.buildEli(
-                agent,
-                year,
-                naturalIdentifier,
-                pointInTime,
-                version,
-                language,
-                subtype
-        );
+    // when
+    String eli =
+        EliBuilder.buildEli(
+            agent, year, naturalIdentifier, pointInTime, version, language, subtype);
 
-        // then
-        assertEquals(
-                eli,
-                "eli/bund/agent/year/naturalIdentifier/pointInTime/version/language/subtype");
-    }
+    // then
+    Assertions.assertEquals(
+        eli, "eli/bund/agent/year/naturalIdentifier/pointInTime/version/language/subtype");
+  }
 }
