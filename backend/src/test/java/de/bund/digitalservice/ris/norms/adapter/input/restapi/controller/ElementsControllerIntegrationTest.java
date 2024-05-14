faaaab0eb1e1eee4210a6871a35163621f3d305d
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-05-14 12:49:18 +0200

    Red test on returning an article's information
    RISDEV-3890

--- backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ElementsControllerIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ElementsControllerIntegrationTest.java
@@ -3,8 +3,11 @@ package de.bund.digitalservice.ris.norms.adapter.input.restapi.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import de.bund.digitalservice.ris.norms.adapter.output.database.mapper.NormMapper;
 import de.bund.digitalservice.ris.norms.adapter.output.database.repository.NormRepository;
+import de.bund.digitalservice.ris.norms.domain.entity.NormFixtures;
 import de.bund.digitalservice.ris.norms.integration.BaseIntegrationTest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,44 +57,34 @@ class ElementsControllerIntegrationTest extends BaseIntegrationTest {
               get(
                   "/api/v1/norms/eli/bund/bgbl-1/2023/413/2023-12-29/1/deu/regelungstext-1/elements?type=article"))
           // then
-          .andExpect(
-              status()
-                  .isOk())
+          .andExpect(status().isOk())
           // 200 OK
-          .andExpect(
-              jsonPath("$[0]")
-                  .exists())
+          .andExpect(jsonPath("$[0]").exists())
           // returning a non-empty list
           .andExpect(jsonPath("$[0].title").exists())
           .andExpect(jsonPath("$[0].eid").exists())
           .andExpect(jsonPath("$[0].type").exists());
     }
 
-    //    @Test
-    //    void itReturnsEntriesWithAnArticlesInformation() throws Exception {
-    //      // given
-    //      // TODO: later we may need a norm of our own as it needs to contain intros and outro
-    //      var norm = NormFixtures.loadFromDisk("NormWithMultiplePassiveModifications.xml");
-    //      normRepository.save(NormMapper.mapToDto(norm));
-    //
-    //      // when
-    //      mockMvc
-    //          .perform(
-    //
-    // get("/api/v1/norms/eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1/elements?type=article"))
-    //          // then
-    //          .andExpect(
-    //              status()
-    //                  .isOk()) // TODO Hannnes: Once this test works, we can remove the one on
-    // returning
-    //          // 200 OK
-    //          .andExpect(
-    //              jsonPath("$[0]").exists()) // TODO Hannnes: Once this test works, we can remove
-    // the one on returning a non-empty list
-    //          .andExpect(jsonPath("$[0].title").value("§ 20"))
-    //          .andExpect(jsonPath("$[0].eid").value("hauptteil-1_para-20"))
-    //          .andExpect(jsonPath("$[0].type").value("article"));
-    //    }
+    @Test
+    void itReturnsEntriesWithAnArticlesInformation() throws Exception {
+      // given
+      var norm = NormFixtures.loadFromDisk("NormWithMultiplePassiveModifications.xml");
+      normRepository.save(NormMapper.mapToDto(norm));
+
+      // when
+      mockMvc
+          .perform(
+              get(
+                  "/api/v1/norms/eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1/elements?type=article"))
+          // then
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$[0]").exists())
+          .andExpect(jsonPath("$[0].title").value("§ 20"))
+          .andExpect(jsonPath("$[0].eid").value("hauptteil-1_para-20"))
+          .andExpect(jsonPath("$[0].type").value("article"));
+    }
+
     //  also check for intro/outro data
   }
 
