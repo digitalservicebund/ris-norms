35d519f914635450ccbecd5db21b520127054c29
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-05-16 09:55:49 +0200

    Refactor: Remove unused import
    RISDEV-3890

--- backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ElementsController.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ElementsController.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
-import org.jetbrains.annotations.NotNull;
+
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
