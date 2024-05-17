9bf1f268ac31273a552f33c848b91f06bb422ff8
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-05-17 12:11:25 +0200

    Feat: Return 200 OK
    RISDEV-3890

--- backend/src/test/java/de/bund/digitalservice/ris/norms/integration/adapter/input/restapi/NormControllerIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/integration/adapter/input/restapi/NormControllerIntegrationTest.java
@@ -683,7 +683,7 @@ class NormControllerIntegrationTest extends BaseIntegrationTest {
           .perform(
               get("/api/v1/norms/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/NONEXISTENT_EID")
                   .accept(MediaType.TEXT_HTML))
-      // then
+          // then
           .andExpect(status().isNotFound());
     }
 
@@ -695,13 +695,13 @@ class NormControllerIntegrationTest extends BaseIntegrationTest {
 
       // when
       mockMvc
-              .perform(
-                      get("/api/v1/norms/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/hauptteil-1_art-1")
-                              .accept(MediaType.TEXT_HTML))
-      // then
-              .andExpect(status().isOk());
-              // TODO: expect html
-              // TODO: expect "Änderung des Vereinsgesetzes" in text
+          .perform(
+              get("/api/v1/norms/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/hauptteil-1_art-1")
+                  .accept(MediaType.TEXT_HTML))
+          // then
+          .andExpect(status().isOk());
+      // TODO: expect html
+      // TODO: expect "Änderung des Vereinsgesetzes" in text
     }
   }
 }
