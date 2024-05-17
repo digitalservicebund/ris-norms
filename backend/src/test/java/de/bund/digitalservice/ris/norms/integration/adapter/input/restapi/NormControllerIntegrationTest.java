802ec4df90a98a8ad167e202bdc2fb645d265bd6
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-05-17 12:05:38 +0200

    Test: (red) Return 200 OK
    RISDEV-3890

--- backend/src/test/java/de/bund/digitalservice/ris/norms/integration/adapter/input/restapi/NormControllerIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/integration/adapter/input/restapi/NormControllerIntegrationTest.java
@@ -665,10 +665,10 @@ class NormControllerIntegrationTest extends BaseIntegrationTest {
       // given no norm
       // when
       mockMvc
-              .perform(
-                      get("/api/v1/norms/eli/bund/NONEXISTENT_NORM/1964/s593/1964-08-05/1/deu/regelungstext-1/hauptteil-1_art-3")
-                              .accept(MediaType.TEXT_HTML))
-              .andExpect(status().isNotFound());
+          .perform(
+              get("/api/v1/norms/eli/bund/NONEXISTENT_NORM/1964/s593/1964-08-05/1/deu/regelungstext-1/hauptteil-1_art-3")
+                  .accept(MediaType.TEXT_HTML))
+          .andExpect(status().isNotFound());
       // then
     }
 
@@ -680,11 +680,28 @@ class NormControllerIntegrationTest extends BaseIntegrationTest {
 
       // when
       mockMvc
-              .perform(
-                      get("/api/v1/norms/eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/regelungstext-1/NONEXISTENT_EID")
-                              .accept(MediaType.TEXT_HTML))
-              .andExpect(status().isNotFound());
+          .perform(
+              get("/api/v1/norms/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/NONEXISTENT_EID")
+                  .accept(MediaType.TEXT_HTML))
       // then
+          .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void returnsElementRenderedAsHtml() throws Exception {
+      // given
+      var norm = NormFixtures.loadFromDisk("NormWithMultipleMods.xml");
+      normRepository.save(NormMapper.mapToDto(norm));
+
+      // when
+      mockMvc
+              .perform(
+                      get("/api/v1/norms/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/hauptteil-1_art-1")
+                              .accept(MediaType.TEXT_HTML))
+      // then
+              .andExpect(status().isOk());
+              // TODO: expect html
+              // TODO: expect "Änderung des Vereinsgesetzes" in text
     }
   }
 }
