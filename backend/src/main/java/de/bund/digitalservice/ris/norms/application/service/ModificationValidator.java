5391128025df19c8bac7fa23ff8ce78fa1563c8a
Sebastian Reck <sebastian.reck@digitalservice.bund.de>
2024-05-23 18:55:23 +0200

    Activate modHasValidDestRangeForDestNode
    RISDEV-3966

--- backend/src/main/java/de/bund/digitalservice/ris/norms/application/service/ModificationValidator.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/application/service/ModificationValidator.java
@@ -2,11 +2,11 @@ package de.bund.digitalservice.ris.norms.application.service;
 
 import de.bund.digitalservice.ris.norms.adapter.output.database.service.DBService;
 import de.bund.digitalservice.ris.norms.application.port.output.LoadNormPort;
-import de.bund.digitalservice.ris.norms.domain.entity.Article;
-import de.bund.digitalservice.ris.norms.domain.entity.Norm;
+import de.bund.digitalservice.ris.norms.domain.entity.*;
 import de.bund.digitalservice.ris.norms.utils.exceptions.XmlProcessingException;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
 import org.springframework.stereotype.Service;
 
 /** */
@@ -67,13 +67,36 @@ public class ModificationValidator {
   }
 
   /**
+   * Throws an error if any of the textualModifications of the passed amendingLaw has a broken
+   * destination href character range. The error message contains a comma separated list of all
+   * textualMod eIds, that are affected. Pre-assumption: For this to work, the textualMod needs to
+   * have a valid eId.
+   *
    * @param amendingLaw the amending law to be checked
    */
   public void modHasValidDestRangeForDestNode(Norm amendingLaw) {
-    //    amendingLaw.getActiveModifications().stream()
-    //        .map(ActiveModification::getDestinationCharacterRange)
-    //        .toList();
-    // TODO
+    final String regex = "^\\d*-\\d*$";
+    final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+
+    List<String> textualModEidsWhereDestRangeIsBroken =
+        amendingLaw.getActiveModifications().stream()
+            .filter(
+                tm ->
+                    (tm.getDestinationHref().isEmpty()
+                            || tm.getDestinationHref().get().getCharacterRange().isEmpty()
+                            || !pattern
+                                .matcher(tm.getDestinationHref().get().getCharacterRange().get())
+                                .hasMatch())
+                        && tm.getEid().isPresent())
+            .map(am -> am.getEid().get())
+            .toList();
+
+    if (!textualModEidsWhereDestRangeIsBroken.isEmpty()) {
+      throw new XmlProcessingException(
+          "Some textual modifications have broken destination ranges. Here are the according textualMod eIds: %s"
+              .formatted(String.join(", ", textualModEidsWhereDestRangeIsBroken)),
+          null);
+    }
   }
 
   /**
@@ -90,7 +113,7 @@ public class ModificationValidator {
    * @param amendingLaw the amending law to be checked
    */
   public void throwErrorNoDestinationSet(Norm amendingLaw) {
-    List<String> emptyEliEids =
+    List<String> articleEidsWhereAffectedDocumentEliIsEmpty =
         amendingLaw.getArticles().stream()
             .filter(a -> a.getAffectedDocumentEli().isEmpty())
             .map(Article::getEid)
@@ -98,10 +121,10 @@ public class ModificationValidator {
             .map(Optional::get)
             .toList();
 
-    if (!emptyEliEids.isEmpty()) {
+    if (!articleEidsWhereAffectedDocumentEliIsEmpty.isEmpty()) {
       throw new XmlProcessingException(
-          "Some articles have empty affected document Elis. Here are the according eIds: %s"
-              .formatted(String.join(", ", emptyEliEids)),
+          "Some articles have empty affected document Elis. Here are the according article eIds: %s"
+              .formatted(String.join(", ", articleEidsWhereAffectedDocumentEliIsEmpty)),
           null);
     }
   }
