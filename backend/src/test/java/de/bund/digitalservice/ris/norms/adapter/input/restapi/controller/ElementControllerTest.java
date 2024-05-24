5b48b6907ddeb9ad18f6b648e094e0782df90db4
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2024-05-24 12:05:31 +0200

    refactor: move loading individual element HTML to service
    RISDEV-3988

--- backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ElementControllerTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ElementControllerTest.java
@@ -5,10 +5,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import de.bund.digitalservice.ris.norms.application.port.input.ApplyPassiveModificationsUseCase;
-import de.bund.digitalservice.ris.norms.application.port.input.LoadElementFromNormUseCase;
-import de.bund.digitalservice.ris.norms.application.port.input.LoadNormUseCase;
-import de.bund.digitalservice.ris.norms.application.port.input.TransformLegalDocMlToHtmlUseCase;
+import de.bund.digitalservice.ris.norms.application.port.input.*;
 import de.bund.digitalservice.ris.norms.config.SecurityConfig;
 import de.bund.digitalservice.ris.norms.domain.entity.NormFixtures;
 import java.util.Optional;
@@ -33,6 +30,7 @@ class ElementControllerTest {
   @MockBean private TransformLegalDocMlToHtmlUseCase transformLegalDocMlToHtmlUseCase;
   @MockBean private ApplyPassiveModificationsUseCase applyPassiveModificationsUseCase;
   @MockBean private LoadElementFromNormUseCase loadElementFromNormUseCase;
+  @MockBean private LoadElementHtmlFromNormUseCase loadElementHtmlFromNormUseCase;
 
   @Test
   void itReturnsPrefacePreambleArticleAndConclusionDataInElementsResponseEntrySchema()
