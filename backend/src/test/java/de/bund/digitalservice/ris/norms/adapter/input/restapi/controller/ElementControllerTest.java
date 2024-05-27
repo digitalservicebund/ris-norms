b2d2245e22b2d300115891c8f10565b9b8a1469a
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2024-05-27 17:50:49 +0200

    refactor: simplify controller for getting element HTML
    RISDEV-3899

--- backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ElementControllerTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ElementControllerTest.java
@@ -82,7 +82,7 @@ class ElementControllerTest {
             .perform(
                 get("/api/v1/norms/eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1/elements/hauptteil-1_para-20?atIsoDate=INVALID")
                     .accept(MediaType.TEXT_HTML))
-            .andExpect(status().isBadRequest());
+            .andExpect(status().is5xxServerError());
       }
     }
 
