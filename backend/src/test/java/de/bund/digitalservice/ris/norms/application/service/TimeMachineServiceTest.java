c57a945873a21fc13737e89e0105efc2a7242d63
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2024-05-28 12:14:39 +0200

    Remove TimeMachineUseCase
    RISDEV-4009

--- backend/src/test/java/de/bund/digitalservice/ris/norms/application/service/TimeMachineServiceTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/application/service/TimeMachineServiceTest.java
@@ -1,62 +1,24 @@
 package de.bund.digitalservice.ris.norms.application.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 import de.bund.digitalservice.ris.norms.application.port.input.ApplyPassiveModificationsUseCase;
-import de.bund.digitalservice.ris.norms.application.port.input.TimeMachineUseCase;
 import de.bund.digitalservice.ris.norms.domain.entity.Norm;
 import de.bund.digitalservice.ris.norms.domain.entity.NormFixtures;
 import de.bund.digitalservice.ris.norms.utils.NodeParser;
 import de.bund.digitalservice.ris.norms.utils.XmlMapper;
-import de.bund.digitalservice.ris.norms.utils.exceptions.XmlProcessingException;
 import java.time.Instant;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class TimeMachineServiceTest {
   final NormService normService = mock(NormService.class);
 
-  final TimeMachineService timeMachineService =
-      new TimeMachineService(new XmlDocumentService(), normService);
-
-  @Nested
-  class applyTimeMachine {
-    @Test
-    void itAppliesTimeMachine() {
-
-      // Given
-      var eli = "eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/regelungstext-1";
-
-      String amendingLawString =
-          "<?xml version=\"1.0\" encoding=\"UTF-8\"?><amendingLaw><akn:mod>In <akn:ref"
-              + " href=\"eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/regelungstext-1/two/9-34.xml\">paragraph 2</akn:ref> "
-              + "<akn:quotedText>old</akn:quotedText> with <akn:quotedText>new</akn:quotedText>.</akn:mod></amendingLaw>";
-      String targetLawString =
-          "<?xml version=\"1.0\" encoding=\"UTF-8\"?><targetLaw><akn:p eId=\"two\">old text</akn:p></targetLaw>";
-
-      when(normService.loadNormXml(any())).thenReturn(Optional.of(targetLawString));
-
-      // When
-      var xml =
-          timeMachineService.applyTimeMachine(new TimeMachineUseCase.Query(eli, amendingLawString));
-
-      // Then
-      verify(normService, times(1))
-          .loadNormXml(argThat(argument -> Objects.equals(argument.eli(), eli)));
-      assertThat(xml)
-          .isPresent()
-          .contains(
-              "<?xml version=\"1.0\" encoding=\"UTF-8\"?><targetLaw><akn:p eId=\"two\">new text</akn:p></targetLaw>");
-    }
-  }
+  final TimeMachineService timeMachineService = new TimeMachineService(normService);
 
   @Nested
   class applyPassiveModifications {
@@ -257,183 +219,6 @@ class TimeMachineServiceTest {
       assertThat(changedNodeValue.get())
           .isEqualToIgnoringWhitespace(
               "entgegen § 9 Absatz 1 Satz 2, Absatz 2 oder 3 Kennezichen eines verbotenen Vereins oder einer Ersatzorganisation verwendet,");
-    }
-  }
-
-  @Nested
-  class applyAmendingLawOnTargetLaw {
-
-    @Test
-    void oldTextIsReplacedByNewText() {
-      // given
-      String amendingLawString =
-          "<?xml version=\"1.0\" encoding=\"UTF-8\"?><amendingLaw><akn:mod>In <akn:ref"
-              + " href=\"eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/regelungstext-1/two/9-34.xml\">paragraph 2</akn:ref> "
-              + "<akn:quotedText>old</akn:quotedText> with <akn:quotedText>new</akn:quotedText>.</akn:mod></amendingLaw>";
-      String targetLawString =
-          "<?xml version=\"1.0\" encoding=\"UTF-8\"?><targetLaw><akn:p eId=\"two\">old text</akn:p></targetLaw>";
-
-      // when
-      String result = timeMachineService.apply(amendingLawString, targetLawString);
-
-      // then
-      assertThat(result)
-          .isEqualTo(
-              "<?xml version=\"1.0\" encoding=\"UTF-8\"?><targetLaw><akn:p eId=\"two\">new text</akn:p></targetLaw>");
-    }
-
-    @Test
-    void XmlProcessingExceptionIsThrownWhenAmendingLawXmlIsInvalid() {
-      // given
-      String amendingLawString = "SomeRandomText";
-      String targetLawString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><targetLaw></targetLaw>";
-
-      // when
-      Throwable thrown =
-          catchThrowable(() -> timeMachineService.apply(amendingLawString, targetLawString));
-
-      // then
-      assertThat(thrown).isInstanceOf(XmlProcessingException.class);
-    }
-
-    @Test
-    void XmlProcessingExceptionIsThrownWhenTargetLawXmlIsInvalid() {
-      // given
-      String amendingLawString =
-          "<?xml version=\"1.0\" encoding=\"UTF-8\"?><amendingLaw></amendingLaw>";
-
-      String targetLawString = "randomString";
-
-      // when
-      Throwable thrown =
-          catchThrowable(() -> timeMachineService.apply(amendingLawString, targetLawString));
-
-      // then
-      assertThat(thrown).isInstanceOf(XmlProcessingException.class);
-    }
-
-    @ParameterizedTest
-    @ValueSource(
-        strings = {
-          """
-                 only one quotedText
-
-                 <akn:mod>
-                   In <akn:ref
-            href="eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/regelungstext-1/two/9-34.xml">paragraph
-            2</akn:ref> replace with <akn:quotedText>new</akn:quotedText>.
-                 </akn:mod>
-               """,
-          """
-                 eId THREE not found in target law
-
-                 <akn:mod>
-                   In <akn:ref
-            href="eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/regelungstext-1/THREE/9-34.xml">paragraph
-            2</akn:ref> replace <akn:quotedText>old</akn:quotedText> with
-            <akn:quotedText>new</akn:quotedText>.
-                 </akn:mod>
-               """,
-          """
-                 no href attribute in "ref" tag
-
-                 <akn:mod>
-                   In <akn:ref>paragraph 2</akn:ref> replace <akn:quotedText>old</akn:quotedText> with
-            <akn:quotedText>new</akn:quotedText>.
-                 </akn:mod>
-               """,
-          """
-                 can't get eId from href
-
-                 <akn:mod>
-                   In <akn:ref href="invalid-eli-href">paragraph 2</akn:ref> replace
-            <akn:quotedText>old</akn:quotedText> with <akn:quotedText>new</akn:quotedText>.
-                 </akn:mod>
-               """
-        })
-    void throwModificationExceptionOnMissingParts(String modificationNodeText) {
-      // given
-      final String amendingLawXmlText =
-          """
-          <?xml version="1.0" encoding="UTF-8"?>
-          <akn:body>
-          """
-              + modificationNodeText
-              + """
-
-                </akn:body>
-              """;
-      final String targetLawXmlText =
-          """
-          <?xml version="1.0" encoding="UTF-8"?>
-          <akn:body>
-              <akn:p eId="one">old text</akn:p>
-              <akn:p eId="two">old text</akn:p>
-            </akn:body>
-          """;
-
-      // when
-      Throwable thrown =
-          catchThrowable(() -> timeMachineService.apply(amendingLawXmlText, targetLawXmlText));
-
-      // then
-      assertThat(thrown).isInstanceOf(XmlProcessingException.class);
-    }
-
-    @Test
-    void throwModificationExceptionIfAmendingLawHasNoModifications() {
-      // given
-      final String amendingLaw = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><amending/>";
-      final String targetLaw = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><target/>";
-
-      // when
-      Throwable thrown = catchThrowable(() -> timeMachineService.apply(amendingLaw, targetLaw));
-
-      // then
-      assertThat(thrown).isInstanceOf(XmlProcessingException.class);
-    }
-
-    @Test
-    void targetLawToContainTheNewTextInPlaceOfTheOldOne() {
-      // given
-      final String amendingLawXmlText =
-          """
-             <?xml version="1.0" encoding="UTF-8"?>
-             <akn:body>
-                 <akn:mod>
-               In <akn:ref href="eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/regelungstext-1/two/9-34.xml">paragraph 2</akn:ref> replace <akn:quotedText>old</akn:quotedText> with
-               <akn:quotedText>new</akn:quotedText>.
-                 </akn:mod>
-
-                 "old" -> "new"
-
-          </akn:body>
-             """
-              .strip();
-      final String targetLawXmlText =
-          """
-          <?xml version="1.0" encoding="UTF-8"?>
-          <akn:body>
-              <akn:p eId="one">old text</akn:p>
-              <akn:p eId="two">old text</akn:p>
-            </akn:body>
-          """;
-
-      final String expectedResultingLawXmlText =
-          """
-          <?xml version="1.0" encoding="UTF-8"?>
-          <akn:body>
-              <akn:p eId="one">old text</akn:p>
-              <akn:p eId="two">new text</akn:p>
-            </akn:body>
-          """
-              .replaceAll("[\\n ]", "");
-
-      // when applying the TimeMachine
-      final String resultingLaw = timeMachineService.apply(amendingLawXmlText, targetLawXmlText);
-
-      // the result contains the new text in place of the old text
-      assertThat(resultingLaw.replaceAll("[\\n ]", "")).isEqualTo(expectedResultingLawXmlText);
     }
   }
 }
