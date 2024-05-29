926f3ba14fcb600a61af09476c5c108d0367676f
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-05-29 16:05:42 +0200

    Test: Nest test cases
    RISDEV-0000

--- backend/src/test/java/de/bund/digitalservice/ris/norms/application/service/ArticleServiceTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/application/service/ArticleServiceTest.java
@@ -9,6 +9,8 @@ import de.bund.digitalservice.ris.norms.application.port.input.LoadArticleHtmlUs
 import de.bund.digitalservice.ris.norms.application.port.output.LoadNormPort;
 import de.bund.digitalservice.ris.norms.domain.entity.NormFixtures;
 import java.util.Optional;
+
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class ArticleServiceTest {
@@ -17,57 +19,59 @@ class ArticleServiceTest {
   final TimeMachineService timeMachineService = mock(TimeMachineService.class);
   final XsltTransformationService xsltTransformationService = mock(XsltTransformationService.class);
   final ArticleService articleService =
-      new ArticleService(loadNormPort, timeMachineService, xsltTransformationService);
+          new ArticleService(loadNormPort, timeMachineService, xsltTransformationService);
 
-  // TODO Hannes: Nest
+  @Nested
+  class loadArticleHtml {
 
-  @Test
-  void returnResult() {
-    // given
-    var eli = "eli/bund/bgbl-1/2000/s1/1970-01-01/1/deu/regelungstext-1";
-    var norm = NormFixtures.loadFromDisk("NormWithMods.xml");
-    var eid = "hauptteil-1_art-1";
-    when(loadNormPort.loadNorm(new LoadNormPort.Command(eli))).thenReturn(Optional.of(norm));
-    when(timeMachineService.applyPassiveModifications(any())).thenReturn(norm);
-    when(xsltTransformationService.transformLegalDocMlToHtml(any())).thenReturn("<div></div>");
-    // when
-    var result =
-        articleService.loadArticleHtml(
-            // TODO Hannes: remove null once we have both constructors
-            new LoadArticleHtmlUseCase.Query(eli, eid));
-    // then
-    assertThat(result).isPresent();
-  }
+    @Test
+    void returnResult() {
+      // given
+      var eli = "eli/bund/bgbl-1/2000/s1/1970-01-01/1/deu/regelungstext-1";
+      var norm = NormFixtures.loadFromDisk("NormWithMods.xml");
+      var eid = "hauptteil-1_art-1";
+      when(loadNormPort.loadNorm(new LoadNormPort.Command(eli))).thenReturn(Optional.of(norm));
+      when(timeMachineService.applyPassiveModifications(any())).thenReturn(norm);
+      when(xsltTransformationService.transformLegalDocMlToHtml(any())).thenReturn("<div></div>");
+      // when
+      var result =
+              articleService.loadArticleHtml(
+                      // TODO Hannes: remove null once we have both constructors
+                      new LoadArticleHtmlUseCase.Query(eli, eid));
+      // then
+      assertThat(result).isPresent();
+    }
 
-  @Test
-  void returnEmptyIfNormNotFound() {
-    // given
-    var eli = "eli/bund/DOES_NOT_EXIST/2000/s1/1970-01-01/1/deu/regelungstext-1";
-    var eid = "meta-1";
-    when(loadNormPort.loadNorm(new LoadNormPort.Command(eli))).thenReturn(Optional.empty());
-    // when
-    var result =
-        articleService.loadArticleHtml(
-            // TODO Hannes: remove null once we have both constructors
-            new LoadArticleHtmlUseCase.Query(eli, eid));
-    // then
-    assertThat(result).isEmpty();
-  }
+    @Test
+    void returnEmptyIfNormNotFound() {
+      // given
+      var eli = "eli/bund/DOES_NOT_EXIST/2000/s1/1970-01-01/1/deu/regelungstext-1";
+      var eid = "meta-1";
+      when(loadNormPort.loadNorm(new LoadNormPort.Command(eli))).thenReturn(Optional.empty());
+      // when
+      var result =
+              articleService.loadArticleHtml(
+                      // TODO Hannes: remove null once we have both constructors
+                      new LoadArticleHtmlUseCase.Query(eli, eid));
+      // then
+      assertThat(result).isEmpty();
+    }
 
-  @Test
-  void returnEmptyIfArticleNotFound() {
-    // given
-    var norm = NormFixtures.loadFromDisk("SimpleNorm.xml");
-    var eli = "eli/bund/DOES_NOT_EXIST/2000/s1/1970-01-01/1/deu/regelungstext-1";
-    var eid = "NOT_IN_NORM";
-    when(loadNormPort.loadNorm(new LoadNormPort.Command(eli))).thenReturn(Optional.of(norm));
-    when(loadNormPort.loadNorm(new LoadNormPort.Command(eli))).thenReturn(Optional.of(norm));
-    // when
-    var result =
-        articleService.loadArticleHtml(
-            // TODO Hannes: remove null once we have both constructors
-            new LoadArticleHtmlUseCase.Query(eli, eid));
-    // then
-    assertThat(result).isEmpty();
+    @Test
+    void returnEmptyIfArticleNotFound() {
+      // given
+      var norm = NormFixtures.loadFromDisk("SimpleNorm.xml");
+      var eli = "eli/bund/DOES_NOT_EXIST/2000/s1/1970-01-01/1/deu/regelungstext-1";
+      var eid = "NOT_IN_NORM";
+      when(loadNormPort.loadNorm(new LoadNormPort.Command(eli))).thenReturn(Optional.of(norm));
+      when(loadNormPort.loadNorm(new LoadNormPort.Command(eli))).thenReturn(Optional.of(norm));
+      // when
+      var result =
+              articleService.loadArticleHtml(
+                      // TODO Hannes: remove null once we have both constructors
+                      new LoadArticleHtmlUseCase.Query(eli, eid));
+      // then
+      assertThat(result).isEmpty();
+    }
   }
 }
