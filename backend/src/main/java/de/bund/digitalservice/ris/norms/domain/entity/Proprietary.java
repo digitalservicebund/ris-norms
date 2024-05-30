a35774a4a632f94f2e149065a8f002e93a5a0888
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2024-05-30 09:13:42 +0200

    feat: extend norm to return proprietary metadata
    RISDEV-4052

---
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/domain/entity/Proprietary.java
@@ -0,0 +1,17 @@
+package de.bund.digitalservice.ris.norms.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import org.w3c.dom.Node;
+
+/**
+ * Encapsulates metadata that is found in the proprietary section of the document. This includes
+ * both metadata that is proprietary to the LDML.de standard and to DigitalService.
+ */
+@Getter
+@SuperBuilder(toBuilder = true)
+@AllArgsConstructor
+public class Proprietary {
+  private final Node node;
+}
---
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/domain/entity/ProprietaryTest.java
@@ -0,0 +1,12 @@
+package de.bund.digitalservice.ris.norms.domain.entity;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class ProprietaryTest {
+  @Nested
+  class fna {
+    @Test
+    void getsFna() {}
+  }
+}
