8fab31defb95269e3b85cb6f20862a9b7996b7d0
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-05-31 14:13:16 +0200

    Feature: Return 404 if norm not found
    RISDEV-4052

--- backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ProprietaryController.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ProprietaryController.java
@@ -1,8 +1,22 @@
 package de.bund.digitalservice.ris.norms.adapter.input.restapi.controller;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import de.bund.digitalservice.ris.norms.adapter.input.restapi.schema.ProprietaryResponseSchema;
 import de.bund.digitalservice.ris.norms.domain.entity.Norm;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /** Retrieve proprietary data of a {@link Norm}. */
 @RestController
-public class ProprietaryController {}
+@RequestMapping(
+    "/api/v1/norms/eli/bund/{agent}/{year}/{naturalIdentifier}/{pointInTime}/{version}/{language}/{subtype}/proprietary")
+// "/api/v1/norms/eli/bund/NONEXISTENT_NORM/1964/s593/1964-08-05/1/deu/regelungstext-1/proprietary"
+public class ProprietaryController {
+  @GetMapping(produces = {APPLICATION_JSON_VALUE})
+  public ResponseEntity<ProprietaryResponseSchema> getProprietary() {
+    return ResponseEntity.notFound().build();
+  }
+}
