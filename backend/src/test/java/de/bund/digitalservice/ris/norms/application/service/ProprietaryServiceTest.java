44d3e6d14dcb8a35de03af0b204de229d97fd27e
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-05-31 09:03:26 +0200

    Test: Add mocks
    RISDEV-4052

--- backend/src/test/java/de/bund/digitalservice/ris/norms/application/service/ProprietaryServiceTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/application/service/ProprietaryServiceTest.java
@@ -2,13 +2,17 @@ package de.bund.digitalservice.ris.norms.application.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import de.bund.digitalservice.ris.norms.application.port.input.LoadProprietaryFromNormUseCase;
+import de.bund.digitalservice.ris.norms.application.port.output.LoadNormPort;
+import de.bund.digitalservice.ris.norms.domain.entity.NormFixtures;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 
 class ProprietaryServiceTest {
-
-  final ProprietaryService proprietaryService = mock(ProprietaryService.class);
+  final LoadNormPort loadNormPort = mock(LoadNormPort.class);
+  final ProprietaryService proprietaryService = new ProprietaryService(loadNormPort);
 
   @Test
   void returnsEmptyIfNormNotFound() throws Exception {
@@ -21,10 +25,17 @@ class ProprietaryServiceTest {
     assertThat(result).isEmpty();
   }
 
+  // TODO: proprietary not found
+
+  // TODO: fna not found
+
   @Test
   void returnFna() throws Exception {
     // given
     var eli = "eli/bund/bgbl-1/2002/s1181/2019-11-22/1/deu/rechtsetzungsdokument-1";
+    var normWithProprietary = NormFixtures.loadFromDisk("NormWithProprietary.xml");
+    when(loadNormPort.loadNorm(new LoadNormPort.Command(eli)))
+        .thenReturn(Optional.of(normWithProprietary));
     // when
     var result =
         proprietaryService.loadProprietaryFromNorm(new LoadProprietaryFromNormUseCase.Query(eli));
