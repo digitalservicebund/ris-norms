6c0ea78cb64a9df7a0310d2406472ae5e2f538f6
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-05-31 09:16:43 +0200

    Test: (red) Throw NormNotFound
    RISDEV-4052

--- backend/src/test/java/de/bund/digitalservice/ris/norms/application/service/ProprietaryServiceTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/application/service/ProprietaryServiceTest.java
@@ -1,9 +1,11 @@
 package de.bund.digitalservice.ris.norms.application.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import de.bund.digitalservice.ris.norms.application.port.input.LoadElementsByTypeFromNormUseCase;
 import de.bund.digitalservice.ris.norms.application.port.input.LoadProprietaryFromNormUseCase;
 import de.bund.digitalservice.ris.norms.application.port.output.LoadNormPort;
 import de.bund.digitalservice.ris.norms.domain.entity.NormFixtures;
@@ -15,14 +17,17 @@ class ProprietaryServiceTest {
   final ProprietaryService proprietaryService = new ProprietaryService(loadNormPort);
 
   @Test
-  void returnsEmptyIfNormNotFound() throws Exception {
+  void throwsNormNotFoundExceptionIfNormNotFound()
+      throws LoadElementsByTypeFromNormUseCase.NormNotFoundException {
     // given
     var eli = "eli/bund/INVALID_ELI/2002/s1181/2019-11-22/1/deu/rechtsetzungsdokument-1";
     // when
-    var result =
-        proprietaryService.loadProprietaryFromNorm(new LoadProprietaryFromNormUseCase.Query(eli));
-    // then
-    assertThat(result).isEmpty();
+    assertThatThrownBy(
+            () ->
+                proprietaryService.loadProprietaryFromNorm(
+                    new LoadProprietaryFromNormUseCase.Query(eli)))
+        // then
+        .isInstanceOf(LoadElementsByTypeFromNormUseCase.NormNotFoundException.class);
   }
 
   // TODO: proprietary not found
