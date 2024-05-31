c000536491932a73b164c6fd01f6b7cd3d880463
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-05-31 08:29:10 +0200

    Test: (red) Return empty if norm not found
    RISDEV-4052

---
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/application/service/ProprietaryServiceTest.java
@@ -0,0 +1,23 @@
+package de.bund.digitalservice.ris.norms.application.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+import de.bund.digitalservice.ris.norms.application.port.input.LoadProprietaryFromNormUseCase;
+import org.junit.jupiter.api.Test;
+
+class ProprietaryServiceTest {
+
+  final ProprietaryService proprietaryService = mock(ProprietaryService.class);
+
+  @Test
+  void returnsEmptyIfNormNotFound() throws Exception {
+    // given
+    var eli = "eli/bund/INVALID_ELI/2002/s1181/2019-11-22/1/deu/rechtsetzungsdokument-1";
+    // when
+    var result =
+        proprietaryService.loadProprietaryFromNorm(new LoadProprietaryFromNormUseCase.Query(eli));
+    // then
+    assertThat(result).isEmpty();
+  }
+}
