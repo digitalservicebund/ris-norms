96ae2bec1be9c0890f15eca031694602958d2aed
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-06-03 20:45:31 +0200

    Test: Nest tests without atDate
    RISDEV-4075

--- backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ProprietaryControllerTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ProprietaryControllerTest.java
@@ -9,6 +9,7 @@ import de.bund.digitalservice.ris.norms.application.service.ProprietaryService;
 import de.bund.digitalservice.ris.norms.config.SecurityConfig;
 import de.bund.digitalservice.ris.norms.domain.entity.NormFixtures;
 import de.bund.digitalservice.ris.norms.utils.exceptions.NormNotFoundException;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -29,73 +30,81 @@ class ProprietaryControllerTest {
 
   @MockBean private ProprietaryService proprietaryService;
 
-  @Test
-  void returns404IfNormNotFound() throws Exception {
-    // given
-    var eli = "eli/bund/NONEXISTENT_NORM/1964/s593/1964-08-05/1/deu/regelungstext-1";
-    when(proprietaryService.loadProprietaryFromNorm(new LoadProprietaryFromNormUseCase.Query(eli)))
-        .thenThrow(new NormNotFoundException(eli));
-    // when
-    mockMvc
-        .perform(
-            get("/api/v1/norms/" + eli + "/proprietary").accept(MediaType.APPLICATION_JSON_VALUE))
-        // then
-        .andExpect(status().isNotFound());
-  }
+  @Nested
+  class getProprietary {
 
-  @Test
-  void returnsProprietaryResponseSchema() throws Exception {
-    // given
-    var eli = "eli/bund/bgbl-1/2002/s1181/2019-11-22/1/deu/rechtsetzungsdokument-1";
-    var normWithProprietary = NormFixtures.loadFromDisk("NormWithProprietary.xml");
-    var proprietary = normWithProprietary.getProprietary();
-    when(proprietaryService.loadProprietaryFromNorm(new LoadProprietaryFromNormUseCase.Query(eli)))
-        .thenReturn(proprietary);
+    @Test
+    void returns404IfNormNotFound() throws Exception {
+      // given
+      var eli = "eli/bund/NONEXISTENT_NORM/1964/s593/1964-08-05/1/deu/regelungstext-1";
+      when(proprietaryService.loadProprietaryFromNorm(
+              new LoadProprietaryFromNormUseCase.Query(eli)))
+          .thenThrow(new NormNotFoundException(eli));
+      // when
+      mockMvc
+          .perform(
+              get("/api/v1/norms/" + eli + "/proprietary").accept(MediaType.APPLICATION_JSON_VALUE))
+          // then
+          .andExpect(status().isNotFound());
+    }
 
-    // when
-    mockMvc
-        .perform(
-            get("/api/v1/norms/" + eli + "/proprietary").accept(MediaType.APPLICATION_JSON_VALUE))
-        // then
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("fna.value").value("754-28-1"));
-  }
+    @Test
+    void returnsProprietaryResponseSchema() throws Exception {
+      // given
+      var eli = "eli/bund/bgbl-1/2002/s1181/2019-11-22/1/deu/rechtsetzungsdokument-1";
+      var normWithProprietary = NormFixtures.loadFromDisk("NormWithProprietary.xml");
+      var proprietary = normWithProprietary.getProprietary();
+      when(proprietaryService.loadProprietaryFromNorm(
+              new LoadProprietaryFromNormUseCase.Query(eli)))
+          .thenReturn(proprietary);
 
-  @Test
-  void returnsEmptyValuesIfSpecificProprietaryDataIsNotFound() throws Exception {
-    // given
-    var eli = "eli/bund/bgbl-1/2002/s1181/2019-11-22/1/deu/rechtsetzungsdokument-1";
-    var normWithInvalidProprietary = NormFixtures.loadFromDisk("NormWithInvalidProprietary.xml");
-    var proprietary = normWithInvalidProprietary.getProprietary();
-    when(proprietaryService.loadProprietaryFromNorm(new LoadProprietaryFromNormUseCase.Query(eli)))
-        .thenReturn(proprietary);
+      // when
+      mockMvc
+          .perform(
+              get("/api/v1/norms/" + eli + "/proprietary").accept(MediaType.APPLICATION_JSON_VALUE))
+          // then
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("fna.value").value("754-28-1"));
+    }
 
-    // when
-    mockMvc
-        .perform(
-            get("/api/v1/norms/" + eli + "/proprietary").accept(MediaType.APPLICATION_JSON_VALUE))
-        // then
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("fna").exists())
-        .andExpect(jsonPath("fna.value").doesNotExist());
-  }
+    @Test
+    void returnsEmptyValuesIfSpecificProprietaryDataIsNotFound() throws Exception {
+      // given
+      var eli = "eli/bund/bgbl-1/2002/s1181/2019-11-22/1/deu/rechtsetzungsdokument-1";
+      var normWithInvalidProprietary = NormFixtures.loadFromDisk("NormWithInvalidProprietary.xml");
+      var proprietary = normWithInvalidProprietary.getProprietary();
+      when(proprietaryService.loadProprietaryFromNorm(
+              new LoadProprietaryFromNormUseCase.Query(eli)))
+          .thenReturn(proprietary);
 
-  @Test
-  void returnsEmptyValuesIfProprietaryDoesNotExist() throws Exception {
-    // given
-    var eli = "eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/regelungstext-1";
-    var normWithInvalidProprietary = NormFixtures.loadFromDisk("SimpleNorm.xml");
-    var proprietary = normWithInvalidProprietary.getProprietary();
-    when(proprietaryService.loadProprietaryFromNorm(new LoadProprietaryFromNormUseCase.Query(eli)))
-        .thenReturn(proprietary);
+      // when
+      mockMvc
+          .perform(
+              get("/api/v1/norms/" + eli + "/proprietary").accept(MediaType.APPLICATION_JSON_VALUE))
+          // then
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("fna").exists())
+          .andExpect(jsonPath("fna.value").doesNotExist());
+    }
 
-    // when
-    mockMvc
-        .perform(
-            get("/api/v1/norms/" + eli + "/proprietary").accept(MediaType.APPLICATION_JSON_VALUE))
-        // then
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("fna").exists())
-        .andExpect(jsonPath("fna.value").doesNotExist());
+    @Test
+    void returnsEmptyValuesIfProprietaryDoesNotExist() throws Exception {
+      // given
+      var eli = "eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/regelungstext-1";
+      var normWithInvalidProprietary = NormFixtures.loadFromDisk("SimpleNorm.xml");
+      var proprietary = normWithInvalidProprietary.getProprietary();
+      when(proprietaryService.loadProprietaryFromNorm(
+              new LoadProprietaryFromNormUseCase.Query(eli)))
+          .thenReturn(proprietary);
+
+      // when
+      mockMvc
+          .perform(
+              get("/api/v1/norms/" + eli + "/proprietary").accept(MediaType.APPLICATION_JSON_VALUE))
+          // then
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("fna").exists())
+          .andExpect(jsonPath("fna.value").doesNotExist());
+    }
   }
 }
