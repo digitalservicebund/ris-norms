a90763fbe3395f804bc104249a8e3c86b88ae224
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-06-03 11:53:48 +0200

    Feat: Return proprietary
    RISDEV-4052

--- backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ProprietaryControllerTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ProprietaryControllerTest.java
@@ -44,7 +44,7 @@ class ProprietaryControllerTest {
   }
 
   @Test
-  void returnsProprietary() throws Exception {
+  void returnsProprietaryResponseSchema() throws Exception {
     // given
     var eli = "eli/bund/bgbl-1/2002/s1181/2019-11-22/1/deu/rechtsetzungsdokument-1";
     var norm = NormFixtures.loadFromDisk("NormWithProprietary.xml");
@@ -64,53 +64,4 @@ class ProprietaryControllerTest {
 
     // returns fallback if not found
   }
-
-  //  @Test
-  //  void returnFna() throws Exception {
-  //    // given
-  //    var eli = "eli/bund/NONEXISTENT_NORM/1964/s593/1964-08-05/1/deu/regelungstext-1";
-  //    var proprietaryNodeText =
-  //        """
-  //            <akn:proprietary eId="meta-1_proprietary-1"
-  //                                         GUID="952262d3-de92-4c1d-a06d-95aa94f5f21c"
-  //                                         source="attributsemantik-noch-undefiniert">
-  //                            <meta:legalDocML.de_metadaten
-  // xmlns:meta="http://Metadaten.LegalDocML.de/1.6/">
-  //                                <meta:typ>gesetz</meta:typ>
-  //                                <meta:form>stammform</meta:form>
-  //                                <meta:fassung>verkuendungsfassung</meta:fassung>
-  //                                <meta:art>rechtsetzungsdokument</meta:art>
-  //                                <meta:initiant>nicht-vorhanden</meta:initiant>
-  //
-  // <meta:bearbeitendeInstitution>nicht-vorhanden</meta:bearbeitendeInstitution>
-  //                                <!-- Die vorliegende Angabe von meta:fna stellt einen
-  // beliebigen, exemplarischen Fundstellennachweis dar und besitzt keine fachliche Korrektheit. -->
-  //                                <meta:fna>754-28-1</meta:fna>
-  //                                <!-- Die vorliegende Angabe von meta:gesta besitzt keine
-  // fachliche Korrektheit. -->
-  //                                <meta:gesta>nicht-vorhanden</meta:gesta>
-  //                                <!-- Die vorliegenden Angaben von meta:federfuehrung besitzen
-  // keine fachliche Korrektheit. -->
-  //                                <meta:federfuehrung>
-  //                                    <meta:federfuehrend ab="2002-12-01"
-  // bis="unbestimmt">Bundesministerium des Innern und für Heimat</meta:federfuehrend>
-  //                                    <meta:federfuehrend ab="2002-10-01"
-  // bis="2002-12-01">Bundesministerium der Justiz</meta:federfuehrend>
-  //                                </meta:federfuehrung>
-  //                            </meta:legalDocML.de_metadaten>
-  //                        </akn:proprietary>
-  //            """;
-  //    var proprietary = Proprietary.builder().node(XmlMapper.toNode(proprietaryNodeText)).build();
-  //    when(proprietaryService.loadProprietaryFromNorm(new
-  // LoadProprietaryFromNormUseCase.Query(eli)))
-  //        .thenReturn(Optional.of(proprietary));
-  //    // when
-  //    mockMvc
-  //        .perform(
-  //            get("/api/v1/norms/" + eli +
-  // "/proprietary").accept(MediaType.APPLICATION_JSON_VALUE))
-  //        // then
-  //        .andExpect(status().isOk())
-  //        .andExpect(jsonPath("fna.value").value("754-28-1"));
-  //  }
 }
