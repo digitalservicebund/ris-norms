0bd8d33a11fb2c8989ec1175bafcef4440d1224d
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-06-04 08:54:26 +0200

    Test: Return empty values if there is on proprietary in norm
    RISDEV-4076

--- backend/src/test/java/de/bund/digitalservice/ris/norms/integration/adapter/input/restapi/ProprietaryControllerIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/integration/adapter/input/restapi/ProprietaryControllerIntegrationTest.java
@@ -1,9 +1,11 @@
 package de.bund.digitalservice.ris.norms.integration.adapter.input.restapi;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import de.bund.digitalservice.ris.norms.adapter.output.database.mapper.NormMapper;
 import de.bund.digitalservice.ris.norms.adapter.output.database.repository.NormRepository;
+import de.bund.digitalservice.ris.norms.domain.entity.NormFixtures;
 import de.bund.digitalservice.ris.norms.integration.BaseIntegrationTest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -23,8 +25,7 @@ public class ProprietaryControllerIntegrationTest extends BaseIntegrationTest {
 
   @Test
   void return404IfNormNotFound() throws Exception {
-    // given
-
+    // given no norm
     // when
     mockMvc
         .perform(
@@ -32,5 +33,23 @@ public class ProprietaryControllerIntegrationTest extends BaseIntegrationTest {
                 .accept(MediaType.APPLICATION_JSON_VALUE))
         // then
         .andExpect(status().isNotFound());
+  }
+
+  @Test
+  void returnEmptyValuesIfNormHasNoProprietary() throws Exception {
+    // given
+      var eli = "eli/bund/bgbl-1/2002/s1181/2019-11-22/1/deu/rechtsetzungsdokument-1";
+    var norm = NormFixtures.loadFromDisk("NormWithoutProprietary.xml");
+    normRepository.save(NormMapper.mapToDto(norm));
+
+    // when
+    mockMvc
+        .perform(
+            get("/api/v1/norms/" + eli + "/proprietary")
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+        // then
+        .andExpect(status().isOk())
+            .andExpect(jsonPath("fna").exists())
+            .andExpect(jsonPath("fna.value").doesNotExist());
   }
 }
