e53337ea483e5864e241af1dbd60a867194874f2
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2024-06-04 11:40:54 +0200

    Support constructing eli from eli string
    RISDEV-0000

--- backend/src/main/java/de/bund/digitalservice/ris/norms/domain/entity/Eli.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/domain/entity/Eli.java
@@ -1,7 +1,10 @@
 package de.bund.digitalservice.ris.norms.domain.entity;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
@@ -13,6 +16,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Eli {
   private String agent;
   private String year;
@@ -21,6 +25,25 @@ public class Eli {
   private String version;
   private String language;
   private String subtype;
+
+  public Eli(String eli) {
+    Matcher matcher =
+        Pattern.compile(
+                "eli/bund/(?<agent>[^/]+)/(?<year>[^/]+)/(?<naturalIdentifier>[^/]+)/(?<pointInTime>[^/]+)/(?<version>[^/]+)/(?<language>[^/]+)/(?<subtype>[^/.]+)(\\.xml)?")
+            .matcher(eli);
+
+    if (!matcher.matches()) {
+      throw new IllegalArgumentException("Invalid Eli");
+    }
+
+    this.agent = matcher.group("agent");
+    this.year = matcher.group("year");
+    this.naturalIdentifier = matcher.group("naturalIdentifier");
+    this.pointInTime = matcher.group("pointInTime");
+    this.version = matcher.group("version");
+    this.language = matcher.group("language");
+    this.subtype = matcher.group("subtype");
+  }
 
   public String getValue() {
     return "eli/bund/"
