c65d9ca101184824dcb8dbe054531579dca2064f
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2024-06-04 08:06:02 +0200

    feat: add type definitions for proprietary metadata
    RISDEV-4053

---
+++ frontend/src/types/proprietary.ts
@@ -0,0 +1,11 @@
+/**
+ * Encapsulates metadata that is found in the proprietary section of the
+ * document. This includes both metadata that is proprietary to the LDML.de
+ * standard and to DigitalService.
+ */
+export type Proprietary = {
+  /** FNA (Fundstellennachweis A) of a norm. */
+  fna: {
+    value?: string
+  }
+}
