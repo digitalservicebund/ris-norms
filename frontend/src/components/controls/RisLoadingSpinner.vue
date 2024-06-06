62a1f572237c9a0bfd4c4fd24f334c02ccf28e12
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2024-06-06 08:32:15 +0200

    feat: make loading spinner customizable
    RISDEV-4054

--- frontend/src/components/controls/RisLoadingSpinner.vue
+++ frontend/src/components/controls/RisLoadingSpinner.vue
@@ -1,8 +1,17 @@
-<script setup lang="ts"></script>
-
 <template>
   <div
-    aria-label="Ladestatus"
-    class="inline-block h-32 w-32 animate-spin rounded-full border-[3px] border-solid border-blue-700 border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-  ></div>
+    role="status"
+    aria-label="Lädt..."
+    class="inline-block animate-spin rounded-full border-[3px] border-solid border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+    :class="$style.spinner"
+  />
 </template>
+
+<style module>
+/* Managing size and color as a separate class with a 0-specificity selector so
+they can be easily overridden using classes on the RisLoadingSpinner component.
+See RisLoadingSpinner.story.vue for an example. */
+:where(.spinner) {
+  @apply h-32 w-32 border-blue-700;
+}
+</style>
