2b08fd911f87c1c5f6652881c4505d6951b34ef0
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-06-07 11:50:38 +0200

    Feat: Return art, type, subtype with empty values
    RISDEV-4098

--- backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/schema/ProprietaryResponseSchema.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/schema/ProprietaryResponseSchema.java
@@ -10,12 +10,39 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder(toBuilder = true)
 public class ProprietaryResponseSchema {
   private Fna fna;
+  private Art art;
+  private Typ typ;
+  private Subtyp subtyp;
 
   /** Nested schema for FNAs. */
   @AllArgsConstructor
   @Data
   @SuperBuilder(toBuilder = true)
   public static class Fna {
+    private String value;
+  }
+
+  /** Nested schema for art. */
+  @AllArgsConstructor
+  @Data
+  @SuperBuilder(toBuilder = true)
+  public static class Art {
+    private String value;
+  }
+
+  /** Nested schema for typ. */
+  @AllArgsConstructor
+  @Data
+  @SuperBuilder(toBuilder = true)
+  public static class Typ {
+    private String value;
+  }
+
+  /** Nested schema for subtyp. */
+  @AllArgsConstructor
+  @Data
+  @SuperBuilder(toBuilder = true)
+  public static class Subtyp {
     private String value;
   }
 }
