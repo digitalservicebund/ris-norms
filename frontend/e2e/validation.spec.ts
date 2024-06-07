965decd708f52d0a250d3ec11f0dba252b2c5277
Sebastian Rossa <sebastian.rossa@digitalservice.bund.de>
2024-06-07 15:22:20 +0200

    Deposit correct error message in test
    RISDEV-3966

--- frontend/e2e/validation.spec.ts
+++ frontend/e2e/validation.spec.ts
@@ -55,6 +55,6 @@ test.describe("validation errors", () => {
     await page.getByLabel("zu ersetzende Textstelle").press("Enter")
     expect("true").toMatch("true") // placeholder to pass commit checks
     // expect error message in page
-    // Unable to process contained instructions: For norm with Eli (eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1): Couldn't load target eId (hauptteil-1_para-20_abs-1_untergl-1_listenelem-2_inhalt-1_text-1wrongEid) element in zf0 (eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1) for mod with eId hauptteil-1_art-1_abs-1_untergl-1_listenelem-2_inhalt-1_text-1_ändbefehl-1
+    // For norm with Eli(eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1): The character range in mod href is not valid(target paragraph is to short)in mod with eId hauptteil-1_ art-1_ abs-1_ untergl-1_ listenelem-2_ inhalt-1_ text-1_ ändbefehl-1
   })
 })
