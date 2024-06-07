a33dc45d7ccb98f1f5f4660ad484cbd6b85d2739
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-06-07 14:38:40 +0200

    Test: Return empty art, typ, subtyp if not found
    RISDEV-4098

--- backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ProprietaryControllerTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/ProprietaryControllerTest.java
@@ -167,7 +167,13 @@ class ProprietaryControllerTest {
           // then
           .andExpect(status().isOk())
           .andExpect(jsonPath("fna").exists())
-          .andExpect(jsonPath("fna.value").doesNotExist());
+          .andExpect(jsonPath("fna.value").doesNotExist())
+          .andExpect(jsonPath("art").exists())
+          .andExpect(jsonPath("art.value").doesNotExist())
+          .andExpect(jsonPath("typ").exists())
+          .andExpect(jsonPath("typ.value").doesNotExist())
+          .andExpect(jsonPath("subtyp").exists())
+          .andExpect(jsonPath("subtyp.value").doesNotExist());
     }
 
     @Test
