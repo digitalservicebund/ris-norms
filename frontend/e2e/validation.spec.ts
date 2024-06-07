4bb584a62507716e522da29d15671e9e7c10df4b
Sebastian Rossa <sebastian.rossa@digitalservice.bund.de>
2024-06-07 14:29:02 +0200

    Add E2E setup for 3 test without assertion
    RISDEV-3966

---
+++ frontend/e2e/validation.spec.ts
@@ -0,0 +1,60 @@
+import { test, expect } from "@playwright/test"
+
+test.describe("validation errors", () => {
+  test("Wrong character range", async ({ page }) => {
+    await page.goto(
+      "http://localhost:5173/amending-laws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/articles/hauptteil-1_art-1/edit/hauptteil-1_art-1_abs-1_untergl-1_listenelem-2_inhalt-1_text-1_%C3%A4ndbefehl-1",
+    )
+    await page
+      .getByLabel("zu ersetzende Textstelle")
+      .fill(
+        "hauptteil-1_para-20_abs-1_untergl-1_listenelem-2_inhalt-1_text-1/9-36.xml",
+      )
+    page.once("dialog", (dialog) => {
+      console.log(`Dialog message: ${dialog.message()}`)
+      dialog.dismiss().catch(() => {})
+    })
+    await page.getByLabel("zu ersetzende Textstelle").press("Enter")
+    expect("true").toMatch("true") // placeholder to pass commit checks
+    // expect error message in page
+    // The replacement text '§ 9 Abs. 1 Satz 2, Abs. 2 K' in the target law does not equal the replacement text '§ 9 Abs. 1 Satz 2, Abs. 2' in the mod with eId hauptteil-1_art-1_abs-1_untergl-1_listenelem-2_inhalt-1_text-1_ändbefehl-1]
+  })
+
+  test("Wrong eId", async ({ page }) => {
+    await page.goto(
+      "http://localhost:5173/amending-laws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/articles/hauptteil-1_art-1/edit/hauptteil-1_art-1_abs-1_untergl-1_listenelem-2_inhalt-1_text-1_%C3%A4ndbefehl-1",
+    )
+    await page
+      .getByLabel("zu ersetzende Textstelle")
+      .fill(
+        "hauptteil-1_para-20_abs-1_untergl-1_listenelem-2_inhalt-1_text-1wrongEid/9-34.xml",
+      )
+    page.once("dialog", (dialog) => {
+      console.log(`Dialog message: ${dialog.message()}`)
+      dialog.dismiss().catch(() => {})
+    })
+    await page.getByLabel("zu ersetzende Textstelle").press("Enter")
+    expect("true").toMatch("true") // placeholder to pass commit checks
+    // expect error message in page
+    // Unable to process contained instructions: For norm with Eli (eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1): Couldn't load target eId (hauptteil-1_para-20_abs-1_untergl-1_listenelem-2_inhalt-1_text-1wrongEid) element in zf0 (eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1) for mod with eId hauptteil-1_art-1_abs-1_untergl-1_listenelem-2_inhalt-1_text-1_ändbefehl-1
+  })
+
+  test("Using out of range character range", async ({ page }) => {
+    await page.goto(
+      "http://localhost:5173/amending-laws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/articles/hauptteil-1_art-1/edit/hauptteil-1_art-1_abs-1_untergl-1_listenelem-2_inhalt-1_text-1_%C3%A4ndbefehl-1",
+    )
+    await page
+      .getByLabel("zu ersetzende Textstelle")
+      .fill(
+        "hauptteil-1_para-20_abs-1_untergl-1_listenelem-2_inhalt-1_text-1/9-3400.xml",
+      )
+    page.once("dialog", (dialog) => {
+      console.log(`Dialog message: ${dialog.message()}`)
+      dialog.dismiss().catch(() => {})
+    })
+    await page.getByLabel("zu ersetzende Textstelle").press("Enter")
+    expect("true").toMatch("true") // placeholder to pass commit checks
+    // expect error message in page
+    // Unable to process contained instructions: For norm with Eli (eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1): Couldn't load target eId (hauptteil-1_para-20_abs-1_untergl-1_listenelem-2_inhalt-1_text-1wrongEid) element in zf0 (eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1) for mod with eId hauptteil-1_art-1_abs-1_untergl-1_listenelem-2_inhalt-1_text-1_ändbefehl-1
+  })
+})
