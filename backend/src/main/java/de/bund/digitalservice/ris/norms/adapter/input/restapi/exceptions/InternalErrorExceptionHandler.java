7d6aafcebd6cd169e8a124690f4918900b801a1f
Sebastian Reck <sebastian.reck@digitalservice.bund.de>
2024-06-10 10:27:54 +0200

    Return proper JSON on updateMod exception
    RISDEV-3824

--- backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/exceptions/InternalErrorExceptionHandler.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/exceptions/InternalErrorExceptionHandler.java
@@ -81,6 +81,7 @@ public class InternalErrorExceptionHandler {
 
     log.error("Unable to process contained instructions: {}", e.getMessage(), e);
 
-    return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(e.getMessage());
+    return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+        .body("{\"message\": \"%s\"}".formatted(e.getMessage()));
   }
 }
