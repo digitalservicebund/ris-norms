34b104cab53357df885cbc655db2187796b4ff45
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2024-06-12 17:10:33 +0200

    Add loading states for useArticles and delete unused code
    RISDEV-3804

--- frontend/src/services/articleService.spec.ts
+++ frontend/src/services/articleService.spec.ts
@@ -1,153 +1,69 @@
-import { beforeEach, describe, expect, it, vi } from "vitest"
+import { describe, expect, test, vi } from "vitest"
+import { nextTick, ref } from "vue"
+import { useArticles } from "@/services/articleService"
 
 describe("articleService", () => {
-  beforeEach(() => {
-    vi.resetModules()
-    vi.resetAllMocks()
-  })
-
-  describe("getArticlesByEli(eli)", () => {
-    it("provides the data from the api", async () => {
-      const fetchMock = vi.fn().mockResolvedValueOnce([
-        {
-          enumeration: "1",
-          eid: "article/eid",
-          title: "Example",
-          affectedDocumentEli: "article/eli",
-        },
-      ])
-
-      vi.doMock("./apiService.ts", () => ({ apiFetch: fetchMock }))
-
-      const { getArticlesByEli } = await import("./articleService")
-
-      const result = await getArticlesByEli("example/eli")
-
-      expect(result).toEqual([
-        {
-          enumeration: "1",
-          eid: "article/eid",
-          title: "Example",
-          affectedDocumentEli: "article/eli",
-        },
-      ])
-
-      expect(fetchMock).toHaveBeenCalledWith("/norms/example/eli/articles", {
-        query: {},
-      })
-    })
-
-    it("provides the data from the API with filters", async () => {
-      const fetchMock = vi.fn().mockResolvedValueOnce([
-        {
-          enumeration: "1",
-          eid: "article/eid",
-          title: "Example",
-          affectedDocumentEli: "article/eli",
-        },
-      ])
-
-      vi.doMock("./apiService.ts", () => ({ apiFetch: fetchMock }))
-
-      const { getArticlesByEli } = await import("./articleService")
-
-      const result = await getArticlesByEli("example/eli", {
-        amendedAt: "foo",
-        amendedBy: "bar",
-      })
-
-      expect(result).toEqual([
-        {
-          enumeration: "1",
-          eid: "article/eid",
-          title: "Example",
-          affectedDocumentEli: "article/eli",
-        },
-      ])
-
-      expect(fetchMock).toHaveBeenCalledWith("/norms/example/eli/articles", {
-        query: { amendedAt: "foo", amendedBy: "bar" },
-      })
-    })
-  })
-
-  describe("getArticleXmlByEliAndEid(identifier)", () => {
-    it("provides the data from the api", async () => {
-      const fetchMock = vi
-        .fn()
-        .mockResolvedValueOnce('<?xml version="1.0" encoding="UTF-8"?></xml>')
-
-      vi.doMock("./apiService.ts", () => ({ apiFetch: fetchMock }))
-
-      const { getArticleXmlByEliAndEid } = await import("./articleService")
-
-      const result = await getArticleXmlByEliAndEid({
-        eli: "example/eli",
-        eid: "example/eid",
-      })
-
-      expect(result).toEqual('<?xml version="1.0" encoding="UTF-8"?></xml>')
-
-      expect(fetchMock).toHaveBeenCalledWith(
-        "/norms/example/eli/articles/example/eid",
-        expect.objectContaining({
-          headers: expect.objectContaining({ Accept: "application/xml" }),
-        }),
+  describe("useArticles", () => {
+    test("should provide the articles", async () => {
+      const fetchSpy = vi.spyOn(global, "fetch").mockResolvedValueOnce(
+        new Response(
+          JSON.stringify([
+            {
+              eid: "article eid 1",
+              title: "article eid 1",
+              enumeration: "1",
+              affectedDocumentEli: "example/eli",
+            },
+          ]),
+        ),
       )
-    })
-  })
 
-  describe("getArticleRenderByEliAndEid(identifier, date)", () => {
-    it("provides the data from the api", async () => {
-      const fetchMock = vi.fn().mockResolvedValueOnce("<div></div>")
+      const eli = ref<string>(
+        "eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1",
+      )
+      const { data: articles, isFinished } = useArticles(eli)
+      await vi.waitUntil(() => isFinished.value)
 
-      vi.doMock("./apiService.ts", () => ({ apiFetch: fetchMock }))
+      expect(articles.value).toEqual([
+        {
+          eid: "article eid 1",
+          title: "article eid 1",
+          enumeration: "1",
+          affectedDocumentEli: "example/eli",
+        },
+      ])
 
-      const { getArticleRenderByEliAndEid } = await import("./articleService")
-
-      const result = await getArticleRenderByEliAndEid({
-        eli: "eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1",
-        eid: "hauptteil-1_art-1",
-      })
-
-      expect(result).toEqual("<div></div>")
-
-      expect(fetchMock).toHaveBeenCalledWith(
-        "/norms/eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1/articles/hauptteil-1_art-1",
+      expect(fetchSpy).toHaveBeenCalledWith(
+        `/api/v1/norms/eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1/articles`,
         expect.objectContaining({
-          query: {},
-          headers: expect.objectContaining({ Accept: "text/html" }),
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
         }),
       )
     })
 
-    it("provides the data from the api with at-date", async () => {
-      const fetchMock = vi.fn().mockResolvedValueOnce("<div></div>")
+    test("should load the articles when the ELI changes", async () => {
+      const fetchSpy = vi
+        .spyOn(global, "fetch")
+        .mockResolvedValueOnce(new Response())
 
-      vi.doMock("./apiService.ts", () => ({ apiFetch: fetchMock }))
+      const eli = ref<string>("0")
+      const { isFinished } = useArticles(eli)
+      await nextTick()
+      await vi.waitFor(() => isFinished.value)
 
-      const { getArticleRenderByEliAndEid } = await import("./articleService")
+      eli.value = "1"
+      await nextTick()
+      await vi.waitFor(() => isFinished.value)
 
-      const date = new Date(Date.UTC(2023, 11, 11, 1, 2, 3, 4))
-      const result = await getArticleRenderByEliAndEid(
-        {
-          eli: "eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1",
-          eid: "hauptteil-1_art-1",
-        },
-        date,
-      )
+      eli.value = "1"
+      await nextTick()
+      await vi.waitFor(() => isFinished.value)
 
-      expect(result).toEqual("<div></div>")
-
-      expect(fetchMock).toHaveBeenCalledWith(
-        "/norms/eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1/articles/hauptteil-1_art-1",
-        expect.objectContaining({
-          query: expect.objectContaining({
-            atIsoDate: "2023-12-11T01:02:03.004Z",
-          }),
-          headers: expect.objectContaining({ Accept: "text/html" }),
-        }),
-      )
+      expect(fetchSpy).toBeCalledTimes(2)
     })
   })
 })
--- frontend/src/services/articleService.ts
+++ frontend/src/services/articleService.ts
@@ -1,63 +1,25 @@
-import { apiFetch } from "@/services/apiService"
+import { useApiFetch } from "@/services/apiService"
 import { Article } from "@/types/article"
-import { LawElementIdentifier } from "@/types/lawElementIdentifier"
+import { computed, MaybeRefOrGetter, toValue } from "vue"
+import { UseFetchReturn } from "@vueuse/core"
 
 /**
- * Load the list of articles within a specific law. By default, this returns
- * the list of all articles. You can use the options to narrow this list based
- * on pending changes in the law, e.g. to only return articles that are going
- * to be changed by a specific amending law, or only return articles changed
- * at a specific time boundary.
- *
- * @param eli ELI of the law we want to get the articles from
- * @param options Optional additional filters and queries
+ * Get the data of all articles inside a norm.
+ * @param eli A reference to the ELI of the law for which the article data
+ *  will be returned. Changing the value of the reference will load the data for the
+ *  new ELI.
  */
-export async function getArticlesByEli(
-  eli: string,
-  options?: {
-    /** The eId of the time boundary at which the article was changed. */
-    amendedAt?: string
-    /** The ELI of the amending law changing the article. */
-    amendedBy?: string
-  },
-): Promise<Article[]> {
-  return apiFetch<Article[]>(`/norms/${eli}/articles`, {
-    query: { ...options },
-  })
-}
-
-/**
- * Load the xml version of an article within a specific law from the API.
- *
- * @param identifier The information required for identifying the article
- */
-export async function getArticleXmlByEliAndEid({
-  eli,
-  eid,
-}: LawElementIdentifier): Promise<string> {
-  return apiFetch<string>(`/norms/${eli}/articles/${eid}`, {
-    headers: {
-      Accept: "application/xml",
+export function useArticles(
+  eli: MaybeRefOrGetter<string>,
+): UseFetchReturn<Article[]> {
+  return useApiFetch(
+    computed(() => {
+      return `/norms/${toValue(eli)}/articles`
+    }),
+    {
+      refetch: true,
     },
-  })
-}
-
-/**
- * Load the rendered html version of an article within a specific law from the API.
- *
- * @param identifier The information required for identifying the article
- * @param at Date indicating which modifications should be applied before the HTML gets rendered and returned
- */
-export async function getArticleRenderByEliAndEid(
-  { eli, eid }: LawElementIdentifier,
-  at?: Date,
-): Promise<string> {
-  return apiFetch<string>(`/norms/${eli}/articles/${eid}`, {
-    query: {
-      atIsoDate: at?.toISOString(),
-    },
-    headers: {
-      Accept: "text/html",
-    },
-  })
+  )
+    .json()
+    .get()
 }
