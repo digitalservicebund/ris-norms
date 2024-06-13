c545b308d88af4414c4a5ac1ba55c1b2de84fe5a
Victor del Campo <victor.delcampo@digitalservice.bund.de>
2024-06-13 14:06:14 +0200

    Add RisCheckboxInput component
    RISDEV-4183

---
+++ frontend/src/components/controls/RisCheckboxInput.story.vue
@@ -0,0 +1,66 @@
+<script lang="ts" setup>
+import RisCheckboxInput from "./RisCheckboxInput.vue"
+</script>
+
+<template>
+  <Story :layout="{ type: 'grid', width: '250px' }">
+    <Variant title="Default regular size | checked">
+      <RisCheckboxInput
+        id="test-id"
+        :model-value="true"
+        label="Stammnorm"
+      ></RisCheckboxInput>
+    </Variant>
+    <Variant title="Default regular size | read-only and checked">
+      <RisCheckboxInput
+        id="test-id"
+        read-only
+        :model-value="true"
+        label="Änderungsnorm"
+      ></RisCheckboxInput>
+    </Variant>
+    <Variant title="Default regular size | unchecked">
+      <RisCheckboxInput id="test-id" label="Übergangsnorm"></RisCheckboxInput>
+    </Variant>
+    <Variant title="Default regular size | read-only and unchecked">
+      <RisCheckboxInput
+        id="test-id"
+        read-only
+        label="Stammnorm"
+      ></RisCheckboxInput>
+    </Variant>
+
+    <Variant title="Small size | checked">
+      <RisCheckboxInput
+        id="test-id"
+        :model-value="true"
+        size="small"
+        label="Stammnorm"
+      ></RisCheckboxInput>
+    </Variant>
+    <Variant title="Small size | read-only and checked">
+      <RisCheckboxInput
+        id="test-id"
+        read-only
+        size="small"
+        :model-value="true"
+        label="Änderungsnorm"
+      ></RisCheckboxInput>
+    </Variant>
+    <Variant title="Small size | unchecked">
+      <RisCheckboxInput
+        id="test-id"
+        size="small"
+        label="Übergangsnorm"
+      ></RisCheckboxInput>
+    </Variant>
+    <Variant title="Small size | read-only and unchecked">
+      <RisCheckboxInput
+        id="test-id"
+        read-only
+        size="small"
+        label="Stammnorm"
+      ></RisCheckboxInput>
+    </Variant>
+  </Story>
+</template>
