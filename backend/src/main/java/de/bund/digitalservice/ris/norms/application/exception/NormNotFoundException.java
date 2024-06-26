60949586c115d8ca652cca963f89dc33e879db43
Sebastian Rossa <sebastian.rossa@digitalservice.bund.de>
2024-06-26 14:31:50 +0200

    Remove unused constructor
    RISDEV-4024

--- backend/src/main/java/de/bund/digitalservice/ris/norms/application/exception/NormNotFoundException.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/application/exception/NormNotFoundException.java
@@ -1,14 +1,9 @@
 package de.bund.digitalservice.ris.norms.application.exception;
 
-import java.util.UUID;
-
 /** Indicates that the requested norm does not exist. */
 public class NormNotFoundException extends RuntimeException {
+
   public NormNotFoundException(final String eli) {
     super("Norm with eli %s does not exist".formatted(eli));
-  }
-
-  public NormNotFoundException(final UUID guid) {
-    super("Norm with GUID %s does not exist".formatted(guid));
   }
 }
