e0a9a098e96991417425fb4bf6962456f724946e
Sebastian Rossa <sebastian.rossa@digitalservice.bund.de>
2024-06-27 14:05:21 +0200

    Remove Getter from ValidationException
    RISDEV-4024

--- backend/src/main/java/de/bund/digitalservice/ris/norms/application/exception/ValidationException.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/application/exception/ValidationException.java
@@ -1,9 +1,6 @@
 package de.bund.digitalservice.ris.norms.application.exception;
 
-import lombok.Getter;
-
 /** This exception indicates that there was a validation error. */
-@Getter
 public class ValidationException extends RuntimeException {
 
   public ValidationException(String message) {
