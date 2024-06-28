dade7244e7dd67446ccc1fceb05eab0c828978cf
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2024-06-28 08:35:59 +0200

    feat: add radio button component
    RISDEV-4282

---
+++ frontend/src/components/controls/RisRadioInput.story.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import RisRadioInput from "./RisRadioInput.vue"
+</script>
+
+<template>
+  <!-- eslint-disable vuejs-accessibility/label-has-for -->
+  <Story :layout="{ type: 'grid' }">
+    <Variant title="Default | Small">
+      <label for="small-radio" class="flex items-center gap-8">
+        <RisRadioInput id="small-radio" />
+        Small radio
+      </label>
+    </Variant>
+
+    <Variant title="Medium">
+      <label for="medium-radio" class="flex items-center gap-8">
+        <RisRadioInput id="medium-radio" size="medium" />
+        Medium radio
+      </label>
+    </Variant>
+
+    <Variant title="Large">
+      <label for="large-radio" class="flex items-center gap-8">
+        <RisRadioInput id="large-radio" size="large" />
+        Large radio
+      </label>
+    </Variant>
+  </Story>
+</template>
---
+++ frontend/src/components/controls/RisRadioInput.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+const props = withDefaults(
+  defineProps<{
+    size?: "large" | "medium" | "small"
+  }>(),
+  {
+    size: "small",
+  },
+)
+
+const modelValue = defineModel<string>({ default: undefined })
+</script>
+
+<template>
+  <!-- Label should come from the surrounding context, e.g. InputField component -->
+  <!-- eslint-disable vuejs-accessibility/form-control-has-label -->
+  <input
+    v-model="modelValue"
+    class="ds-radio focus:!shadow-[inset_0_0_0_0.25rem] focus:!shadow-blue-800"
+    :class="{
+      'mr-8': props.size === 'large',
+      'ds-radio-small mr-4': props.size === 'medium',
+      'ds-radio-mini mr-2': props.size === 'small',
+    }"
+    type="radio"
+  />
+</template>
