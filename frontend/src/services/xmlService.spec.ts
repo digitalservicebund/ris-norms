b4052d2a28a80b77d43dcec94f5e099f2e0dc654
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2024-07-04 14:59:22 +0200

    Add method to evaluate XPath and return all results
    RISDEV-4272

--- frontend/src/services/xmlService.spec.ts
+++ frontend/src/services/xmlService.spec.ts
@@ -1,11 +1,12 @@
 import { describe, expect, it, vi } from "vitest"
 import {
+  evaluateXPath,
   evaluateXPathOnce,
   xmlNodeToString,
   xmlStringToDocument,
 } from "@/services/xmlService"
 
-// the evaluateXPathOnce method is mocked in the vitest-setup, to test it here we unmock it.
+// the evaluateXPathOnce and evaluateXPath methods are mocked in the vitest-setup, to test it here we unmock it.
 vi.unmock("@/services/xmlService")
 
 describe("xmlService", () => {
@@ -59,6 +60,26 @@ describe("xmlService", () => {
 
       const result = evaluateXPathOnce("//akn:act/@name", document)
       expect(result?.nodeValue).to.eq("regelungstext")
+    })
+  })
+
+  describe("evaluateXPath", () => {
+    it("should evaluate a xpath", () => {
+      const document =
+        xmlStringToDocument(`<?xml version="1.0" encoding="UTF-8"?>
+        <?xml-model href="../../../Grammatiken/legalDocML.de.sch" schematypens="http://purl.oclc.org/dsdl/schematron"?>
+        <akn:akomaNtoso xmlns:akn="http://Inhaltsdaten.LegalDocML.de/1.6/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://Metadaten.LegalDocML.de/1.6/ ../../../Grammatiken/legalDocML.de-metadaten.xsd                        http://Inhaltsdaten.LegalDocML.de/1.6/ ../../../Grammatiken/legalDocML.de-regelungstextverkuendungsfassung.xsd">
+          <akn:act name="regelungstext">
+          </akn:act>
+          <akn:act name="regelungstext-2">
+          </akn:act>
+        </akn:akomaNtoso>
+      `)
+
+      const result = evaluateXPath("//akn:act/@name", document)
+      expect(result).toHaveLength(2)
+      expect(result[0].nodeValue).to.eq("regelungstext")
+      expect(result[1].nodeValue).to.eq("regelungstext-2")
     })
   })
 })
