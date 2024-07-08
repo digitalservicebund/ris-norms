a4d7532b773abed9a4f4a38204ca5bbdeed6132b
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-07-08 12:46:22 +0200

    Revert "Adds sentry to the frontend"
    This reverts commit 96489c3749e52e7c0f327461f68221fbf2666194.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,23 +2,8 @@ import { createApp } from "vue"
 import "./style.css"
 import App from "./App.vue"
 import router from "./router"
-import * as Sentry from "@sentry/vue"
 import { initializeApiService } from "./services/apiService"
 
 initializeApiService(router)
 
-const app = createApp(App)
-
-Sentry.init({
-  app,
-  dsn: process.env.SENTRY_DSN,
-  integrations: [
-    Sentry.browserTracingIntegration({ router }),
-    Sentry.captureConsoleIntegration(),
-  ],
-  tracesSampleRate: 1.0,
-  attachProps: true,
-  logErrors: true,
-})
-
-app.use(router).mount("#app")
+createApp(App).use(router).mount("#app")
