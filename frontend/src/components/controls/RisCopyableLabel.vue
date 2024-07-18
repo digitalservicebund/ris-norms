b65a04e0b52e271edccecd7ba50db0d012f6f7f5
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2024-07-18 15:51:51 +0200

    feat: add label with copyable value
    RISDEV-4312

---
+++ frontend/src/components/controls/RisCopyableLabel.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import IcBaselineContentCopy from "~icons/ic/baseline-content-copy"
+import IcBaselineCheck from "~icons/ic/baseline-check"
+import { ref } from "vue"
+
+const props = withDefaults(
+  defineProps<{
+    /** Visible text. */
+    text: string
+
+    /**
+     * Value that should be copied. If no value is provided, copying will
+     * copy the `text` by default.
+     */
+    value?: string
+
+    /**
+     * Human-readable description of the value that should be copied. This
+     * will be used to provide an accessible label for the control.
+     *
+     * @default "Wert"
+     */
+    name?: string
+  }>(),
+  {
+    value: undefined,
+    name: "Wert",
+  },
+)
+
+const copySuccess = ref(false)
+
+async function copy() {
+  try {
+    await navigator.clipboard.writeText(props.value ?? props.text)
+    copySuccess.value = true
+    setTimeout(() => {
+      copySuccess.value = false
+    }, 1000)
+  } catch (err) {
+    console.error(err)
+  }
+}
+</script>
+
+<template>
+  <button
+    :aria-label="`${name} in die Zwischenablage kopieren`"
+    :title="`${name} in die Zwischenablage kopieren`"
+    class="ds-link-02-reg inline-flex items-center gap-4"
+    @click="copy()"
+  >
+    {{ text }}
+    <IcBaselineContentCopy v-if="!copySuccess" />
+    <IcBaselineCheck v-else />
+  </button>
+</template>
