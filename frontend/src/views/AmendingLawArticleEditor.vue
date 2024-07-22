d8d1d8210e7315b1adaa353a02a4df940c76e905
VictorDelCampo <victor.delcampo@digitalservice.bund.de>
2024-07-22 14:04:36 +0200

    Fix style frontend
    RISDEV-4411

--- frontend/src/views/AmendingLawArticleEditor.vue
+++ frontend/src/views/AmendingLawArticleEditor.vue
@@ -103,7 +103,7 @@ const breadcrumbs = ref<HeaderBreadcrumb[]>([
     key: "amendingLaw",
     title: () =>
       amendingLaw.value
-        ? getFrbrDisplayText(amendingLaw.value) ?? "..."
+        ? (getFrbrDisplayText(amendingLaw.value) ?? "...")
         : "...",
     to: `/amending-laws/${eli.value}/articles`,
   },
