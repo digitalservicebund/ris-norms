2c1cab08c6463577688186b17fe2039dd1996dce
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2024-08-09 10:15:50 +0200

    feat: add new supported elements to FE types
    RISDEV-4526

--- frontend/src/types/element.ts
+++ frontend/src/types/element.ts
@@ -1,15 +1,24 @@
-export type ElementType = "article" | "preface" | "preamble" | "conclusions"
+export type ElementType =
+  | "article"
+  | "book"
+  | "chapter"
+  | "conclusions"
+  | "part"
+  | "preamble"
+  | "preface"
+  | "subtitle"
+  | "title"
 
 /**
  * Describes an element inside a norm.
  */
 export type Element = {
-  /**
-   * Title of the element. May be empty string if no title can be inferred.
-   */
+  /** Title of the element. May be empty string if no title can be inferred. */
   title: string
+
   /** eId of the element. */
   eid: string
+
   /** Node name of the element. */
   type: ElementType
 }
