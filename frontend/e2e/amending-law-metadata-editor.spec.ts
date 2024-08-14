a9378de90a15b8e8c330a170d893735c04118dfc
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-08-14 12:35:01 +0200

    extend route pattern in e2e test to include new element types
    RISDEV-4527

--- frontend/e2e/amending-law-metadata-editor.spec.ts
+++ frontend/e2e/amending-law-metadata-editor.spec.ts
@@ -139,7 +139,7 @@ test.describe("sidebar navigation", () => {
     page,
   }) => {
     await page.route(
-      /elements\?type=(article|conclusions|preamble|preface|book|chapter|part|subtitle|title)(&type=(article|conclusions|preamble|preface|book|chapter|part|subtitle|title))*&amendedBy=eli/,
+      /elements\?type=article&type=conclusions&type=preamble&type=preface&type=book&type=chapter&type=part&type=subtitle&type=title&amendedBy=eli/,
       (route) => {
         route.abort()
       },
@@ -158,7 +158,7 @@ test.describe("sidebar navigation", () => {
 
   test("shows an empty state when no elements are found", async ({ page }) => {
     await page.route(
-      /elements\?type=(article|conclusions|preamble|preface|book|chapter|part|subtitle|title)(&type=(article|conclusions|preamble|preface|book|chapter|part|subtitle|title))*&amendedBy=eli/,
+      /elements\?type=article&type=conclusions&type=preamble&type=preface&type=book&type=chapter&type=part&type=subtitle&type=title&amendedBy=eli/,
       async (route) => {
         await route.fulfill({ status: 200, json: {} })
       },
