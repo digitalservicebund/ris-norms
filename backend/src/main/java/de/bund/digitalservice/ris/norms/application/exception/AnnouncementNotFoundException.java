aeb0d57f1850df2950de3667987582356a102ec5
Hannes Michalek <hannes.michalek@digitalservice.bund.de>
2024-08-30 11:09:35 +0200

    Fix JavaDoc and enhance error message
    RISDEV-4710

--- backend/src/main/java/de/bund/digitalservice/ris/norms/application/exception/AnnouncementNotFoundException.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/application/exception/AnnouncementNotFoundException.java
@@ -1,9 +1,9 @@
 package de.bund.digitalservice.ris.norms.application.exception;
 
-/** Indicates that the requested norm does not exist. */
+/** Indicates that the requested announcement does not exist. */
 public class AnnouncementNotFoundException extends RuntimeException {
 
   public AnnouncementNotFoundException(final String eli) {
-    super("Announcement with eli %s does not exist".formatted(eli));
+    super("Announcement for norm with eli %s does not exist".formatted(eli));
   }
 }
