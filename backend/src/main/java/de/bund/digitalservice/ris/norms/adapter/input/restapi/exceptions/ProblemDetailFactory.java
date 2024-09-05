fdb41a1846729da10fddb79707272b5a35a57ad6
VictorDelCampo <victor.delcampo@digitalservice.bund.de>
2024-09-05 11:22:56 +0200

    Adapt java docs
    RISDEV-4635

--- backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/exceptions/ProblemDetailFactory.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/exceptions/ProblemDetailFactory.java
@@ -10,28 +10,28 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 
 /**
- * Configuration class responsible for creating ProblemDetail instances for various exceptions. This
- * class provides a static method to create standardized ProblemDetail objects that conform to RFC
- * 9457 - Problem Details for HTTP APIs. Each exception type is mapped to a specific
- * ProblemDetailDescriptor that contains the problem's type URI and title.
+ * Configuration class responsible for creating {@link ProblemDetail} instances for various
+ * exceptions. This class provides a static method to create standardized {@link ProblemDetail}
+ * objects that conform to RFC 9457 - Problem Details for HTTP APIs. Each exception type is mapped
+ * to a specific type in form of a {@link URI} and a title
  */
 public class ProblemDetailFactory {
   /**
-   * Creates a ProblemDetail instance for the given exception and HTTP status. The ProblemDetail is
-   * populated with information from a predefined mapping based on the exception type.
+   * Creates a {@link ProblemDetail} instance for the given exception and HTTP status. The {@link
+   * ProblemDetail} is populated with information from a predefined mapping based on the exception
+   * type.
    *
-   * @param e The exception for which the ProblemDetail is being created.
+   * @param e The exception for which the {@link ProblemDetail} is being created.
    * @param status The HTTP status code associated with the exception.
-   * @return A ProblemDetail object containing information about the problem, including its type,
-   *     title, and detailed message.
+   * @return A {@link ProblemDetail} object containing information about the problem, including its
+   *     type, title, and detailed message.
    */
   public static ProblemDetail createProblemDetail(
       final NormsAppException e, final HttpStatus status) {
     final ProblemMapping problemMapping = ProblemMapping.getInstance(e.getClass());
-    ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, e.getMessage());
+    final ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, e.getMessage());
     problemDetail.setType(problemMapping.getType());
     problemDetail.setTitle(problemMapping.getTitle());
-    problemDetail.setDetail(e.getMessage());
     return problemDetail;
   }
 
@@ -52,7 +52,7 @@ public class ProblemDetailFactory {
      * Creates the Enum
      *
      * @param clazz - Class to create the enum
-     * @return A ProblemMapping enum
+     * @return A {@link ProblemMapping} enum
      */
     public static ProblemMapping getInstance(Class<? extends NormsAppException> clazz) {
       for (ProblemMapping mapping : ProblemMapping.values()) {
