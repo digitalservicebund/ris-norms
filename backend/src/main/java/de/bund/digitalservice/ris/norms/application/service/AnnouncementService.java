16ef5c6b581cce293d27da541aebe56d79746f0d
Sebastian Rossa <sebastian.rossa@digitalservice.bund.de>
2024-09-18 15:45:46 +0200

    Refactoring AnnouncementService
    RISDEV-4821

--- backend/src/main/java/de/bund/digitalservice/ris/norms/application/service/AnnouncementService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/application/service/AnnouncementService.java
@@ -17,9 +17,12 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.w3c.dom.Document;
 
 /**
  * Service class within the application core part of the backend. It is responsible for implementing
@@ -109,19 +112,8 @@ public class AnnouncementService
 
   @Override
   public Announcement createAnnouncement(CreateAnnouncementUseCase.Query query) throws IOException {
-    if (query.file().isEmpty() || !"text/xml".equals(query.file().getContentType())) {
-      throw new NotAXmlFileException(
-        query.file().getOriginalFilename(),
-        query.file().getContentType()
-      );
-    }
-
-    var xmlString = IOUtils.toString(query.file().getInputStream(), Charset.defaultCharset());
-    var document = XmlMapper.toDocument(xmlString);
-
-    if (!document.getDocumentElement().getTagName().equals("akn:akomaNtoso")) {
-      throw new NotLdmlDeXmlFileException(query.file().getOriginalFilename());
-    }
+    validateFileIsXml(query.file());
+    var document = convertFileToDocument(query.file());
     final String actString = billToActService.convert(document);
 
     // it throws an exception if the validation fails or the LDML.de Version is not 1.6
@@ -130,64 +122,107 @@ public class AnnouncementService
     var validatedNorm = ldmlDeValidator.parseAndValidate(actString);
     ldmlDeValidator.validateSchematron(validatedNorm);
 
-    var norm = Norm.builder().document(XmlMapper.toDocument(xmlString)).build();
+    var norm = Norm.builder().document(document).build();
 
-    var activeMods = norm
-      .getMeta()
-      .getAnalysis()
-      .map(Analysis::getActiveModifications)
-      .orElseGet(List::of);
-    var activeModDestinationElis = activeMods
-      .stream()
-      .map(TextualMod::getDestinationHref)
-      .flatMap(Optional::stream)
-      .map(Href::getEli)
-      .flatMap(Optional::stream)
-      .collect(Collectors.toSet());
+    var activeModDestinationElis = getActiveModDestinationElis(norm);
 
-    activeModDestinationElis.forEach(eli -> {
-      if (loadNormPort.loadNorm(new LoadNormPort.Command(eli)).isEmpty()) {
-        throw new ActiveModDestinationNormNotFoundException(norm.getEli(), eli);
-      }
-    });
+    validateTargetNormsExist(activeModDestinationElis, norm);
 
-    final boolean normExists = loadNormPort
-      .loadNorm(new LoadNormPort.Command(norm.getEli()))
-      .isPresent();
+    final boolean normFound = isNormRetrievableByEli(query.force(), norm);
 
-    if (normExists && !query.force()) {
-      throw new NormExistsAlreadyException(norm.getEli());
-    }
-
-    if (normExists) {
-      deleteAnnouncementByNormEliPort.deleteAnnouncementByNormEli(
-        new DeleteAnnouncementByNormEliPort.Command(norm.getEli())
-      );
-      activeModDestinationElis.forEach(eli ->
-        loadNormPort
-          .loadNorm(new LoadNormPort.Command(eli))
-          .ifPresent(targetNorm ->
-            targetNorm
-              .getMeta()
-              .getFRBRExpression()
-              .getFRBRaliasNextVersionId()
-              .ifPresent(uuid -> {
-                deleteNormByGuidPort.loadNormByGuid(new DeleteNormByGuidPort.Command(uuid));
-                targetNorm.getMeta().getFRBRExpression().deleteAliasNextVersionId();
-              })
-          )
-      );
+    if (normFound && query.force()) {
+      deleteAnnouncement(norm);
+      deleteTargetNormsZf0(activeModDestinationElis);
     } else if (
       loadNormByGuidPort.loadNormByGuid(new LoadNormByGuidPort.Command(norm.getGuid())).isPresent()
     ) {
       throw new NormWithGuidAlreadyExistsException(norm.getGuid());
     }
 
+    return saveNewAnnouncement(norm);
+  }
+
+  private void validateFileIsXml(MultipartFile file) {
+    if (file == null || file.isEmpty() || !"text/xml".equals(file.getContentType())) {
+      throw new NotAXmlFileException(
+        file != null ? file.getOriginalFilename() : "null",
+        file != null ? file.getContentType() : "null"
+      );
+    }
+  }
+
+  private Document convertFileToDocument(MultipartFile file) throws IOException {
+    var xmlString = IOUtils.toString(file.getInputStream(), Charset.defaultCharset());
+    var document = XmlMapper.toDocument(xmlString);
+
+    if (!document.getDocumentElement().getTagName().equals("akn:akomaNtoso")) {
+      throw new NotLdmlDeXmlFileException(file.getOriginalFilename());
+    }
+    return document;
+  }
+
+  private Set<String> getActiveModDestinationElis(Norm norm) {
+    var activeMods = norm
+      .getMeta()
+      .getAnalysis()
+      .map(Analysis::getActiveModifications)
+      .orElseGet(List::of);
+    return activeMods
+      .stream()
+      .map(TextualMod::getDestinationHref)
+      .flatMap(Optional::stream)
+      .map(Href::getEli)
+      .flatMap(Optional::stream)
+      .collect(Collectors.toSet());
+  }
+
+  private void validateTargetNormsExist(Set<String> activeModDestinationElis, Norm norm) {
+    activeModDestinationElis.forEach(eli -> {
+      if (loadNormPort.loadNorm(new LoadNormPort.Command(eli)).isEmpty()) {
+        throw new ActiveModDestinationNormNotFoundException(norm.getEli(), eli);
+      }
+    });
+  }
+
+  private boolean isNormRetrievableByEli(boolean forceOverwrite, Norm norm) {
+    final boolean normExists = loadNormPort
+      .loadNorm(new LoadNormPort.Command(norm.getEli()))
+      .isPresent();
+
+    if (normExists && !forceOverwrite) {
+      throw new NormExistsAlreadyException(norm.getEli());
+    }
+    return normExists;
+  }
+
+  private void deleteAnnouncement(Norm norm) {
+    deleteAnnouncementByNormEliPort.deleteAnnouncementByNormEli(
+      new DeleteAnnouncementByNormEliPort.Command(norm.getEli())
+    );
+  }
+
+  private void deleteTargetNormsZf0(Set<String> activeModDestinationElis) {
+    activeModDestinationElis.forEach(eli ->
+      loadNormPort
+        .loadNorm(new LoadNormPort.Command(eli))
+        .ifPresent(targetNorm ->
+          targetNorm
+            .getMeta()
+            .getFRBRExpression()
+            .getFRBRaliasNextVersionId()
+            .ifPresent(uuid -> {
+              deleteNormByGuidPort.loadNormByGuid(new DeleteNormByGuidPort.Command(uuid));
+              targetNorm.getMeta().getFRBRExpression().deleteAliasNextVersionId();
+            })
+        )
+    );
+  }
+
+  private Announcement saveNewAnnouncement(Norm norm) {
     var announcement = Announcement.builder().norm(norm).build();
     updateOrSaveAnnouncementPort.updateOrSaveAnnouncement(
       new UpdateOrSaveAnnouncementPort.Command(announcement)
     );
-
     return announcement;
   }
 }
