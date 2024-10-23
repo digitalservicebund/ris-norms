44b09b069f2ac3a67ea21967aa2e52b737fe93e5
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2024-10-23 08:34:51 +0200

    Update eli column to contain expression instead of manifestation elis
    RISDEV-5142

--- backend/src/main/resources/db/migration/V0.15__announcement_norm_one_to_many.sql
+++ backend/src/main/resources/db/migration/V0.15__announcement_norm_one_to_many.sql
@@ -1,3 +1,5 @@
 ALTER TABLE announcements
     DROP CONSTRAINT announcements_eli_fkey;
 
+-- Change eli column to contain expression instead of manifestation elis
+UPDATE announcements announcement SET eli = (SELECT norms.eli_expression FROM norms WHERE eli_manifestation = announcement.eli);
