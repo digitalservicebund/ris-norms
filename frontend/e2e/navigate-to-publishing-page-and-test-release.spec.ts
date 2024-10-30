cdd110d9d007363030892ff4fc39444ece37ff50
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2024-10-30 16:43:08 +0100

    Create some first e2e tests for publishing
    RISDEV-5228

--- frontend/e2e/navigate-to-publishing-page-and-test-release.spec.ts
+++ frontend/e2e/navigate-to-publishing-page-and-test-release.spec.ts
@@ -1,114 +1,83 @@
 import { test, expect, Page } from "@playwright/test"
-import { readFileSync } from "fs"
 
-test.describe("Publishing flow for an amending law", () => {
-  async function verifyPublicationTime(
-    page: Page,
-    expectedDate: string,
-    expectedTime: string,
-  ): Promise<void> {
-    const timeElement = page.getByRole("time")
-    await expect(timeElement).toHaveText(
-      `${expectedDate} um ${expectedTime} Uhr. Die aktuelle Version kann hier eingesehen werden: `,
-    )
-  }
-
-  async function downloadAndVerifyFile(
-    page: Page,
-    linkSelector: string,
-    expectedDownloadAttribute: string,
-    expectedContent: string,
-  ): Promise<void> {
-    const [download] = await Promise.all([
-      page.waitForEvent("download"),
-      page.click(linkSelector),
-    ])
-
-    expect(await download.suggestedFilename()).toBe(expectedDownloadAttribute)
-
-    const path = await download.path()
-    const fileContent = readFileSync(path, { encoding: "utf8" })
-    expect(fileContent).toContain(expectedContent)
-  }
-
-  test("navigate to publishing page for an amending law", async ({ page }) => {
-    await page.goto(
-      "/amending-laws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/publishing",
-    )
-
-    await expect(
-      page.getByText("Das Gesetz wurde noch nicht veröffentlicht."),
-    ).toBeVisible()
-
-    // Trigger the publication of the amending law
-    await page.getByRole("button", { name: "Jetzt abgeben" }).click()
-
-    const currentTimestamp = new Date()
-    const expectedDateString = currentTimestamp.toLocaleDateString("de-DE", {
-      dateStyle: "medium",
-    })
-    const expectedTimeString = currentTimestamp.toLocaleTimeString("de-DE", {
-      hour: "2-digit",
-      minute: "2-digit",
-    })
-
-    // Verify publication time
-    await verifyPublicationTime(page, expectedDateString, expectedTimeString)
-
-    // Verify Links
-    const amendingLawEli =
-      "eli_bund_bgbl-1_2017_s419_2017-03-15_1_deu_regelungstext-1.xml"
-    const targetLawEli =
-      "eli_bund_bgbl-1_1964_s593_1964-08-05_1_deu_regelungstext-1.xml"
-
-    const amendingLawLink = page.getByRole("link", {
-      name: `${amendingLawEli.replace(/_/g, "/")}`,
-    })
-
-    await expect(amendingLawLink).toHaveAttribute("download", amendingLawEli)
-
-    const targetLawLink = page.getByRole("link", {
-      name: `${targetLawEli.replace(/_/g, "/")}`,
-    })
-
-    await expect(targetLawLink).toHaveAttribute("download", targetLawEli)
-
-    // Download and verify files
-    const publishedAmendingLawExpectedContent =
-      'value="eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1"'
-    const publishedTargetLawExpectedContent =
-      'value="eli/bund/bgbl-1/1964/s593/regelungstext-1"'
-
-    await downloadAndVerifyFile(
+test.describe(
+  "Publishing flow for an announcement",
+  {
+    tag: ["@RISDEV-4708"],
+  },
+  () => {
+    test("navigate to publishing page for an unpublished announcement", async ({
       page,
-      `a[download='${amendingLawEli}']`,
-      amendingLawEli,
-      publishedAmendingLawExpectedContent,
-    )
-    await downloadAndVerifyFile(
-      page,
-      `a[download='${targetLawEli}']`,
-      targetLawEli,
-      publishedTargetLawExpectedContent,
-    )
+    }) => {
+      await page.goto(
+        "/amending-laws/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/publishing",
+      )
 
-    // Check new published time after republishing
-    await page.getByRole("button", { name: "Jetzt abgeben" }).click()
+      await expect(
+        page.getByText("Das Gesetz wurde noch nicht veröffentlicht."),
+      ).toBeVisible()
 
-    const newTimestamp = new Date()
-    const newExpectedDateString = newTimestamp.toLocaleDateString("de-DE", {
-      dateStyle: "medium",
-    })
-    const newExpectedTimeString = newTimestamp.toLocaleTimeString("de-DE", {
-      hour: "2-digit",
-      minute: "2-digit",
+      // Trigger the publication of the amending law
+      await expect(
+        page.getByRole("button", { name: "Jetzt abgeben" }),
+      ).toBeVisible()
     })
 
-    await page.reload()
-    await verifyPublicationTime(
-      page,
-      newExpectedDateString,
-      newExpectedTimeString,
-    )
-  })
-})
+    test("publishing a norm", async ({ page }) => {
+      await page.goto(
+        "/amending-laws/eli/bund/bgbl-1/2023/413/2023-12-29/1/deu/regelungstext-1/publishing",
+      )
+
+      await page.getByRole("button", { name: "Jetzt abgeben" }).click()
+
+      const currentTimestamp = new Date()
+      const expectedDateString = currentTimestamp.toLocaleDateString("de-DE", {
+        dateStyle: "medium",
+      })
+      const expectedTimeString = currentTimestamp.toLocaleTimeString("de-DE", {
+        hour: "2-digit",
+        minute: "2-digit",
+      })
+
+      // Verify publication time
+      await verifyPublicationTime(page, expectedDateString, expectedTimeString)
+
+      // Verify Links
+      await expect(
+        page.getByRole("link", {
+          name: `eli/bund/bgbl-1/2023/413/2023-12-29/1/deu/${new Date().toISOString().substring(0, 10)}/regelungstext-1.xml`,
+        }),
+      ).toBeVisible()
+      await expect(
+        page.getByRole("link", {
+          name: `eli/bund/bgbl-1/1990/s2954/2024-06-01/1/deu/${new Date().toISOString().substring(0, 10)}/regelungstext-1.xml`,
+        }),
+      ).toBeVisible()
+      await expect(
+        page.getByRole("link", {
+          name: `eli/bund/bgbl-1/1990/s2954/2023-12-30/1/deu/${new Date().toISOString().substring(0, 10)}/regelungstext-1.xml`,
+        }),
+      ).toBeVisible()
+      await expect(
+        page.getByRole("link", {
+          name: `eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/${new Date().toISOString().substring(0, 10)}/regelungstext-1.xml`,
+        }),
+      ).toBeVisible()
+
+      // TODO: (Malte Laukötter, 2024-10-30) check that further editing doesn't change published norms
+      // TODO: (Malte Laukötter, 2024-10-30) check that further editing & then publishing again does change published norms (at same date)
+      // TODO: (Malte Laukötter, 2024-10-30) check published file content?
+    })
+
+    async function verifyPublicationTime(
+      page: Page,
+      expectedDate: string,
+      expectedTime: string,
+    ): Promise<void> {
+      const timeElement = page.getByRole("time")
+      await expect(timeElement).toHaveText(
+        `${expectedDate} um ${expectedTime} Uhr. Die aktuelle Version kann hier eingesehen werden: `,
+      )
+    }
+  },
+)
