242bd0108656df24327af0f48fb08141dfac7e86
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2024-11-08 10:34:49 +0100

    refactor: Reorganize frontend folders and files as per ADR-13 RISDEV-4689 (#760)

---
+++ frontend/src/views/amending-law/affected-documents/references/References.view.vue
@@ -0,0 +1,148 @@
+<script setup lang="ts">
+import RisErrorCallout from "@/components/controls/RisErrorCallout.vue"
+import RisHeader, {
+  HeaderBreadcrumb,
+} from "@/components/controls/RisHeader.vue"
+import RisLoadingSpinner from "@/components/controls/RisLoadingSpinner.vue"
+import RisModRefsEditor from "@/views/amending-law/affected-documents/references/RisModRefsEditor.vue"
+import RisModSelectionPanel from "@/views/amending-law/affected-documents/references/RisModSelectionPanel.vue"
+import { useEliPathParameter } from "@/composables/useEliPathParameter"
+import { useNormXml } from "@/composables/useNormXml"
+import { getFrbrDisplayText } from "@/lib/frbr"
+import { useGetNorm } from "@/services/normService"
+import { computed, ref } from "vue"
+import { useRoute, useRouter } from "vue-router"
+
+/* -------------------------------------------------- *
+ * Previews                                           *
+ * -------------------------------------------------- */
+
+const amendingNormEli = useEliPathParameter()
+const {
+  data: amendingNorm,
+  isFetching: amendingNormIsLoading,
+  error: amendingNormError,
+} = useGetNorm(amendingNormEli)
+
+const affectedNormEli = useEliPathParameter("affectedDocument")
+const {
+  data: affectedNorm,
+  isFetching: affectedNormIsLoading,
+  error: affectedNormError,
+} = useGetNorm(affectedNormEli)
+
+/* -------------------------------------------------- *
+ * Data and states for editing                        *
+ * -------------------------------------------------- */
+
+const newNormXml = ref()
+
+const {
+  data: amendingNormXml,
+  isFetching: amendingNormXmlIsLoading,
+  error: amendingNormXmlError,
+  update: {
+    execute: save,
+    isFetching: isSaving,
+    isFinished: hasSaved,
+    error: saveError,
+  },
+} = useNormXml(amendingNormEli, newNormXml)
+
+function handleSave(xml: string) {
+  newNormXml.value = xml
+  save()
+}
+
+const router = useRouter()
+const route = useRoute()
+
+const selectedModEId = computed({
+  get() {
+    return route.params.modEid?.toString()
+  },
+  set(newEid) {
+    router.replace({
+      params: {
+        modEid: newEid ?? "",
+        refEid: "",
+      },
+    })
+  },
+})
+
+/* -------------------------------------------------- *
+ * Header                                             *
+ * -------------------------------------------------- */
+
+const breadcrumbs = ref<HeaderBreadcrumb[]>([
+  {
+    key: "amendingNorm",
+    title: () =>
+      amendingNorm.value
+        ? (getFrbrDisplayText(amendingNorm.value) ?? "...")
+        : "...",
+    to: `/amending-laws/${amendingNormEli.value}/affected-documents`,
+  },
+  {
+    key: "affectedNorm",
+    title: () => affectedNorm.value?.shortTitle ?? "...",
+  },
+  { key: "textMetadataEditor", title: "Textbasierte Metadaten" },
+])
+</script>
+
+<template>
+  <div class="flex h-[calc(100dvh-5rem-1px)] flex-col bg-gray-100">
+    <div
+      v-if="
+        amendingNormIsLoading ||
+        affectedNormIsLoading ||
+        amendingNormXmlIsLoading
+      "
+      class="flex h-full items-center justify-center"
+    >
+      <RisLoadingSpinner />
+    </div>
+
+    <div
+      v-else-if="amendingNormError || affectedNormError || amendingNormXmlError"
+      class="p-24"
+    >
+      <RisErrorCallout
+        :error="amendingNormError ?? affectedNormError ?? amendingNormXmlError"
+      />
+    </div>
+
+    <RisHeader v-else :breadcrumbs>
+      <div
+        class="grid flex-grow grid-cols-3 grid-rows-1 gap-32 overflow-hidden p-24"
+      >
+        <section aria-labelledby="changeCommandsHeading" class="flex flex-col">
+          <h3 id="changeCommandsHeading" class="ris-label2-bold mb-12 block">
+            Änderungsbefehle
+          </h3>
+          <RisModSelectionPanel
+            v-if="amendingNormXml"
+            v-model="selectedModEId"
+            :norm-xml="amendingNormXml"
+            :class="['overflow-hidden']"
+            data-testid="mod-selection-panel"
+          />
+        </section>
+
+        <RisModRefsEditor
+          v-if="amendingNormXml"
+          :norm-xml="amendingNormXml"
+          :selected-mod-e-id="selectedModEId"
+          :eli="amendingNormEli"
+          class="col-span-2 grid grid-cols-subgrid"
+          :has-saved="hasSaved"
+          :is-saving="isSaving"
+          :save-error="saveError"
+          @save="handleSave"
+        />
+      </div>
+    </RisHeader>
+  </div>
+</template>
---
+++ frontend/src/views/amending-law/affected-documents/references/RisModRefsEditor.vue
@@ -0,0 +1,178 @@
+<script setup lang="ts">
+import RisRefEditorTable from "@/views/amending-law/affected-documents/references/RisRefEditorTable.vue"
+import RisEmptyState from "@/components/controls/RisEmptyState.vue"
+import Button from "primevue/button"
+import RisRefSelectionPanel from "@/views/amending-law/affected-documents/references/RisRefSelectionPanel.vue"
+import { computed, ref, triggerRef, watch } from "vue"
+import {
+  evaluateXPathOnce,
+  xmlNodeToString,
+  xmlStringToDocument,
+} from "@/services/xmlService"
+import { getNodeByEid } from "@/services/ldmldeService"
+import { useRoute, useRouter } from "vue-router"
+import { useToast } from "primevue/usetoast"
+
+const props = defineProps<{
+  normXml: string
+  selectedModEId: string
+  isSaving: boolean
+  hasSaved: boolean
+  saveError: unknown
+}>()
+
+const emit = defineEmits<{
+  save: [string]
+}>()
+
+const amendingNormDocument = ref<Document>()
+
+watch(
+  [() => props.selectedModEId, () => props.normXml],
+  () => {
+    if (!props.normXml) {
+      return
+    }
+
+    amendingNormDocument.value = xmlStringToDocument(props.normXml ?? "")
+  },
+  { immediate: true },
+)
+
+const selectedModQuotedContent = computed(() => {
+  if (!props.selectedModEId || !amendingNormDocument.value) {
+    return null
+  }
+
+  const node = getNodeByEid(amendingNormDocument.value, props.selectedModEId)
+
+  if (!node) {
+    return null
+  }
+
+  return evaluateXPathOnce("./akn:quotedStructure | ./akn:quotedText[2]", node)
+})
+
+const selectedModQuotedContentXmlString = computed({
+  get() {
+    if (selectedModQuotedContent.value) {
+      return xmlNodeToString(selectedModQuotedContent.value)
+    }
+
+    return ""
+  },
+  set(newValue) {
+    const parentNode = selectedModQuotedContent.value?.parentNode
+    if (!parentNode) {
+      return
+    }
+
+    const newNode = xmlStringToDocument(newValue).firstChild
+    if (!newNode) {
+      return
+    }
+
+    parentNode.replaceChild(newNode, selectedModQuotedContent.value)
+    // we mutate the norm Document (as a side effect), so we need to trigger a recompute of all things that use it
+    triggerRef(amendingNormDocument)
+  },
+})
+
+const router = useRouter()
+const route = useRoute()
+
+const selectedRefEId = computed({
+  get() {
+    return route.params.refEid?.toString()
+  },
+  set(newEid) {
+    router.replace({
+      params: {
+        refEid: newEid ?? "",
+      },
+    })
+  },
+})
+
+function handleSave() {
+  if (!amendingNormDocument.value) {
+    return
+  }
+
+  emit("save", xmlNodeToString(amendingNormDocument.value))
+  showToast()
+}
+
+const { add: addToast } = useToast()
+
+function showToast() {
+  if (props.hasSaved) {
+    if (props.saveError) {
+      addToast({
+        summary: "Speichern fehlgeschlagen",
+        severity: "error",
+      })
+    } else {
+      addToast({
+        summary: "Gespeichert!",
+        severity: "success",
+      })
+    }
+  }
+}
+
+watch(
+  () => props.hasSaved,
+  (hasSaved) => {
+    if (hasSaved) {
+      showToast()
+    }
+  },
+)
+</script>
+
+<template>
+  <div class="grid grid-rows-[minmax(0,max-content),max-content,max-content]">
+    <section aria-labelledby="textBasedMetadataHeading" class="flex flex-col">
+      <h3 id="textBasedMetadataHeading" class="ris-label2-bold mb-12 block">
+        Textbasierte Metadaten
+      </h3>
+      <RisRefSelectionPanel
+        v-if="selectedModQuotedContentXmlString"
+        v-model:selected-ref="selectedRefEId"
+        v-model:xml-snippet="selectedModQuotedContentXmlString"
+        class="overflow-hidden"
+      />
+      <RisEmptyState
+        v-else
+        text-content="Wählen Sie links einen Änderungsbefehl zur Dokumentation von textbasierten Metadaten aus."
+      />
+    </section>
+    <section
+      v-if="selectedModQuotedContentXmlString"
+      aria-labelledby="referencesHeading"
+      class="flex flex-col"
+    >
+      <h3 id="referencesHeading" class="ris-label2-bold mb-12 block">
+        Verweise
+      </h3>
+      <RisRefEditorTable
+        v-if="selectedModQuotedContentXmlString"
+        v-model:selected-ref="selectedRefEId"
+        v-model:xml-snippet="selectedModQuotedContentXmlString"
+        class="overflow-hidden"
+      />
+    </section>
+
+    <hr class="col-span-2 mb-16 mt-32 border border-solid border-gray-400" />
+
+    <div class="col-span-2 flex flex-row-reverse">
+      <Button
+        :disabled="isSaving"
+        :loading="isSaving"
+        label="Speichern"
+        @click="handleSave"
+      />
+    </div>
+  </div>
+</template>
