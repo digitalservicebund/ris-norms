139437e9b8e353362853f39b740c988879208e88
VictorDelCampo <victor.delcampo@digitalservice.bund.de>
2024-11-27 15:54:06 +0100

    Remove namespaces and use local-name
    RISDEV-5537

--- backend/src/main/resources/db/migration/V0.20__ldml_1.7.1.sql
+++ backend/src/main/resources/db/migration/V0.20__ldml_1.7.1.sql
@@ -1,35 +1,33 @@
--- once we support psql 17 this can be solved simpler, see: https://dba.stackexchange.com/questions/250868/how-to-change-definition-of-a-postgres-generated-stored-column
-ALTER TABLE release_norms DROP CONSTRAINT fk_norm_eli_manifestation;
+-- This approach avoids declaring specific namespaces and uses local-name() for flexibility.
 
-ALTER TABLE norms DROP COLUMN eli_manifestation;
-
+ALTER TABLE release_norms
+    DROP CONSTRAINT fk_norm_eli_manifestation;
+ALTER TABLE norms
+    DROP COLUMN eli_manifestation;
 ALTER TABLE norms
     ADD COLUMN eli_manifestation text GENERATED ALWAYS AS ((xpath(
-            '//akn:act/akn:meta/akn:identification/akn:FRBRManifestation/akn:FRBRthis/@value', xml,
-            '{{akn,http://Inhaltsdaten.LegalDocML.de/1.7.1/}}'))[1]::text) STORED UNIQUE NOT NULL;
+            '//*[local-name()="act"]/*[local-name()="meta"]/*[local-name()="identification"]/*[local-name()="FRBRManifestation"]/*[local-name()="FRBRthis"]/@value',
+            xml))[1]::text) STORED UNIQUE NOT NULL;
+ALTER TABLE release_norms
+    ADD CONSTRAINT fk_norm_eli_manifestation FOREIGN KEY (norm_eli_manifestation) REFERENCES norms (eli_manifestation);
 
-ALTER TABLE release_norms ADD CONSTRAINT fk_norm_eli_manifestation
-    FOREIGN KEY (norm_eli_manifestation)
-        REFERENCES norms (eli_manifestation);
-
-ALTER TABLE norms DROP COLUMN eli_expression;
-
+ALTER TABLE norms
+    DROP COLUMN eli_expression;
 ALTER TABLE norms
     ADD COLUMN eli_expression text GENERATED ALWAYS AS ((xpath(
-            '//akn:act/akn:meta/akn:identification/akn:FRBRExpression/akn:FRBRthis/@value', xml,
-            '{{akn,http://Inhaltsdaten.LegalDocML.de/1.7.1/}}'))[1]::text) STORED NOT NULL;
+            '//*[local-name()="act"]/*[local-name()="meta"]/*[local-name()="identification"]/*[local-name()="FRBRExpression"]/*[local-name()="FRBRthis"]/@value',
+            xml))[1]::text) STORED NOT NULL;
 
-ALTER TABLE norms DROP COLUMN guid;
-
+ALTER TABLE norms
+    DROP COLUMN guid;
 ALTER TABLE norms
     ADD COLUMN guid uuid GENERATED ALWAYS AS ((xpath(
-            '//akn:act/akn:meta/akn:identification/akn:FRBRExpression/akn:FRBRalias[@name="aktuelle-version-id"]/@value',
-            xml,
-            '{{akn,http://Inhaltsdaten.LegalDocML.de/1.7.1/}}'))[1]::text::uuid) STORED NOT NULL;
-
-ALTER TABLE norms DROP COLUMN eli_work;
+            '//*[local-name()="act"]/*[local-name()="meta"]/*[local-name()="identification"]/*[local-name()="FRBRExpression"]/*[local-name()="FRBRalias"][@name="aktuelle-version-id"]/@value',
+            xml))[1]::text::uuid) STORED NOT NULL;
 
 ALTER TABLE norms
+    DROP COLUMN eli_work;
+ALTER TABLE norms
     ADD COLUMN eli_work text GENERATED ALWAYS AS ((xpath(
-            '//akn:act/akn:meta/akn:identification/akn:FRBRWork/akn:FRBRthis/@value', xml,
-            '{{akn,http://Inhaltsdaten.LegalDocML.de/1.7.1/}}'))[1]::text) STORED NOT NULL;
+            '//*[local-name()="act"]/*[local-name()="meta"]/*[local-name()="identification"]/*[local-name()="FRBRWork"]/*[local-name()="FRBRthis"]/@value',
+            xml))[1]::text) STORED NOT NULL;
