8409088225b8d2af536c7b89b600e96ede9e74e7
VictorDelCampo <victor.delcampo@digitalservice.bund.de>
2025-01-14 11:20:04 +0100

    Keep fallback controller to local profile
    RISDEV-0000

--- backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/FrontendFallbackController.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/FrontendFallbackController.java
@@ -1,5 +1,6 @@
 package de.bund.digitalservice.ris.norms.adapter.input.restapi.controller;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
  * It ensures that the Vue.js single-page application (SPA) can take over client-side routing.
  */
 @Controller
+@Profile("local")
 public class FrontendFallbackController {
 
   /**
