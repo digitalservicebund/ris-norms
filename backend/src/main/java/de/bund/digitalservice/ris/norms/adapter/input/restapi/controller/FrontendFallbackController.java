88be24d6c58810f787eba81e29e4f39eae31f088
VictorDelCampo <victor.delcampo@digitalservice.bund.de>
2025-01-14 11:34:44 +0100

    Keep fallback controller to all envs
    RISDEV-0000

--- backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/FrontendFallbackController.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/FrontendFallbackController.java
@@ -1,6 +1,5 @@
 package de.bund.digitalservice.ris.norms.adapter.input.restapi.controller;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
  * It ensures that the Vue.js single-page application (SPA) can take over client-side routing.
  */
 @Controller
-@Profile("local")
 public class FrontendFallbackController {
 
   /**
