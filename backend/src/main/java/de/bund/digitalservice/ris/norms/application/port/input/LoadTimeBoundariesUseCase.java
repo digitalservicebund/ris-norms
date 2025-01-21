a8dc2cdab569050c7fc855d574d39a41e3c0d18e
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2025-01-21 14:29:16 +0100

    refactor: adapt timeboundaries-related code to use Regelungstext entity
    RISDEV-6259

--- backend/src/main/java/de/bund/digitalservice/ris/norms/application/port/input/LoadTimeBoundariesUseCase.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/application/port/input/LoadTimeBoundariesUseCase.java
@@ -6,22 +6,22 @@ import java.util.List;
 
 /**
  * Interface representing the use case for loading a list of {@link TimeBoundary}. Implementations
- * of this interface should provide functionality to load all time boundaries related to a norm
+ * of this interface should provide functionality to load all time boundaries related to a regelungstext
  * based on a given query.
  */
 public interface LoadTimeBoundariesUseCase {
   /**
-   * Retrieves a list of time boundaries related to the specified norm based on the provided query.
+   * Retrieves a list of time boundaries related to the specified regelungstext based on the provided query.
    *
-   * @param query The query containing the ELI (European Legislation Identifier) of the norm.
-   * @return A list of {@link TimeBoundary} entities related to the specified norm.
+   * @param query The query containing the ELI (European Legislation Identifier) of the regelungstext.
+   * @return A list of {@link TimeBoundary} entities related to the specified regelungstext.
    */
-  List<TimeBoundary> loadTimeBoundariesOfNorm(Query query);
+  List<TimeBoundary> loadTimeBoundariesFromRegelungstext(Query query);
 
   /**
-   * A record representing the parameters needed to query time boundaries related to a norm.
+   * A record representing the parameters needed to query time boundaries related to a regelungstext.
    *
-   * @param eli The ELI used to identify the norm in the query.
+   * @param eli The ELI used to identify the regelungstext in the query.
    */
   record Query(DokumentExpressionEli eli) {}
 }
--- backend/src/main/java/de/bund/digitalservice/ris/norms/application/port/input/UpdateTimeBoundariesUseCase.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/application/port/input/UpdateTimeBoundariesUseCase.java
@@ -7,22 +7,22 @@ import java.util.List;
 
 /**
  * Interface representing the use case for updating a list of {@link TimeBoundary}. Implementations
- * of this interface should provide functionality to update all time boundaries related to a norm
+ * of this interface should provide functionality to update all time boundaries related to a regelungstext
  * based on a given query.
  */
 public interface UpdateTimeBoundariesUseCase {
   /**
-   * Updates a list of time boundaries related to the specified norm based on the provided query.
+   * Updates a list of time boundaries related to the specified regelungstext based on the provided query.
    *
-   * @param query The query containing the ELI (European Legislation Identifier) of the norm.
-   * @return A list of {@link TimeBoundary} entities related to the specified norm.
+   * @param query The query containing the ELI (European Legislation Identifier) of the regelungstext.
+   * @return A list of {@link TimeBoundary} entities related to the specified regelungstext.
    */
-  List<TimeBoundary> updateTimeBoundariesOfNorm(Query query);
+  List<TimeBoundary> updateTimeBoundariesOfRegelungstext(Query query);
 
   /**
-   * A record representing the parameters needed to update time boundaries related to a norm.
+   * A record representing the parameters needed to update time boundaries related to a regelungstext.
    *
-   * @param eli The ELI used to identify the norm in the query.
+   * @param eli The ELI used to identify the regelungstext in the query.
    * @param timeBoundaries The list of the changed time boundaries.
    */
   record Query(DokumentExpressionEli eli, List<TimeBoundaryChangeData> timeBoundaries) {}
