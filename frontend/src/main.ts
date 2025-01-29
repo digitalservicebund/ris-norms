5b8523204d62cf31c8863e06f0fcc0722ab2d071
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2025-01-29 12:13:43 +0100

    fix: re-commit broken main.ts
    Somehow lefthook messed this file up when some validation failed :(
    
    RISDEV-6420

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,13 @@
+import { Sentry } from "@/plugins/sentry"
 import "@digitalservicebund/ris-ui/fonts.css"
 import { RisUiLocale, RisUiTheme } from "@digitalservicebund/ris-ui/primevue"
 import "@digitalservicebund/ris-ui/primevue/style.css"
-import * as Sentry from "@sentry/vue"
 import PrimeVue from "primevue/config"
 import ConfirmationService from "primevue/confirmationservice"
 import ToastService from "primevue/toastservice"
 import { createApp } from "vue"
 import App from "./App.vue"
-import { useAuthentication, auth } from "./lib/auth"
+import { useAuthentication } from "./lib/auth"
 import router from "./router"
 import "./style.css"
 
@@ -20,6 +20,8 @@ const app = createApp(App)
   .use(ToastService)
   .use(ConfirmationService)
   .use(router)
+  .use(Sentry, { environmentName: "local", router })
+
 const auth = useAuthentication()
 
 await auth.configure({
@@ -28,34 +30,4 @@ await auth.configure({
   realm: "ris",
 })
 
-const env = detectEnv()
-
-const enableSentry = [
-  RisEnvironment.PRODUCTION,
-  RisEnvironment.UAT,
-  RisEnvironment.STAGING,
-].includes(env)
-
-console.info(
-  `Sentry reporting is ${enableSentry ? "enabled" : "disabled"} in environment "${env}"`,
-)
-
-if (enableSentry) {
-  Sentry.init({
-    app,
-    environment: env,
-    dsn: "https://bc002a52fd187905497284bed2d771c1@o1248831.ingest.us.sentry.io/4507543284613120",
-    initialScope: { tags: { source: "frontend" } },
-    integrations: [
-      Sentry.browserTracingIntegration({ router }),
-      Sentry.captureConsoleIntegration(),
-    ],
-    tracesSampleRate: 0.01,
-    attachProps: true,
-    logErrors: true,
-  })
-}
-
 app.mount("#app")
-
-console.log(auth.isAuthenticated())
