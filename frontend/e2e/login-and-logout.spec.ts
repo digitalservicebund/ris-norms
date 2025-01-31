051050dda4504aa430f6823f10724134cf964460
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2025-01-31 11:45:52 +0100

    test: partially fix login/logout test
    It still fails because logging out is not implemented yet.
    
    RISDEV-6410

--- frontend/e2e/login-and-logout.spec.ts
+++ frontend/e2e/login-and-logout.spec.ts
@@ -2,15 +2,11 @@ import { test } from "@e2e/utils/test-with-auth"
 import { expect } from "@playwright/test"
 
 test.describe("Login / logout functionality", { tag: ["@RISDEV-5654"] }, () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto("/")
-  })
-
   test("Clicking logout should redirect to the login UI and verify user info after login", async ({
     page,
   }) => {
     await page.goto("/")
-    await page.getByRole("link").click()
+
     await expect(
       page.getByRole("heading", { name: "Sign in to your account" }),
     ).toBeVisible()
@@ -21,7 +17,7 @@ test.describe("Login / logout functionality", { tag: ["@RISDEV-5654"] }, () => {
     await page.getByRole("textbox", { name: "Password" }).fill("test")
     await page.getByRole("button", { name: "Sign In" }).click()
 
-    await page.goto("/amending-laws")
+    await page.waitForURL(/\/amending-laws/)
     const logoutLinkAfterLogin = page.getByRole("link", { name: "Ausloggen" })
     await expect(logoutLinkAfterLogin).toBeVisible()
 
