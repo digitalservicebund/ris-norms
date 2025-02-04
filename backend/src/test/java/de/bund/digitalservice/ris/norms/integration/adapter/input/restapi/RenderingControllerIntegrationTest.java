8d559f6d57eb1ed00e89982c461c46ac5b8f1f71
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-02-04 16:22:48 +0100

    Remove timemachine from rendering controller
    RISDEV-0000

--- backend/src/test/java/de/bund/digitalservice/ris/norms/integration/adapter/input/restapi/RenderingControllerIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/integration/adapter/input/restapi/RenderingControllerIntegrationTest.java
@@ -4,7 +4,6 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import de.bund.digitalservice.ris.norms.adapter.output.database.repository.DokumentRepository;
 import de.bund.digitalservice.ris.norms.domain.entity.Fixtures;
@@ -32,7 +31,7 @@ class RenderingControllerIntegrationTest extends BaseIntegrationTest {
   }
 
   @Nested
-  class getHtmlPreviewWithCustomRegelungstexte {
+  class getHtmlPreview {
 
     @Test
     void itReturnsRender() throws Exception {
@@ -42,9 +41,6 @@ class RenderingControllerIntegrationTest extends BaseIntegrationTest {
         "regelungstext",
         Fixtures.loadTextFromDisk("NormWithPassiveModifications.xml")
       );
-      var customRegelungstexteJson = new JsonArray();
-      customRegelungstexteJson.add(Fixtures.loadTextFromDisk("NormWithMods.xml"));
-      jsonPayload.add("customRegelungstexte", customRegelungstexteJson);
 
       // When // Then
       mockMvc
@@ -57,81 +53,7 @@ class RenderingControllerIntegrationTest extends BaseIntegrationTest {
         .andExpect(status().isOk())
         .andExpect(
           xpath("//*[@data-eId='hauptteil-1_art-1_abs-1_untergl-1_listenelem-2_inhalt-1_text-1']")
-            .string(containsString("§ 9 Absatz 1 Satz 2, Absatz 2 oder 3"))
-        );
-    }
-
-    @Test
-    void itReturnsRenderOfQuotedStructure() throws Exception {
-      // Given
-      var jsonPayload = new JsonObject();
-      jsonPayload.addProperty(
-        "regelungstext",
-        Fixtures.loadTextFromDisk("NormWithPassiveModsQuotedStructure.xml")
-      );
-      var customRegelungstexteJson = new JsonArray();
-      customRegelungstexteJson.add(Fixtures.loadTextFromDisk("NormWithQuotedStructureMods.xml"));
-      jsonPayload.add("customRegelungstexte", customRegelungstexteJson);
-
-      // When // Then
-      mockMvc
-        .perform(
-          post("/api/v1/renderings?atIsoDate=1002-11-01T00:00:00.0Z")
-            .accept(MediaType.TEXT_HTML)
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(jsonPayload.toString())
-        )
-        .andExpect(status().isOk())
-        .andExpect(
-          xpath("//*[@data-eId='einleitung-1_doktitel-1_text-1_doctitel-1']")
-            .string(
-              containsString(
-                "Fiktives Beispielgesetz für das Ersetzen von Strukturen und Gliederungseinheiten mit\n" +
-                "                  Änderungsbefehlen"
-              )
-            )
-        )
-        .andExpect(
-          xpath("//*[@data-eId='preambel-1_ernormen-1_ernorm-1_text-1']")
-            .string(
-              containsString(
-                "Aufgrund der Spezifikation von Änderungsbefehlen können Strukturen und Gliederungseinheiten\n" +
-                "                  ersetzt werden."
-              )
-            )
-        )
-        .andExpect(
-          xpath("//*[@data-eId='preambel-1_präambeln-1_präambelinh-1_text-1']")
-            .string(
-              containsString(
-                "Im Bewusstsein der Herausforderungen bei der Umsetzung von Änderungsbefehlen hat der\n" +
-                "                  Deutsche Bundestag das nachstehende Gesetz beschlossen."
-              )
-            )
-        )
-        .andExpect(
-          xpath("//*[@data-eId='preambel-1_blockcontainer-1_inhuebs-1_eintrag-1_span-1']")
-            .string(containsString("Neue Inhaltsübersicht."))
-        )
-        .andExpect(
-          xpath("//*[@data-eId='hauptteil-1_art-2_abs-1_inhalt-1_text-1']")
-            .string(
-              containsString(
-                "Dieses Gesetz findet Anwendung auf fast alle definierten Struktur und Gliederungsebenen."
-              )
-            )
-        )
-        .andExpect(
-          xpath(
-            "//*[@data-eId='hauptteil-1_art-2_abs-3_untergl-1_listenelem-2_untergl-1_listenelem-1_inhalt-1_text-1']"
-          )
-            .string(containsString("Zeilen,"))
-        )
-        .andExpect(
-          xpath(
-            "//*[@data-eId='hauptteil-1_art-2_abs-3_untergl-1_listenelem-2_untergl-1_listenelem-2_inhalt-1_text-1']"
-          )
-            .string(containsString("oder Zellen."))
+            .string(containsString("§ 9 Abs. 1 Satz 2, Abs. 2"))
         );
     }
   }
--- frontend/e2e/amending-laws-article-editor-quoted-structure.spec.ts
+++ frontend/e2e/amending-laws-article-editor-quoted-structure.spec.ts
@@ -79,44 +79,6 @@ test.describe("Load mod details", () => {
       "Fiktives Beispielgesetz für das Ersetzen von Strukturen und Gliederungseinheiten mit Änderungsbefehlen (Strukturänderungsgesetz)",
     )
   })
-
-  test("Display preview of time machine", async () => {
-    const amendingLawSection = sharedPage.getByRole("region", {
-      name: "Änderungsbefehle",
-    })
-    const previewSection = sharedPage.getByRole("region", {
-      name: "Vorschau",
-    })
-
-    await amendingLawSection.getByText("Fiktives Beispielgesetz").click()
-
-    await expect(
-      previewSection.getByText(
-        "Beispielgesetz für das Ersetzen von Strukturen und Gliederungseinheiten (Strukturänderungsgesetz)",
-      ),
-    ).toBeHidden()
-
-    await expect(
-      previewSection.getByText(
-        "Fiktives Beispielgesetz für das Ersetzen von Strukturen und Gliederungseinheiten mit Änderungsbefehlen (Strukturänderungsgesetz)",
-      ),
-    ).toBeVisible()
-
-    await previewSection.getByRole("tab", { name: "xml" }).click()
-
-    // Do a search
-    await previewSection
-      .getByText('<?xml version="1.0" encoding="UTF-8"?>')
-      .click()
-    await sharedPage.keyboard.press("ControlOrMeta+f")
-    await previewSection
-      .getByRole("textbox", { name: "Find" })
-      .fill("mit Änderungsbefehlen")
-    await previewSection.getByRole("button", { name: "next" }).click()
-    await expect(
-      previewSection.getByText("mit Änderungsbefehlen").first(),
-    ).toBeVisible()
-  })
 })
 
 test.describe("Editing a single mod", () => {
@@ -156,63 +118,6 @@ test.describe("Editing a single mod", () => {
       sharedPage.getByRole("combobox", { name: "Zeitgrenze" }),
     ).toHaveText("10.01.1002")
   })
-
-  test("select another target node and see preview", async () => {
-    const amendingLawSection = sharedPage.getByRole("region", {
-      name: "Änderungsbefehle",
-    })
-    const modFormSection = sharedPage.getByRole("region", {
-      name: "Änderungsbefehl bearbeiten",
-    })
-    const previewSection = sharedPage.getByRole("region", {
-      name: "Vorschau",
-    })
-
-    await amendingLawSection.getByText("Fiktives Beispielgesetz").click()
-
-    const elementToBeReplacedField = sharedPage.getByTestId(
-      "elementToBeReplaced",
-    )
-
-    await elementToBeReplacedField
-      .getByRole("button", {
-        name: "heading",
-        exact: true,
-      })
-      .and(
-        elementToBeReplacedField.getByText("Strukturen und Gliederungsebenen"),
-      )
-      .click()
-
-    await modFormSection.getByRole("button", { name: "Speichern" }).click()
-
-    await expect(
-      previewSection.getByText(
-        "Beispielgesetz für das Ersetzen von Strukturen und Gliederungseinheiten (Strukturänderungsgesetz)",
-      ),
-    ).toBeVisible()
-
-    await expect(
-      previewSection.getByText(
-        "Fiktives Beispielgesetz für das Ersetzen von Strukturen und Gliederungseinheiten mit Änderungsbefehlen (Strukturänderungsgesetz)",
-      ),
-    ).toBeVisible()
-
-    await previewSection.getByRole("tab", { name: "xml" }).click()
-
-    // Do a search
-    await previewSection
-      .getByText('<?xml version="1.0" encoding="UTF-8"?>')
-      .click()
-    await sharedPage.keyboard.press("ControlOrMeta+f")
-    await previewSection
-      .getByRole("textbox", { name: "Find" })
-      .fill("<akn:longTitle")
-    await previewSection.getByRole("button", { name: "next" }).click()
-    await expect(
-      previewSection.getByText("<akn:longTitle").first(),
-    ).toBeVisible()
-  })
 })
 
 test.describe("Editing multiple mods", () => {
@@ -247,27 +152,6 @@ test.describe("Editing multiple mods", () => {
     await timeBoundariesElement.click()
     const timeBoundaryOptionElements = sharedPage.getByRole("option")
     await expect(timeBoundaryOptionElements).toHaveCount(4)
-  })
-
-  test("rendering the preview by default when clicking on a command", async () => {
-    const amendingLawSection = sharedPage.getByRole("region", {
-      name: "Änderungsbefehle",
-    })
-
-    await amendingLawSection.getByText("Fiktives Beispielgesetz").click()
-    await amendingLawSection
-      .getByText("Aufgrund der Spezifikation")
-      .click({ modifiers: ["ControlOrMeta"] })
-
-    const previewSection = sharedPage.getByRole("region", {
-      name: "Vorschau",
-    })
-
-    await expect(
-      previewSection.getByText(
-        "Fiktives Beispielgesetz für das Ersetzen von Strukturen und Gliederungseinheiten mit Änderungsbefehlen (Strukturänderungsgesetz)",
-      ),
-    ).toBeVisible()
   })
 
   test("selecting and saving the time boundary", async () => {
@@ -345,104 +229,6 @@ test.describe("Range mod", () => {
       "bgbl-1_1002_2_mods-subsitution_01/aenderungsgesetz.xml",
     )
   })
-  test("Loading of mod details into form", async () => {
-    const amendingLawSection = sharedPage.getByRole("region", {
-      name: "Änderungsbefehle",
-    })
-
-    const previewSection = sharedPage.getByRole("region", {
-      name: "Vorschau",
-    })
-
-    await amendingLawSection
-      .getByText("Absatz 2 Nummer 2 bis Nummer 3 wird ersetzt durch:")
-      .click()
-
-    await expect(
-      sharedPage.getByRole("heading", {
-        level: 3,
-        name: "Änderungsbefehl bearbeiten",
-      }),
-    ).toBeVisible()
-
-    const modFormSection = sharedPage.getByRole("region", {
-      name: "Änderungsbefehl bearbeiten",
-    })
-    await expect(modFormSection).toBeVisible()
-
-    // Textual Mode Type
-    const textualModeTypeElement = modFormSection.getByRole("textbox", {
-      name: "Änderungstyp",
-    })
-    await expect(textualModeTypeElement).toBeVisible()
-    await expect(textualModeTypeElement).toHaveValue("Ersetzen")
-    await expect(textualModeTypeElement).toHaveAttribute("readonly", "")
-
-    // Time Boundaries
-    const timeBoundariesElement = modFormSection.getByRole("combobox", {
-      name: "Zeitgrenze",
-    })
-    await expect(timeBoundariesElement).toBeVisible()
-    await expect(timeBoundariesElement).toHaveText("11.01.1002")
-
-    await timeBoundariesElement.click()
-
-    const timeBoundaryOptionElements = sharedPage.getByRole("option")
-    await expect(timeBoundaryOptionElements).toHaveCount(3)
-
-    // // Destination Href Eli
-    const destinationHrefEliElement = modFormSection.getByRole("textbox", {
-      name: "ELI Zielgesetz",
-    })
-    await expect(destinationHrefEliElement).toBeVisible()
-    await expect(destinationHrefEliElement).toHaveValue(
-      "eli/bund/bgbl-1/1002/1/1002-01-01/1/deu/regelungstext-1",
-    )
-    await expect(destinationHrefEliElement).toHaveAttribute("readonly", "")
-
-    const elementToBeReplacedField = sharedPage.getByTestId(
-      "elementToBeReplaced",
-    )
-
-    const firstSelectedElementLocator = elementToBeReplacedField
-      .getByRole("button", {
-        name: "point",
-        exact: true,
-      })
-      .and(elementToBeReplacedField.getByText("2. der Anzahl der"))
-    await expect(firstSelectedElementLocator).toBeInViewport()
-    await expect(firstSelectedElementLocator).toHaveClass(/selected/)
-
-    const secondSelectedElementLocator = elementToBeReplacedField
-      .getByRole("button", {
-        name: "point",
-        exact: true,
-      })
-      .and(elementToBeReplacedField.getByText("3. den spezifischen"))
-
-    await expect(secondSelectedElementLocator).toHaveClass(/selected/)
-
-    await expect(sharedPage.getByTestId("replacingElement")).toHaveText(
-      "2. den spezifischen regionalen Anforderungen.",
-    )
-
-    await previewSection.getByRole("tab", { name: "xml" }).click()
-
-    // Do a search
-    await previewSection
-      .getByText('<?xml version="1.0" encoding="UTF-8"?>')
-      .click()
-    await sharedPage.keyboard.press("ControlOrMeta+f")
-    await previewSection
-      .getByRole("textbox", { name: "Find" })
-      .fill("den spezifischen regionalen Anforderungen.")
-    await previewSection.getByRole("button", { name: "next" }).click()
-    await expect(
-      previewSection
-        .getByText("den spezifischen regionalen Anforderungen.")
-        .first(),
-    ).toBeVisible()
-  })
 
   test("Changing target range to single range", async () => {
     const amendingLawSection = sharedPage.getByRole("region", {
@@ -519,83 +305,5 @@ test.describe("Range mod", () => {
         )
         .first(),
     ).toBeVisible()
-  })
-
-  test("Changing single range to target range", async () => {
-    const amendingLawSection = sharedPage.getByRole("region", {
-      name: "Änderungsbefehle",
-    })
-
-    const modFormSection = sharedPage.getByRole("region", {
-      name: "Änderungsbefehl bearbeiten",
-    })
-
-    const previewSection = sharedPage.getByRole("region", {
-      name: "Vorschau",
-    })
-
-    await amendingLawSection.getByText("Absatz 1 wird ersetzt durch:").click()
-
-    await expect(
-      sharedPage.getByRole("heading", {
-        level: 3,
-        name: "Änderungsbefehl bearbeiten",
-      }),
-    ).toBeVisible()
-
-    await expect(modFormSection).toBeVisible()
-
-    const elementToBeReplacedField = sharedPage.getByTestId(
-      "elementToBeReplaced",
-    )
-
-    const firstSelectedElementLocator = elementToBeReplacedField
-      .getByRole("button", {
-        name: "paragraph",
-        exact: true,
-      })
-      .and(elementToBeReplacedField.getByText("(1) Dieses Gesetz findet"))
-
-    await expect(firstSelectedElementLocator).toBeInViewport()
-    await expect(firstSelectedElementLocator).toHaveClass(/selected/)
-
-    // Select range with keyboard navigation (because of frontend having nested interactive elements)
-    await firstSelectedElementLocator.focus()
-    await sharedPage.keyboard.press("Tab")
-    await sharedPage.keyboard.press("Tab")
-    await sharedPage.keyboard.press("Tab")
-    await sharedPage.keyboard.down("Shift")
-    await sharedPage.keyboard.press("Enter")
-    await sharedPage.keyboard.up("Shift")
-
-    const secondSelectedElementLocator = elementToBeReplacedField
-      .getByRole("button", {
-        name: "paragraph",
-        exact: true,
-      })
-      .and(
-        elementToBeReplacedField.getByText("(2) Die Berechnung der Anwendung"),
-      )
-
-    await expect(secondSelectedElementLocator).toHaveClass(/selected/)
-
-    await previewSection.getByRole("tab", { name: "xml" }).click()
-
-    // Do a search
-    await previewSection
-      .getByText('<?xml version="1.0" encoding="UTF-8"?>')
-      .click()
-    await sharedPage.keyboard.press("ControlOrMeta+f")
-    await previewSection
-      .getByRole("textbox", { name: "Find" })
-      .fill("Die Berechnung der Anwendung erfolgt nach folgender Formel:")
-    await previewSection.getByRole("button", { name: "next" }).click()
-    await expect(
-      previewSection
-        .getByText(
-          "Die Berechnung der Anwendung erfolgt nach folgender Formel:",
-        )
-        .first(),
-    ).toBeHidden()
   })
 })
