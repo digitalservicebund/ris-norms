bd9f41d1f8adce4906d3df5cd25a870201480d9a
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2025-02-17 14:38:57 +0100

    chore(e2e): adds e2e tests for expression metadata
    RISDEV-6266

---
+++ frontend/e2e/expression-metadata-element.spec.ts
@@ -0,0 +1,213 @@
+import { ElementProprietary } from "@/types/proprietary"
+import { Locator, Page, expect } from "@playwright/test"
+import { test } from "@e2e/utils/test-with-auth"
+
+async function restoreInitialState(page: Page) {
+  const dataIn2023: ElementProprietary = {
+    artDerNorm: "ÄN",
+  }
+
+  await page.request.put(
+    "/api/v1/norms/eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1/proprietary/hauptteil-1_abschnitt-1_art-6",
+    { data: dataIn2023 },
+  )
+}
+
+test.describe("navigate to page", () => {
+  test("shows an error when an element page could not be loaded", async ({
+    page,
+  }) => {
+    // Given
+    await page.route(
+      /elements\/hauptteil-1_abschnitt-1_art-6/,
+      async (route) => {
+        await route.abort()
+      },
+    )
+
+    await page.goto(
+      "/eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1/metadata/element/hauptteil-1_abschnitt-1_art-6",
+    )
+
+    // Then
+    await expect(
+      page.getByText("Ein unbekannter Fehler ist aufgetreten."),
+    ).toBeVisible()
+
+    await expect(
+      page.getByRole("button", {
+        name: "Trace-ID in die Zwischenablage kopieren",
+      }),
+    ).toBeVisible()
+
+    // Cleanup
+    await page.unrouteAll()
+  })
+
+  test("navigates between elements", async ({ page }) => {
+    // Given
+    await page.goto(
+      "/eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1/metadata/element/hauptteil-1_abschnitt-1_art-2",
+    )
+
+    const heading = page.getByRole("heading", { level: 2 })
+    await expect(heading).toHaveText("§ 2 Verfassungsschutzbehörden")
+
+    // When
+    await page
+      .getByRole("link", {
+        name: "Gegenseitige Unterrichtung der Verfassungsschutzbehörden",
+      })
+      .click()
+
+    // Then
+    await expect(heading).toHaveText(
+      "§ 6 Gegenseitige Unterrichtung der Verfassungsschutzbehörden",
+    )
+  })
+})
+
+test.describe("preview", () => {
+  test("displays the title and preview", async ({ page }) => {
+    // Given
+    await page.goto(
+      "/eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1/metadata/element/hauptteil-1_abschnitt-1_art-6",
+    )
+
+    const preview = page.getByRole("region", { name: "Vorschau" })
+
+    // Then
+    await expect(
+      page.getByRole("heading", {
+        name: "§ 6 Gegenseitige Unterrichtung der Verfassungsschutzbehörden",
+
+        level: 2,
+      }),
+    ).toBeVisible()
+
+    await expect(
+      preview.getByRole("heading", {
+        name: "§ 6 Gegenseitige Unterrichtung der Verfassungsschutzbehörden",
+        level: 3,
+      }),
+    ).toBeVisible()
+  })
+
+  test("shows an error when the preview could not be loaded", async ({
+    page,
+  }) => {
+    // Given
+    await page.route(
+      /elements\/hauptteil-1_abschnitt-1_art-6/,
+      async (request) => {
+        await request.abort()
+      },
+    )
+
+    await page.goto(
+      "/eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1/metadata/element/hauptteil-1_abschnitt-1_art-6",
+    )
+
+    // Then
+    await expect(
+      page.getByText("Ein unbekannter Fehler ist aufgetreten."),
+    ).toBeVisible()
+
+    await expect(
+      page.getByRole("button", {
+        name: "Trace-ID in die Zwischenablage kopieren",
+      }),
+    ).toBeVisible()
+
+    // Cleanup
+    await page.unrouteAll()
+  })
+})
+
+test.describe("metadata view", () => {
+  let sharedPage: Page
+
+  async function saveMetadata() {
+    await sharedPage.getByRole("button", { name: "Speichern" }).click()
+    await sharedPage.waitForResponse(/proprietary/)
+  }
+
+  async function mockPutResponse(data: ElementProprietary) {
+    await sharedPage.route(
+      /\/proprietary\/hauptteil-1_abschnitt-1_art-6/,
+      async (route) => {
+        if (route.request().method() === "PUT") {
+          const response = await route.fetch()
+          const body = await response.json()
+
+          await route.fulfill({
+            response,
+            body: JSON.stringify({ ...body, ...data }),
+          })
+        } else await route.continue()
+      },
+    )
+  }
+
+  test.beforeAll(async ({ browser }) => {
+    sharedPage = await browser.newPage()
+    await sharedPage.goto(
+      "/eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1/metadata/element/hauptteil-1_abschnitt-1_art-6",
+    )
+    await restoreInitialState(sharedPage)
+  })
+
+  test.afterAll(async () => {
+    await restoreInitialState(sharedPage)
+  })
+
+  test.describe("Art der Norm", () => {
+    let artSnRadio: Locator
+    let artAnRadio: Locator
+    let artUnRadio: Locator
+
+    test.beforeAll(() => {
+      // Given
+      artSnRadio = sharedPage.getByRole("radio", {
+        name: "SN - Stammnorm",
+      })
+      artAnRadio = sharedPage.getByRole("radio", {
+        name: "ÄN - Änderungsnorm",
+      })
+      artUnRadio = sharedPage.getByRole("radio", {
+        name: "ÜN - Übergangsnorm",
+      })
+    })
+
+    test("saves changes", async () => {
+      // When
+      await artUnRadio.check()
+      await saveMetadata()
+      await sharedPage.reload()
+
+      // Then
+      await expect(artSnRadio).not.toBeChecked()
+      await expect(artAnRadio).not.toBeChecked()
+      await expect(artUnRadio).toBeChecked()
+    })
+
+    test("is updated with backend state after saving", async () => {
+      // Given
+      await mockPutResponse({ artDerNorm: "SN" })
+      await expect(artSnRadio).not.toBeChecked()
+      await expect(artAnRadio).not.toBeChecked()
+      await expect(artUnRadio).toBeChecked()
+
+      // When
+      await saveMetadata()
+
+      // Then
+      await expect(artSnRadio).toBeChecked()
+      await expect(artAnRadio).not.toBeChecked()
+      await expect(artUnRadio).not.toBeChecked()
+
+      // Cleanup
+      await sharedPage.unrouteAll()
+    })
+  })
+})
---
+++ frontend/e2e/expression-metadata-rahmen.spec.ts
@@ -0,0 +1,326 @@
+import { RahmenProprietary } from "@/types/proprietary"
+import { expect } from "@playwright/test"
+import { test } from "@e2e/utils/test-with-auth"
+import { MetadataEditorRahmenPage } from "@e2e/pages/MetadataEditorRahmenPage"
+import { uploadAmendingLaw } from "@e2e/utils/upload-with-force"
+
+test.describe("preview", () => {
+  test("displays the title and preview", async ({ page }) => {
+    // Given
+    await page.goto(
+      "/eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1/metadata",
+    )
+
+    const preview = page.getByRole("region", { name: "Vorschau" })
+
+    // Then
+    await expect(
+      page.getByRole("heading", {
+        name: "Bundesverfassungsschutzgesetz\n",
+        exact: true,
+      }),
+    ).toBeVisible()
+
+    await expect(
+      preview.getByRole("heading", {
+        name: "Gesetz über die Zusammenarbeit des Bundes und der Länder in Angelegenheiten des Verfassungsschutzes und über das Bundesamt für Verfassungsschutz",
+      }),
+    ).toBeVisible()
+  })
+
+  test("shows an error when the preview could not be loaded", async ({
+    page,
+  }) => {
+    // Given
+    await page.route(
+      /api\/v1\/norms\/eli\/bund\/bgbl-1\/1990\/s2954\/2022-12-19\/1\/deu\/regelungstext-1\?/,
+      async (route) => {
+        await route.abort()
+      },
+    )
+
+    await page.goto(
+      "/eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1/metadata",
+    )
+
+    const previewRegion = page.getByRole("region", {
+      name: "Vorschau",
+    })
+
+    // Then
+    await expect(
+      previewRegion.getByText("Ein unbekannter Fehler ist aufgetreten."),
+    ).toBeVisible()
+
+    await expect(
+      page.getByRole("button", {
+        name: "Trace-ID in die Zwischenablage kopieren",
+      }),
+    ).toBeVisible()
+
+    // Cleanup
+    await page.unrouteAll()
+  })
+})
+
+test.describe("metadata view", () => {
+  test.afterAll(async ({ request }) => {
+    await uploadAmendingLaw(request, "bgbl-1_2023_413/aenderungsgesetz.xml")
+  })
+
+  test.beforeEach(async ({ authenticatedRequest }) => {
+    const testData: RahmenProprietary = {
+      fna: "210-5",
+      art: "regelungstext",
+      typ: "gesetz",
+      subtyp: "Rechtsverordnung",
+      bezeichnungInVorlage: "Testbezeichnung nach meiner Vorlage",
+      artDerNorm: "SN,ÜN",
+      staat: "BEO - Berlin (Ost)",
+      beschliessendesOrgan: "BMinJ - Bundesministerium der Justiz",
+      qualifizierteMehrheit: true,
+      ressort: "BMVg - Bundesministerium der Verteidigung",
+      organisationsEinheit: "Einheit 1",
+    }
+
+    await authenticatedRequest.put(
+      "/api/v1/norms/eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-1/proprietary",
+      { data: testData },
+    )
+  })
+
+  test("displays metadata for the expression", async ({ page }) => {
+    const metadataPage = new MetadataEditorRahmenPage(page)
+
+    // When
+    await metadataPage.goto()
+
+    // Then
+    await expect.soft(metadataPage.fnaInput).toHaveValue("210-5")
+    await expect
+      .soft(metadataPage.documentTypeDropdown)
+      .toHaveText("Rechtsverordnung")
+    await expect.soft(metadataPage.artSnCheckbox).toBeChecked()
+    await expect.soft(metadataPage.artAnCheckbox).not.toBeChecked()
+    await expect.soft(metadataPage.artUnCheckbox).toBeChecked()
+    await expect
+      .soft(metadataPage.bezeichnungInput)
+      .toHaveValue("Testbezeichnung nach meiner Vorlage")
+    await expect
+      .soft(metadataPage.staatDropdown)
+      .toHaveText("BEO - Berlin (Ost)")
+    await expect
+      .soft(metadataPage.organDropdown)
+      .toHaveText("BMinJ - Bundesministerium der Justiz")
+    await expect.soft(metadataPage.qualMehrheitCheckbox).toBeChecked()
+    await expect
+      .soft(metadataPage.ressortDropdown)
+      .toHaveText("BMVg - Bundesministerium der Verteidigung")
+    await expect
+      .soft(metadataPage.organisationsEinheitInput)
+      .toHaveValue("Einheit 1")
+  })
+
+  test("saves changes", async ({ page }) => {
+    const metadataPage = new MetadataEditorRahmenPage(page)
+
+    await metadataPage.goto()
+
+    // When
+    await metadataPage.fnaInput.fill("123-4")
+    await metadataPage.documentTypeDropdown.click()
+    await page
+      .getByRole("option", {
+        name: "Satzung",
+      })
+      .click()
+    await metadataPage.artSnCheckbox.check()
+    await metadataPage.artAnCheckbox.uncheck()
+    await metadataPage.artUnCheckbox.check()
+    await metadataPage.bezeichnungInput.fill("Andere Bezeichnung")
+    await metadataPage.staatDropdown.click()
+    await page
+      .getByRole("option", {
+        name: "BE - Berlin",
+      })
+      .click()
+    await metadataPage.organDropdown.click()
+    await page
+      .getByRole("option", {
+        name: "BT - Bundestag",
+      })
+      .click()
+    await metadataPage.qualMehrheitCheckbox.check()
+    await metadataPage.ressortDropdown.click()
+    await page
+      .getByRole("option", {
+        name: "BMVg - Bundesministerium der Verteidigung",
+      })
+      .click()
+    await metadataPage.organisationsEinheitInput.fill("Einheit 3")
+
+    await metadataPage.saveMetadata()
+    await page.reload()
+
+    // Then
+    await expect.soft(metadataPage.fnaInput).toHaveValue("123-4")
+    await expect.soft(metadataPage.documentTypeDropdown).toHaveText("Satzung")
+    await expect.soft(metadataPage.artSnCheckbox).toBeChecked()
+    await expect.soft(metadataPage.artAnCheckbox).not.toBeChecked()
+    await expect.soft(metadataPage.artUnCheckbox).toBeChecked()
+    await expect
+      .soft(metadataPage.bezeichnungInput)
+      .toHaveValue("Andere Bezeichnung")
+    await expect.soft(metadataPage.staatDropdown).toHaveText("BE - Berlin")
+    await expect.soft(metadataPage.organDropdown).toHaveText("BT - Bundestag")
+    await expect.soft(metadataPage.qualMehrheitCheckbox).toBeChecked()
+    await expect
+      .soft(metadataPage.ressortDropdown)
+      .toHaveText("BMVg - Bundesministerium der Verteidigung")
+    await expect
+      .soft(metadataPage.organisationsEinheitInput)
+      .toHaveValue("Einheit 3")
+  })
+
+  test("is updated with backend state after saving", async ({ page }) => {
+    const metadataPage = new MetadataEditorRahmenPage(page)
+
+    await metadataPage.goto()
+
+    // Given
+    await metadataPage.mockPutResponse({
+      fna: "fake-backend-state",
+      art: "offene-struktur",
+      typ: "sonstige-bekanntmachung",
+      subtyp: "Technische Norm",
+      artDerNorm: "SN,ÄN,ÜN",
+      bezeichnungInVorlage: "fake-backend-bezeichnung",
+      staat: "SL - Saarland",
+      beschliessendesOrgan: "AA - Auswärtiges Amt",
+      qualifizierteMehrheit: false,
+      ressort: "AA - Auswärtiges Amt",
+      organisationsEinheit: "Fake Einheit",
+    })
+
+    // When
+    await metadataPage.saveMetadata()
+
+    // Then
+    await expect.soft(metadataPage.fnaInput).toHaveValue("fake-backend-state")
+    await expect
+      .soft(metadataPage.documentTypeDropdown)
+      .toHaveText("Technische Norm")
+    await expect.soft(metadataPage.artSnCheckbox).toBeChecked()
+    await expect.soft(metadataPage.artAnCheckbox).toBeChecked()
+    await expect.soft(metadataPage.artUnCheckbox).toBeChecked()
+    await expect
+      .soft(metadataPage.bezeichnungInput)
+      .toHaveValue("fake-backend-bezeichnung")
+    await expect.soft(metadataPage.staatDropdown).toHaveText("SL - Saarland")
+    await expect
+      .soft(metadataPage.organDropdown)
+      .toHaveText("AA - Auswärtiges Amt")
+    await expect.soft(metadataPage.qualMehrheitCheckbox).not.toBeChecked()
+    await expect
+      .soft(metadataPage.ressortDropdown)
+      .toHaveText("AA - Auswärtiges Amt")
+    await expect
+      .soft(metadataPage.organisationsEinheitInput)
+      .toHaveValue("Fake Einheit")
+  })
+
+  test.describe("Dokumenttyp", () => {
+    test("displays the value as unknown", async ({ page }) => {
+      const metadataPage = new MetadataEditorRahmenPage(page)
+
+      // Given
+      await page.route(
+        /\/norms\/eli\/bund\/bgbl-1\/1990\/s2954\/2022-12-19\/1\/deu\/regelungstext-1\/proprietary$/,
+        async (route) => {
+          if (route.request().method() === "GET") {
+            const response = await route.fetch()
+            const body = await response.json()
+
+            await route.fulfill({
+              response,
+              body: JSON.stringify({
+                ...body,
+                art: "dummy",
+                typ: "dummy",
+                subtyp: "",
+              }),
+            })
+          } else await route.fulfill()
+        },
+      )
+      await metadataPage.goto()
+
+      // Then
+      await expect(metadataPage.documentTypeDropdown).toHaveText("Unbekannt")
+    })
+
+    test("displays the value as empty", async ({ page }) => {
+      const metadataPage = new MetadataEditorRahmenPage(page)
+
+      // Given
+      await page.route(
+        /\/norms\/eli\/bund\/bgbl-1\/1990\/s2954\/2022-12-19\/1\/deu\/regelungstext-1\/proprietary$/,
+        async (route) => {
+          if (route.request().method() === "GET") {
+            const response = await route.fetch()
+            const body = await response.json()
+
+            await route.fulfill({
+              response,
+              body: JSON.stringify({ ...body, art: "", typ: "", subtyp: "" }),
+            })
+          } else await route.fulfill()
+        },
+      )
+      await metadataPage.goto()
+
+      // Then
+      await expect(metadataPage.documentTypeDropdown).toHaveText("")
+    })
+  })
+
+  test("shows an error if the metadata could not be loaded", async ({
+    page,
+  }) => {
+    const metadataPage = new MetadataEditorRahmenPage(page)
+
+    // Given
+    await page.route(
+      /\/norms\/eli\/bund\/bgbl-1\/1990\/s2954\/2022-12-19\/1\/deu\/regelungstext-1\/proprietary$/,
+      (request) => {
+        request.abort()
+      },
+    )
+
+    await metadataPage.goto()
+
+    const editorRegion = page.getByRole("region", {
+      name: "Metadaten dokumentieren",
+    })
+
+    // Then
+    await expect(
+      editorRegion.getByText("Ein unbekannter Fehler ist aufgetreten."),
+    ).toBeVisible()
+  })
+
+  test("displays a success message when the data has been saved", async ({
+    page,
+  }) => {
+    const metadataPage = new MetadataEditorRahmenPage(page)
+    // Given
+    await metadataPage.goto()
+
+    // When
+    await metadataPage.saveMetadata()
+
+    // Then
+    const toastMessage = page.getByRole("alert")
+    await expect(toastMessage).toHaveText("Gespeichert!")
+  })
+})
