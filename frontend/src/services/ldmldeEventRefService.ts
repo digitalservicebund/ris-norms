a527e1a0a971c9a3cd2c77591c791b2c613e5631
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-02-19 16:00:52 +0100

    Remove unneeded files
    RISDEV-0000

--- frontend/src/services/ldmldeEventRefService.ts
+++
@@ -1,8 +0,0 @@
-import { evaluateXPathOnce } from "@/services/xmlService"
-
-/**
- * Get the date of an akn:eventRef.
- */
-export function getEventRefDate(eventRefNode: Node) {
-  return evaluateXPathOnce(`@date`, eventRefNode)?.nodeValue
-}
