b1ef4c38a9ccf4f69c01463d5e322c1881f1de3f
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2025-02-28 15:15:24 +0100

    feat: implement updating migration logs in the database
    RISDEV-6898

---
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/application/port/output/UpdateMigrationLogCompletedPort.java
@@ -0,0 +1,25 @@
+package de.bund.digitalservice.ris.norms.application.port.output;
+
+import de.bund.digitalservice.ris.norms.domain.entity.MigrationLog;
+import java.util.UUID;
+
+/**
+ * Port for updating the completion state of a migration log entry.
+ */
+public interface UpdateMigrationLogCompletedPort {
+  /**
+   * Updates the completion state of an existing {@link MigrationLog} entry.
+   *
+   * @param command Specifies the entry to update and the new value
+   * @return The updated {@link MigrationLog} entry
+   */
+  MigrationLog updateMigrationLogCompleted(final Command command);
+
+  /**
+   * Command for updating the completion state of a migration log entry.
+   *
+   * @param id The ID of the migration log entry to be updated
+   * @param completed The new completion state of the migration log entry
+   */
+  record Command(UUID id, boolean completed) {}
+}
