3d85aa10f03d49c4dde072e93a2482a52a6aae89
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-03-07 12:49:26 +0100

    Add general publishing steps
    RISDEV-6988

--- backend/src/main/java/de/bund/digitalservice/ris/norms/application/service/PortalPrototypePublishService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/application/service/PortalPrototypePublishService.java
@@ -2,11 +2,14 @@ package de.bund.digitalservice.ris.norms.application.service;
 
 import de.bund.digitalservice.ris.norms.application.port.input.PublishNormsToPortalPrototypeUseCase;
 import de.bund.digitalservice.ris.norms.application.port.output.DeleteAllPublishedDokumentePort;
-import de.bund.digitalservice.ris.norms.application.port.output.DeletePublishedNormPort;
 import de.bund.digitalservice.ris.norms.application.port.output.LoadNormManifestationElisByPublishStatePort;
+import de.bund.digitalservice.ris.norms.application.port.output.LoadNormPort;
 import de.bund.digitalservice.ris.norms.application.port.output.PublishChangelogPort;
 import de.bund.digitalservice.ris.norms.application.port.output.PublishNormPort;
+import de.bund.digitalservice.ris.norms.domain.entity.Norm;
 import de.bund.digitalservice.ris.norms.domain.entity.NormPublishState;
+import java.time.Instant;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
@@ -22,26 +25,28 @@ public class PortalPrototypePublishService implements PublishNormsToPortalProtot
 
   private final LoadNormManifestationElisByPublishStatePort loadNormManifestationElisByPublishStatePort;
   private final PublishNormPort publishNormPort;
-  private final DeletePublishedNormPort deletePublishedNormPort;
   private final DeleteAllPublishedDokumentePort deleteAllPublishedDokumentePort;
   private final PublishChangelogPort publishChangelogPort;
+  private final LoadNormPort loadNormPort;
 
   public PortalPrototypePublishService(
     LoadNormManifestationElisByPublishStatePort loadNormManifestationElisByPublishStatePort,
     @Qualifier("portalPrototype") PublishNormPort publishNormPort,
-    @Qualifier("portalPrototype") DeletePublishedNormPort deletePublishedNormPort,
     @Qualifier("portalPrototype") DeleteAllPublishedDokumentePort deleteAllPublishedDokumentePort,
-    @Qualifier("portalPrototype") PublishChangelogPort publishChangelogsPort
+    @Qualifier("portalPrototype") PublishChangelogPort publishChangelogsPort,
+    LoadNormPort loadNormPort
   ) {
     this.loadNormManifestationElisByPublishStatePort = loadNormManifestationElisByPublishStatePort;
     this.publishNormPort = publishNormPort;
-    this.deletePublishedNormPort = deletePublishedNormPort;
     this.deleteAllPublishedDokumentePort = deleteAllPublishedDokumentePort;
     this.publishChangelogPort = publishChangelogsPort;
+    this.loadNormPort = loadNormPort;
   }
 
   @Override
   public void publishNormsToPortalPrototype() {
+    final Instant startOfProcessing = Instant.now();
+
     var publishedNormElis =
       loadNormManifestationElisByPublishStatePort.loadNormManifestationElisByPublishState(
         new LoadNormManifestationElisByPublishStatePort.Command(NormPublishState.PUBLISHED)
@@ -50,9 +55,42 @@ public class PortalPrototypePublishService implements PublishNormsToPortalProtot
     log.info("Currently {} norms are in state PUBLISHED", publishedNormElis.size());
 
     publishedNormElis.forEach(eli -> {
-      // TODO: (Malte Laukötter, 2025-03-06) implement
+      log.info("Processing norm with manifestation eli {}", eli);
+      Optional<Norm> norm = loadNormPort.loadNorm(new LoadNormPort.Command(eli));
+
+      if (norm.isEmpty()) {
+        log.error("Norm with manifestation eli {} not found", eli);
+        return;
+      }
+
+      // TODO: (Malte Laukötter, 2025-03-07) check if norm is allowed to be published
+
+      // TODO: (Malte Laukötter, 2025-03-07) clean up norm
+      // TODO: (Malte Laukötter, 2025-03-07) also run the cleanup from the normal PublishService
+
+      try {
+        publishNormPort.publishNorm(new PublishNormPort.Command(norm.get()));
+        log.info(
+          "Published norm to portal-prototype: {}",
+          norm.get().getManifestationEli().toString()
+        );
+      } catch (final Exception e) {
+        log.error(
+          "Norm {} could not be published to portal-prototype",
+          norm.get().getManifestationEli().toString()
+        );
+        log.error(e.getMessage(), e);
+      }
     });
 
-    log.info("Publishing norms to port prototype - not yet implemented");
+    log.info("Deleting all old dokumente from portal-prototype bucket");
+    deleteAllPublishedDokumentePort.deleteAllPublishedDokumente(
+      new DeleteAllPublishedDokumentePort.Command(startOfProcessing)
+    );
+    log.info("Deleted all old dokumente from portal-prototype  bucket");
+
+    publishChangelogPort.publishChangelogs(new PublishChangelogPort.Command(false));
+
+    log.info("Publish job for portal prototype successfully completed.");
   }
 }
