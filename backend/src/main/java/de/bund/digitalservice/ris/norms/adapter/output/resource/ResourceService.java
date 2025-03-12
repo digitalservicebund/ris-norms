b08cf47f61554ddc9a8dda73c2581c059b8807d5
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-03-12 09:01:43 +0100

    Simplify exception handling and use immutable list
    RISDEV-6990

--- backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/output/resource/ResourceService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/output/resource/ResourceService.java
@@ -3,7 +3,6 @@ package de.bund.digitalservice.ris.norms.adapter.output.resource;
 import de.bund.digitalservice.ris.norms.application.port.output.LoadPortalPublishingAllowListPort;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,24 +16,15 @@ import org.springframework.stereotype.Service;
 @Log4j2
 public class ResourceService implements LoadPortalPublishingAllowListPort {
 
-  private final List<String> publishingAllowlist = new ArrayList<>();
+  private final List<String> publishingAllowlist;
 
   public ResourceService(
     @Value(
       "classpath:/portal-prototype-publishing-allowlist.txt"
     ) Resource portalPrototypePublishingAllowlist
-  ) {
-    try {
-      this.publishingAllowlist.addAll(
-          Files.readAllLines(portalPrototypePublishingAllowlist.getFile().toPath())
-        );
-    } catch (IOException e) {
-      log.error(
-        "Could not read portal prototype publishing allowlist. Keep the list of allowed norms empty.",
-        e
-      );
-      this.publishingAllowlist.clear();
-    }
+  ) throws IOException {
+    this.publishingAllowlist =
+    List.copyOf(Files.readAllLines(portalPrototypePublishingAllowlist.getFile().toPath()));
   }
 
   @Override
