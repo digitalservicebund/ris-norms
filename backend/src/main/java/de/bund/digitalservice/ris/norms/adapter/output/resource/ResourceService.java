053de935c1ff0a1454160ca1a1178c6c55a5d613
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-03-12 11:34:15 +0100

    Fix loading portal-prototype-publishing-allowlist.txt
    As it is only available within the jar file it can not be converted to a
    File. We therefore need to use the input stream provided by the Resource
    
    RISDEV-0000

--- backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/output/resource/ResourceService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/output/resource/ResourceService.java
@@ -1,8 +1,10 @@
 package de.bund.digitalservice.ris.norms.adapter.output.resource;
 
 import de.bund.digitalservice.ris.norms.application.port.output.LoadPortalPublishingAllowListPort;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,7 +26,14 @@ public class ResourceService implements LoadPortalPublishingAllowListPort {
     ) Resource portalPrototypePublishingAllowlist
   ) throws IOException {
     this.publishingAllowlist =
-    List.copyOf(Files.readAllLines(portalPrototypePublishingAllowlist.getFile().toPath()));
+    new BufferedReader(
+      new InputStreamReader(
+        portalPrototypePublishingAllowlist.getInputStream(),
+        StandardCharsets.UTF_8
+      )
+    )
+      .lines()
+      .toList();
   }
 
   @Override
