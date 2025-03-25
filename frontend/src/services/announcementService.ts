1b3ea21aba660a1469975f7f8f66a41559b9ee8c
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2025-03-25 13:06:05 +0100

    feat: extends announcement service to get affected documents
    RISDEV-6942

--- frontend/src/services/announcementService.ts
+++ frontend/src/services/announcementService.ts
@@ -10,7 +10,7 @@ import type { NormExpressionEli } from "@/lib/eli/NormExpressionEli"
  * Load all announcements from the API.
  */
 export function useAnnouncementsService(): UseFetchReturn<Norm[]> {
-  return useApiFetch("/announcements").json()
+  return useApiFetch("/verkuendungen").json()
 }
 
 /**
@@ -21,7 +21,19 @@ export function useAnnouncementsService(): UseFetchReturn<Norm[]> {
 export function useGetAnnouncementService(
   eli: MaybeRefOrGetter<NormExpressionEli>,
 ): UseFetchReturn<Announcement> {
-  const url = computed(() => `/announcements/${toValue(eli)}`)
+  const url = computed(() => `/verkuendungen/${toValue(eli)}`)
 
+  return useApiFetch(url).json()
+}
+
+/**
+ * Fetch the zielnormen of a specific announcement.
+ *
+ * @param eli ELI of the announcement
+ */
+export function useGetZielnormen(
+  eli: MaybeRefOrGetter<NormExpressionEli>,
+): UseFetchReturn<Norm[]> {
+  const url = computed(() => `/verkuendungen/${toValue(eli)}/zielnormen`)
   return useApiFetch(url).json()
 }
