ee1b59522fccd8bcd0e410a8838b42e51e467fd6
Sebastian Rossa <sebastian.rossa@digitalservice.bund.de>
2025-03-31 16:00:26 +0200

    Skip initializing verkuendung api and bucket for prod and staging
    RISDEV-6981

--- backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/VerkuendungControllerTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/VerkuendungControllerTest.java
@@ -16,10 +16,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SecurelessControllerTest(VerkuendungController.class)
+@ActiveProfiles(value = "uat")
 class VerkuendungControllerTest {
 
   @Autowired
