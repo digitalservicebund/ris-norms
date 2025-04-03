44d9dfa8b5ad991a2ae28685272680936e56597c
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-04-03 16:21:10 +0200

    Extend validator for rechtsetzungsdokument and bekanntmachung
    RISDEV-0000

---
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/domain/entity/Rechtsetzungsdokument.java
@@ -0,0 +1,35 @@
+package de.bund.digitalservice.ris.norms.domain.entity;
+
+import de.bund.digitalservice.ris.norms.utils.NodeParser;
+import lombok.Getter;
+import org.w3c.dom.Document;
+
+/**
+ * Represents the "Rechtsetzungsdokument" of a norm in LDML.de.
+ */
+@Getter
+public class Rechtsetzungsdokument extends Dokument {
+
+  @Override
+  public Dokument copy() {
+    return new Rechtsetzungsdokument(this);
+  }
+
+  public Rechtsetzungsdokument(Rechtsetzungsdokument rechtsetzungsdokument) {
+    this((Document) rechtsetzungsdokument.getDocument().cloneNode(true));
+  }
+
+  public Rechtsetzungsdokument(Document document) {
+    super(document);
+  }
+
+  /**
+   * Is the Rechtsetzungsdokument a Verkündungsfassung, opposed to an Entwurfsfassung.
+   * @return true if it is a Verkündungsfassung
+   */
+  public boolean isVerkuendungsfassung() {
+    return NodeParser
+      .getValueFromMandatoryNodeFromExpression("//documentCollection/@name", getDocument())
+      .equals("rechtsetzungsdokument-verkuendungsfassung");
+  }
+}
