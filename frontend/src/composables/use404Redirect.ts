1e00f9ef7403fa2838ef6d9c2ad5e328402e1711
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2025-04-07 18:40:59 +0200

    docs: add missing JSDoc
    RISDEV-7421

--- frontend/src/composables/use404Redirect.ts
+++ frontend/src/composables/use404Redirect.ts
@@ -2,6 +2,12 @@ import type { MaybeRefOrGetter } from "vue"
 import { toValue, watchEffect } from "vue"
 import { useRouter } from "vue-router"
 
+/**
+ * Takes a list of errors and redirects to the 404 page as soon as any of them
+ * has a status of 404. Undefined values will be ignored.
+ *
+ * @param errors List of errors
+ */
 export function use404Redirect(
   errors: MaybeRefOrGetter<Array<{ status?: number } | undefined>>,
 ) {
