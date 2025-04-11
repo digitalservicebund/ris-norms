816470d0fd8942c382270040d490c105ee6d5751
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2025-04-11 11:04:10 +0200

    e2e: Updates e2e tests
    - Update E2E tests with real test data
    - add E2E test for page reload
    - add test for error when saving fails
    
    RISDEV-7427

--- frontend/e2e/application/verkuendungenZeitgrenzen.spec.ts
+++ frontend/e2e/application/verkuendungenZeitgrenzen.spec.ts
@@ -5,24 +5,6 @@ test.describe(
   "showing the Zeitgrenzen page for a Verkündung",
   { tag: ["@RISDEV-4007"] },
   () => {
-    test.beforeEach(async ({ page }) => {
-      // Temporarily mock the respone while we're waiting for the backend implementation
-      // TODO: Remove once backend is ready
-      await page.route(
-        "/api/v1/norms/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/zeitgrenzen",
-        async (route) => {
-          await route.fulfill({
-            status: 200,
-            json: [
-              { id: "1", date: "2025-04-08", art: "INKRAFT" },
-              { id: "2", date: "2025-04-10", art: "INKRAFT" },
-              { id: "3", date: "2025-04-20", art: "AUSSERKRAFT" },
-            ],
-          })
-        },
-      )
-    })
-
     test("opens the page for a Verkündung", async ({ page }) => {
       await page.goto(
         "./verkuendungen/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1",
@@ -84,45 +66,17 @@ test.describe(
 
       const loadedItems = await editor.getByRole("listitem").all()
 
-      expect(loadedItems).toHaveLength(3)
-
-      // Item 1
+      expect(loadedItems).toHaveLength(1)
       await expect(
         loadedItems[0].getByRole("checkbox", { name: "unbestimmt" }),
       ).not.toBeChecked()
 
       await expect(
         loadedItems[0].getByRole("textbox", { name: "Geltungszeit" }),
-      ).toHaveValue("08.04.2025")
+      ).toHaveValue("16.03.2017")
 
       await expect(
         loadedItems[0].getByRole("combobox", { name: "Inkrafttreten" }),
-      ).toBeVisible()
-
-      // Item 2
-      await expect(
-        loadedItems[1].getByRole("checkbox", { name: "unbestimmt" }),
-      ).not.toBeChecked()
-
-      await expect(
-        loadedItems[1].getByRole("textbox", { name: "Geltungszeit" }),
-      ).toHaveValue("10.04.2025")
-
-      await expect(
-        loadedItems[1].getByRole("combobox", { name: "Inkrafttreten" }),
-      ).toBeVisible()
-
-      // Item 3
-      await expect(
-        loadedItems[2].getByRole("checkbox", { name: "unbestimmt" }),
-      ).not.toBeChecked()
-
-      await expect(
-        loadedItems[2].getByRole("textbox", { name: "Geltungszeit" }),
-      ).toHaveValue("20.04.2025")
-
-      await expect(
-        loadedItems[2].getByRole("combobox", { name: "Außerkrafttreten" }),
       ).toBeVisible()
     })
 
@@ -234,22 +188,24 @@ test.describe(
       // Delete first Zeitgrenze
       await editor
         .getByRole("button", {
-          name: "Zeitgrenze vom 08.04.2025 entfernen",
+          name: "Zeitgrenze vom 16.03.2017 entfernen",
         })
         .click()
 
-      // Change 2nd Zeitgrenze
-      const zeitgrenzen = editor.getByRole("listitem")
-      await expect(zeitgrenzen.first()).toBeVisible()
-      await zeitgrenzen
-        .first()
-        .getByRole("textbox", { name: "Geltungszeit" })
-        .fill("10.04.2025")
+      // Add a new Zeitgrenze
+      await editor
+        .getByRole("button", { name: "Geltungszeit hinzufügen" })
+        .click()
 
-      await zeitgrenzen.first().getByRole("combobox").click()
+      const zeitgrenzen = editor.getByRole("listitem")
+      await zeitgrenzen.last().getByRole("combobox").click()
+      await zeitgrenzen
+        .last()
+        .getByRole("textbox", { name: "Geltungszeit" })
+        .fill("20.05.2025")
       await page.getByRole("option", { name: "Außerkrafttreten" }).click()
 
-      // Add a new Zeitgrenze
+      // Add another new Zeitgrenze
       await editor
         .getByRole("button", { name: "Geltungszeit hinzufügen" })
         .click()
@@ -258,25 +214,121 @@ test.describe(
       await zeitgrenzen
         .last()
         .getByRole("textbox", { name: "Geltungszeit" })
-        .fill("30.05.2025")
-      await page.getByRole("option", { name: "Außerkrafttreten" }).click()
+        .fill("10.05.2025")
+      await page.getByRole("option", { name: "Inkrafttreten" }).click()
+
+      const resposne = page.waitForResponse(
+        "/api/v1/norms/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/zeitgrenzen",
+      )
 
       // Save -> Success toast
       await page.getByRole("button", { name: "Speichern" }).click()
+      await resposne
+      await expect(page.getByText("Gespeichert!")).toBeVisible()
+      await page.reload()
+      const editorAfterReload = page.getByRole("region", {
+        name: "Geltungszeitregeln anlegen",
+      })
+      await expect(editorAfterReload).toBeVisible()
+      const reloadedItems = await editorAfterReload.getByRole("listitem").all()
+      expect(reloadedItems).toHaveLength(2)
+
+      await expect(
+        reloadedItems[0].getByRole("textbox", { name: "Geltungszeit" }),
+      ).toHaveValue("10.05.2025")
+      await expect(reloadedItems[0].getByRole("combobox")).toContainText(
+        "Inkrafttreten",
+      )
+
+      await expect(
+        reloadedItems[1].getByRole("textbox", { name: "Geltungszeit" }),
+      ).toHaveValue("20.05.2025")
+      await expect(reloadedItems[1].getByRole("combobox")).toContainText(
+        "Außerkrafttreten",
+      )
+    })
+
+    test("shows an error message when saving fails", async ({ page }) => {
+      await page.goto(
+        "./verkuendungen/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/zeitgrenzen",
+      )
+
+      const editor = page.getByRole("region", {
+        name: "Geltungszeitregeln anlegen",
+      })
+
+      // Add a new Zeitgrenze with empty date (invalid state)
+      await editor
+        .getByRole("button", { name: "Geltungszeit hinzufügen" })
+        .click()
+
+      await page.getByRole("button", { name: "Speichern" }).click()
+      await expect(
+        page.getByText("Fehler beim Speichern: Eingabefehler"),
+      ).toBeVisible()
+    })
+
+    test.skip("updates the data on the page with the new data from the backend", async ({
+      page,
+    }) => {
+      await page.goto(
+        "./verkuendungen/eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1/zeitgrenzen",
+      )
+
+      const editor = page.getByRole("region", {
+        name: "Geltungszeitregeln anlegen",
+      })
+
+      // Add a new Zeitgrenze
+      await editor
+        .getByRole("button", { name: "Geltungszeit hinzufügen" })
+        .click()
+
+      const zeitgrenzen = editor.getByRole("listitem")
+      await zeitgrenzen.last().getByRole("combobox").click()
+      await zeitgrenzen
+        .last()
+        .getByRole("textbox", { name: "Geltungszeit" })
+        .fill("20.06.2025")
+      await page.getByRole("option", { name: "Außerkrafttreten" }).click()
+
+      // Add another new Zeitgrenze
+      await editor
+        .getByRole("button", { name: "Geltungszeit hinzufügen" })
+        .click()
+      await zeitgrenzen.last().getByRole("combobox").click()
+      await zeitgrenzen
+        .last()
+        .getByRole("textbox", { name: "Geltungszeit" })
+        .fill("30.05.2025")
+      await page.getByRole("option", { name: "Inkrafttreten" }).click()
+
+      await page.getByRole("button", { name: "Speichern" }).click()
       await expect(page.getByText("Gespeichert!")).toBeVisible()
 
-      // Reload -> Data still there
-      // TODO: Add test
-    })
+      const items = await editor.getByRole("listitem").all()
+      expect(items).toHaveLength(3)
 
-    test.skip("shows an error message when saving fails", async ({}) => {
-      // TODO: Go to page, create a Zeitgrenze with an empty date, save -> error toast
-      expect(true).toBe(true)
-    })
+      await expect(
+        items[0].getByRole("textbox", { name: "Geltungszeit" }),
+      ).toHaveValue("16.03.2017")
+      await expect(items[0].getByRole("combobox")).toContainText(
+        "Inkrafttreten",
+      )
 
-    test.skip("updates the data on the page with the new data from the backend", ({}) => {
-      // TODO: Test when implemented
-      expect(true).toBe(true)
+      await expect(
+        items[1].getByRole("textbox", { name: "Geltungszeit" }),
+      ).toHaveValue("30.05.2025")
+      await expect(items[1].getByRole("combobox")).toContainText(
+        "Außerkrafttreten",
+      )
+
+      await expect(
+        items[2].getByRole("textbox", { name: "Geltungszeit" }),
+      ).toHaveValue("20.06.2025")
+      await expect(items[2].getByRole("combobox")).toContainText(
+        "Außerkrafttreten",
+      )
     })
   },
 )
