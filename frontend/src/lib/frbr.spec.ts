771112819608e1c00751d095f666727a422cc905
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2025-04-29 09:49:42 +0200

    refactor: support falsy inputs when formatting FRBR info
    RISDEV-7627

--- frontend/src/lib/frbr.spec.ts
+++ frontend/src/lib/frbr.spec.ts
@@ -4,7 +4,7 @@ import { getFrbrDisplayText } from "./frbr"
 
 describe("getFrbrDisplayText", () => {
   it("formats the name of a printed announced law", () => {
-    const amendingLaw: Norm = {
+    const norm: Norm = {
       eli: "eli/bund/bgbl-1/2017/s419/2017-03-15/1/deu/regelungstext-1",
       frbrName: "BGBl. I",
       frbrDateVerkuendung: "2017-03-15",
@@ -12,11 +12,11 @@ describe("getFrbrDisplayText", () => {
       title: "Entwurf eines Zweiten Gesetzes zur Änderung des Vereinsgesetzes",
     }
 
-    expect(getFrbrDisplayText(amendingLaw)).toBe("BGBl. I 2017 S. 419")
+    expect(getFrbrDisplayText(norm)).toBe("BGBl. I 2017 S. 419")
   })
 
   it("formats the name of a digitally announced law", () => {
-    const amendingLaw: Norm = {
+    const norm: Norm = {
       eli: "eli/bund/bgbl-1/2023/413/2023-12-29/1/deu/regelungstext-1",
       frbrName: "BGBl. I",
       frbrDateVerkuendung: "2023-12-29",
@@ -24,6 +24,31 @@ describe("getFrbrDisplayText", () => {
       title: "Gesetz zum ersten Teil der Reform des Nachrichtendienstrechts",
     }
 
-    expect(getFrbrDisplayText(amendingLaw)).toBe("BGBl. I 2023 Nr. 413")
+    expect(getFrbrDisplayText(norm)).toBe("BGBl. I 2023 Nr. 413")
+  })
+
+  it("returns undefined if the input is empty", () => {
+    expect(getFrbrDisplayText(undefined)).toBeUndefined()
+  })
+
+  it("returns undefined if some necessary properties are missing", () => {
+    const normWithoutName: Norm = {
+      eli: "eli/bund/bgbl-1/2023/413/2023-12-29/1/deu/regelungstext-1",
+      frbrName: undefined,
+      frbrDateVerkuendung: "2023-12-29",
+      frbrNumber: "413",
+      title: "Gesetz zum ersten Teil der Reform des Nachrichtendienstrechts",
+    }
+
+    const normWithoutNumber: Norm = {
+      eli: "eli/bund/bgbl-1/2023/413/2023-12-29/1/deu/regelungstext-1",
+      frbrName: "BGBl. I",
+      frbrDateVerkuendung: "2023-12-29",
+      frbrNumber: undefined,
+      title: "Gesetz zum ersten Teil der Reform des Nachrichtendienstrechts",
+    }
+
+    expect(getFrbrDisplayText(normWithoutName)).toBeUndefined()
+    expect(getFrbrDisplayText(normWithoutNumber)).toBeUndefined()
   })
 })
