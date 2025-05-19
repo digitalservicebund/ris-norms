d45269225e08f4ca86e1f48c79aed763e5e47b3b
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-05-19 17:21:34 +0200

    Add validation of files to conversion script
    RISDEV-7806

--- LegalDocML.de/1.8/run_conversion_from_1.7.2_to_1.8.sh
+++ LegalDocML.de/1.8/run_conversion_from_1.7.2_to_1.8.sh
@@ -35,8 +35,46 @@ for filepath in $FOLDER_OLD/**/*.xml; do
   rm "$filepath.tmp"
   mv "$filepath.tmp2" "$new_filepath"
 
-  if [[ "$create_rechtsetzungsdokument" = true  ]]; then
-      echo "TODO: create rechtsetzungsdokument"
+  new_filename=${new_filepath##*/}
+
+  schema_file="unknown_file_type"
+
+  if [[ ${new_filename%-*} = "rechtsetzungsdokument" ]]; then
+    schema_file="legalDocML.de-risnorms-rechtsetzungsdokument.xsd"
   fi
+  if [[ ${new_filename%-*} = "regelungstext" ]]; then
+    schema_file="legalDocML.de-risnorms-regelungstextverkuendungsfassung.xsd"
+  fi
+  if [[ ${new_filename%-*} = "offenestruktur" ]]; then
+    schema_file="legalDocML.de-risnorms-offenestruktur.xsd"
+  fi
+  if [[ ${new_filename%-*} = "bekanntmachungstext" ]]; then
+    schema_file="legalDocML.de-risnorms-bekanntmachung.xsd"
+  fi
+
+  if [[ "$create_rechtsetzungsdokument" = true  ]]; then
+    filepath_rechtsetzungsdokument="${new_filepath/$new_filename/rechtsetzungsdokument-1.xml}"
+
+    echo "Create Rechtsetzungsdokument $filepath_rechtsetzungsdokument"
+
+    cat "$new_filepath" | java -jar $SAXON_HE -s:- -xsl:./xslt/create-rechtsetzungsdokument-from-regelungstext.xslt | java -jar $SAXON_HE -s:- -xsl:./xslt/fix-eids.xslt > "$filepath_rechtsetzungsdokument.tmp"
+
+    # Replace the string REPLACE_WITH_NEW_UUID with unique uuids, this is hard to do in xslt itself so we do it as a post-processing step
+    while IFS= read -r line;
+    do
+      printf '%s\n' "${line/REPLACE_WITH_NEW_UUID/$(uuidgen | awk '{print tolower($0)}')}"
+    done < "$filepath_rechtsetzungsdokument.tmp" > "$filepath_rechtsetzungsdokument"
+
+    rm "$filepath_rechtsetzungsdokument.tmp"
+
+    xmllint --noout --schema legalDocML.de-risnorms-rechtsetzungsdokument.xsd $filepath_rechtsetzungsdokument
+  fi
+
+  if [[ ${new_filename%-*} = "regelungstext" ]]; then
+    cat "$new_filepath" | java -jar $SAXON_HE -s:- -xsl:./xslt/remove-rechtsetzungsdokument-metadata.xslt > "$new_filepath.tmp"
+    mv "$new_filepath.tmp" "$new_filepath"
+  fi;
+
+  xmllint --noout --schema $schema_file $new_filepath
 done
 
