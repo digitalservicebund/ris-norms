d84fcb56f22a6c240863e10a7e853c0a3be25319
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-05-19 13:36:28 +0200

    Create folders for every norm
    We need to have a rechtsetzungsdokument for every norm as well so having
    them in individual folders is necessary
    
    RISDEV-7806

--- LegalDocML.de/1.8/run_conversion_from_1.7.2_to_1.8.sh
+++ LegalDocML.de/1.8/run_conversion_from_1.7.2_to_1.8.sh
@@ -1,16 +1,42 @@
 SAXON_HE="$HOME/Downloads/SaxonHE12-7J/saxon-he-12.7.jar"
 
-for filename in ../1.7.2/fixtures/**/*.xml; do
-  echo "$filename -> ${filename/1.7.2/1.8}"
-  cat "$filename" | java -jar $SAXON_HE -s:- -xsl:./xslt/1.7.2-to-1.8.xslt | java -jar $SAXON_HE -s:- -xsl:./xslt/fix-eids.xslt > "${filename}.tmp"
+FOLDER_OLD="../1.7.2/fixtures"
+FOLDER_NEW="../1.8/fixtures"
+
+# FOLDER_OLD="../1.7.2/schema-extension-fixtures"
+# FOLDER_NEW="../1.8/schema-extension-fixtures"
+
+for filepath in $FOLDER_OLD/**/*.xml; do
+  new_filepath=${filepath/$FOLDER_OLD/$FOLDER_NEW}
+  filename="${filepath##*/}"
+  create_rechtsetzungsdokument=false
+
+  if [[ ${filename%-*} != "rechtsetzungsdokument" ]]; then
+    if [[ ${filename%-*} == "regelungstext" || ${filename%-*} == "offenestruktur"  || ${filename%-*} == "bekanntmachungstext" ]]; then
+      if [[ ! -e "${filepath/$filename/rechtsetzungsdokument-1.xml}" ]]; then
+        create_rechtsetzungsdokument=true
+      fi
+    else
+      mkdir -p "${new_filepath/.xml/}"
+      new_filepath="${new_filepath/.xml//regelungstext-1.xml}"
+      create_rechtsetzungsdokument=true
+    fi
+  fi
+
+  echo "$filepath -> $new_filepath"
+  cat "$filepath" | java -jar $SAXON_HE -s:- -xsl:./xslt/1.7.2-to-1.8.xslt | java -jar $SAXON_HE -s:- -xsl:./xslt/fix-eids.xslt > "$filepath.tmp"
 
   # Replace the string REPLACE_WITH_NEW_UUID with unique uuids, this is hard to do in xslt itself so we do it as a post-processing step
   while IFS= read -r line;
   do
     printf '%s\n' "${line/REPLACE_WITH_NEW_UUID/$(uuidgen | awk '{print tolower($0)}')}"
-  done < "${filename}.tmp" > "${filename}.tmp2"
+  done < "$filepath.tmp" > "$filepath.tmp2"
 
-  rm "${filename}.tmp"
+  rm "$filepath.tmp"
+  mv "$filepath.tmp2" "$new_filepath"
 
-  mv "${filename}.tmp2" "${filename/1.7.2/1.8}"
+  if [[ "$create_rechtsetzungsdokument" = true  ]]; then
+      echo "TODO: create rechtsetzungsdokument"
+  fi
 done
+
