a8ad03f2c2c2f518a06f110d4bffb687224127c4
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-05-27 17:11:19 +0200

    Change Norm::getRechtsetzungsdokument to throw if it is missing
    RISDEV-7807

--- backend/src/main/java/de/bund/digitalservice/ris/norms/domain/entity/Norm.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/domain/entity/Norm.java
@@ -100,12 +100,14 @@ public class Norm {
    *
    * @return the {@link Rechtsetzungsdokument}, or an empty set if none exist
    */
-  public Optional<Rechtsetzungsdokument> getRechtsetzungsdokument() {
+  public Rechtsetzungsdokument getRechtsetzungsdokument() {
     return dokumente
       .stream()
       .filter(Rechtsetzungsdokument.class::isInstance)
       .map(Rechtsetzungsdokument.class::cast)
-      .findFirst();
+      .findFirst()
+      // TODO: (Malte Laukötter, 2025-05-27) create proper exception
+      .orElseThrow(() -> new RuntimeException("Missing Rechtsetzungsdokument"));
   }
 
   /**
