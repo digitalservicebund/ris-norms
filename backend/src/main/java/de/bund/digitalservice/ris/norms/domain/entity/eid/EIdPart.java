12a4c019f52ab2c77143150895c151f2556b9101
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-05-27 12:54:25 +0200

    Adjust eId computation for ldml_de 1.8
    RISDEV-7807

--- backend/src/main/java/de/bund/digitalservice/ris/norms/domain/entity/eid/EIdPart.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/domain/entity/eid/EIdPart.java
@@ -6,16 +6,34 @@ package de.bund.digitalservice.ris.norms.domain.entity.eid;
  * @param value the part of the eId as {@link String}
  */
 public record EIdPart(String value) {
-  public EIdPart(String type, String position) {
-    this(type + "-" + position);
+  public EIdPart(String type, EIdPosition position) {
+    this(type + "-" + position.toString());
   }
 
   public String getType() {
     return value.split("-")[0];
   }
 
-  public String getPosition() {
-    return value.split("-")[1];
+  /**
+   * Get the position information
+   * @return the position information
+   */
+  public EIdPosition getPosition() {
+    var position = value.split("-")[1];
+
+    if (position.startsWith(OrdinalEIdPosition.PREFIX)) {
+      return OrdinalEIdPosition.fromString(position);
+    }
+
+    if (position.startsWith(ZaehlbezeichnungsbasierteEIdPosition.PREFIX)) {
+      return ZaehlbezeichnungsbasierteEIdPosition.fromString(position);
+    }
+
+    throw new IllegalStateException(
+      "EId position doesn't start with one of the expected prefixes (either n or z): %s".formatted(
+          position
+        )
+    );
   }
 
   @Override
