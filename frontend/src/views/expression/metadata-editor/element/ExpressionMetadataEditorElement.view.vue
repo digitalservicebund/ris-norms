c8635993ded52ce63bfe27fb74bfa5f01d268b44
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2025-06-12 12:01:55 +0200

    refactor: use SplitterPanel layout and make sections scrollable
    RISDEV-7851

--- frontend/src/views/expression/metadata-editor/element/ExpressionMetadataEditorElement.view.vue
+++ frontend/src/views/expression/metadata-editor/element/ExpressionMetadataEditorElement.view.vue
@@ -107,106 +107,112 @@ watch(hasSaved, (finished) => {
 </script>
 
 <template>
-  <div
-    v-if="elementIsLoading"
-    class="flex h-full items-center justify-center p-24"
-  >
-    <RisLoadingSpinner />
-  </div>
-
-  <div v-else-if="elementError" class="p-24">
-    <RisErrorCallout :error="elementError" />
-  </div>
-
-  <div v-else class="flex flex-col overflow-hidden p-24">
-    <div class="flex gap-16">
-      <div class="grow">
-        <h2 class="ris-label2-bold">
-          {{ element?.title }}
-        </h2>
-      </div>
+  <div class="flex h-full flex-col overflow-hidden p-24">
+    <div
+      v-if="elementIsLoading"
+      class="flex h-full items-center justify-center p-24"
+    >
+      <RisLoadingSpinner />
     </div>
 
-    <div class="gap grid min-h-0 grow grid-cols-2 grid-rows-1 gap-16">
-      <section
-        class="mt-16 flex flex-col gap-8 overflow-hidden"
-        aria-label="Vorschau"
-      >
-        <div v-if="renderIsLoading" class="my-16 flex justify-center">
-          <RisLoadingSpinner />
+    <div v-else-if="elementError" class="p-24">
+      <RisErrorCallout :error="elementError" />
+    </div>
+
+    <div v-else class="flex flex-col overflow-hidden">
+      <div class="flex gap-16">
+        <div class="grow">
+          <h2 class="ris-label2-bold">
+            {{ element?.title }}
+          </h2>
         </div>
+      </div>
 
-        <RisErrorCallout v-else-if="renderError" :error="renderError" />
-
-        <RisLawPreview
-          v-else
-          class="h-full grow overflow-auto p-2"
-          :content="render ?? ''"
-        />
-      </section>
-
-      <section class="flex flex-col gap-8" aria-label="Metadaten dokumentieren">
-        <div v-if="isFetching" class="my-16 flex justify-center">
-          <RisLoadingSpinner />
-        </div>
-
-        <RisErrorCallout v-else-if="fetchError" :error="fetchError" />
-
-        <form
-          v-else
-          class="grid grid-cols-[max-content_1fr] items-center gap-x-16 gap-y-14 overflow-auto"
-          @submit.prevent
+      <div class="gap grid min-h-0 grow grid-cols-2 grid-rows-1 gap-16">
+        <section
+          class="mt-16 flex flex-col gap-8 overflow-hidden"
+          aria-label="Vorschau"
         >
-          <fieldset class="contents">
-            <legend class="ris-label2-bold col-span-2">Dokumenttyp</legend>
-
-            <fieldset class="col-span-2 contents">
-              <legend class="self-start">Art der Norm</legend>
-
-              <div class="space-y-10">
-                <div class="flex items-center">
-                  <RadioButton
-                    v-model="artNorm"
-                    :input-id="artNormSnId"
-                    value="SN"
-                    name="artNorm"
-                  />
-                  <label :for="artNormSnId">SN - Stammnorm</label>
-                </div>
-                <div class="flex items-center">
-                  <RadioButton
-                    v-model="artNorm"
-                    :input-id="artNormAnId"
-                    value="ÄN"
-                    name="artNorm"
-                  />
-                  <label :for="artNormAnId">ÄN - Änderungsnorm</label>
-                </div>
-                <div class="flex items-center">
-                  <RadioButton
-                    v-model="artNorm"
-                    :input-id="artNormUnId"
-                    value="ÜN"
-                    name="artNorm"
-                  />
-                  <label :for="artNormUnId">ÜN - Übergangsnorm</label>
-                </div>
-              </div>
-            </fieldset>
-          </fieldset>
-        </form>
-        <!-- Save button -->
-        <Teleport v-if="actionTeleportTarget" :to="actionTeleportTarget">
-          <div class="relative">
-            <Button
-              :disabled="isFetching || !!fetchError"
-              :loading="isSaving"
-              label="Speichern"
-              @click="save()"
-            />
+          <div v-if="renderIsLoading" class="my-16 flex justify-center">
+            <RisLoadingSpinner />
           </div>
-        </Teleport>
-      </section>
+
+          <RisErrorCallout v-else-if="renderError" :error="renderError" />
+
+          <RisLawPreview
+            v-else
+            class="h-full grow overflow-auto p-2"
+            :content="render ?? ''"
+            :arrow-focus="false"
+          />
+        </section>
+
+        <section
+          class="flex flex-col gap-8"
+          aria-label="Metadaten dokumentieren"
+        >
+          <div v-if="isFetching" class="my-16 flex justify-center">
+            <RisLoadingSpinner />
+          </div>
+
+          <RisErrorCallout v-else-if="fetchError" :error="fetchError" />
+
+          <form
+            v-else
+            class="grid grid-cols-[max-content_1fr] items-center gap-x-16 gap-y-14 overflow-auto"
+            @submit.prevent
+          >
+            <fieldset class="contents">
+              <legend class="ris-label2-bold col-span-2">Dokumenttyp</legend>
+
+              <fieldset class="col-span-2 contents">
+                <legend class="self-start">Art der Norm</legend>
+
+                <div class="space-y-10">
+                  <div class="flex items-center">
+                    <RadioButton
+                      v-model="artNorm"
+                      :input-id="artNormSnId"
+                      value="SN"
+                      name="artNorm"
+                    />
+                    <label :for="artNormSnId">SN - Stammnorm</label>
+                  </div>
+                  <div class="flex items-center">
+                    <RadioButton
+                      v-model="artNorm"
+                      :input-id="artNormAnId"
+                      value="ÄN"
+                      name="artNorm"
+                    />
+                    <label :for="artNormAnId">ÄN - Änderungsnorm</label>
+                  </div>
+                  <div class="flex items-center">
+                    <RadioButton
+                      v-model="artNorm"
+                      :input-id="artNormUnId"
+                      value="ÜN"
+                      name="artNorm"
+                    />
+                    <label :for="artNormUnId">ÜN - Übergangsnorm</label>
+                  </div>
+                </div>
+              </fieldset>
+            </fieldset>
+          </form>
+          <!-- Save button -->
+          <Teleport v-if="actionTeleportTarget" :to="actionTeleportTarget">
+            <div class="relative">
+              <Button
+                :disabled="isFetching || !!fetchError"
+                :loading="isSaving"
+                label="Speichern"
+                @click="save()"
+              />
+            </div>
+          </Teleport>
+        </section>
+      </div>
     </div>
   </div>
 </template>
