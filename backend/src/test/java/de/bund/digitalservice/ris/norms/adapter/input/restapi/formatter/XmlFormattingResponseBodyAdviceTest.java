d7349aa0a09abe628e96afbe3ccc238b961a6f22
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2025-06-30 00:45:37 +0200

    chore(xml-formatting): apply PR review feedback
    - Update XmlFormattingResponseBodyAdviceTest
    - Refactor XmlFormattingIntegrationTest
    - Update XmlFormattingResponseBodyAdvice
    
    RISDEV-8144

--- backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/formatter/XmlFormattingResponseBodyAdviceTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/formatter/XmlFormattingResponseBodyAdviceTest.java
@@ -1,9 +1,14 @@
 package de.bund.digitalservice.ris.norms.adapter.input.restapi.formatter;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 
 class XmlFormattingResponseBodyAdviceTest {
 
@@ -14,13 +19,17 @@ class XmlFormattingResponseBodyAdviceTest {
     String compactXml =
       "<?xml version=\"1.0\" encoding=\"UTF-8\"?><akn:doc><akn:meta><akn:identification><akn:FRBRWork><akn:FRBRthis value=\"test\"/></akn:FRBRWork></akn:identification></akn:meta><akn:body><akn:article eId=\"art-1\"><akn:heading>Test</akn:heading></akn:article></akn:body></akn:doc>";
 
-    var result = xmlFormatter.beforeBodyWrite(
+    Class<? extends HttpMessageConverter<?>> dummyConverterType = StringHttpMessageConverter.class;
+    ServerHttpRequest dummyRequest = mock(ServerHttpRequest.class);
+    ServerHttpResponse dummyResponse = mock(ServerHttpResponse.class);
+
+    Object result = xmlFormatter.beforeBodyWrite(
       compactXml,
       null,
       MediaType.APPLICATION_XML,
-      null,
-      null,
-      null
+      dummyConverterType,
+      dummyRequest,
+      dummyResponse
     );
 
     assertThat((String) result).isEqualToIgnoringWhitespace(
@@ -48,13 +57,17 @@ class XmlFormattingResponseBodyAdviceTest {
   void shouldReturnOriginalWhenXmlIsInvalid() {
     String invalidXml = "<akn:doc><invalid xml"; // malformed
 
-    var result = xmlFormatter.beforeBodyWrite(
+    Class<? extends HttpMessageConverter<?>> dummyConverterType = StringHttpMessageConverter.class;
+    ServerHttpRequest dummyRequest = mock(ServerHttpRequest.class);
+    ServerHttpResponse dummyResponse = mock(ServerHttpResponse.class);
+
+    Object result = xmlFormatter.beforeBodyWrite(
       invalidXml,
       null,
       MediaType.APPLICATION_XML,
-      null,
-      null,
-      null
+      dummyConverterType,
+      dummyRequest,
+      dummyResponse
     );
 
     assertThat(result).isEqualTo(invalidXml);
