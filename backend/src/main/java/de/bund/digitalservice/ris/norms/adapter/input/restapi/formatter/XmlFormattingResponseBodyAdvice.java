d59b8fe7bd3262d1bb0e793bf83b17f43f5d8abb
VictorDelCampo <victor.delcampo@digitalservice.bund.de>
2025-07-01 14:28:20 +0200

    Remove xml-declaration paramete (default true)
    RISDEV-8144

--- backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/formatter/XmlFormattingResponseBodyAdvice.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/formatter/XmlFormattingResponseBodyAdvice.java
@@ -62,7 +62,6 @@ public class XmlFormattingResponseBodyAdvice implements ResponseBodyAdvice<Objec
       LSSerializer serializer = impl.createLSSerializer();
 
       serializer.getDomConfig().setParameter("format-pretty-print", Boolean.TRUE);
-      serializer.getDomConfig().setParameter("xml-declaration", Boolean.TRUE);
 
       LSOutput lsOutput = impl.createLSOutput();
       StringWriter writer = new StringWriter();
