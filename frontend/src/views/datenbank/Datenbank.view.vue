2d0fe114c047a37ee67c94692fb43bc0a8f3cb1d
Tarek Hamaoui <tarek.hamaoui@digitalservice.bund.de>
2025-07-07 23:51:35 +0200

    fix(datenbank): fix flickering by removing debounced loading
    Moves loading state from RisViewLayout to component level
    
    RISDEV-0000

--- frontend/src/views/datenbank/Datenbank.view.vue
+++ frontend/src/views/datenbank/Datenbank.view.vue
@@ -4,6 +4,7 @@ import RisEmptyState from "@/components/RisEmptyState.vue"
 import { computed, ref } from "vue"
 import { useGetNorms } from "@/services/normService"
 import RisDatenbankTable from "./RisDatenbankTable.vue"
+import RisLoadingSpinner from "@/components/RisLoadingSpinner.vue"
 
 const currentPage = ref(0)
 const pageSize = ref(100)
@@ -23,20 +24,27 @@ function onPageChange(page: number) {
 </script>
 
 <template>
-  <RisViewLayout :errors="[normsError]" :loading="normsIsFetching">
-    <header class="mb-24">
-      <h1 class="ris-heading2-regular">Datenbank</h1>
-    </header>
+  <RisViewLayout :errors="[normsError]">
+    <template v-if="normsIsFetching">
+      <div class="flex items-center justify-center p-24">
+        <RisLoadingSpinner />
+      </div>
+    </template>
+    <template v-else>
+      <header class="mb-24">
+        <h1 class="ris-heading2-regular">Datenbank</h1>
+      </header>
 
-    <RisDatenbankTable
-      v-if="norms.length"
-      :items="norms"
-      :total="total"
-      :current-page="currentPage"
-      :page-size="pageSize"
-      @page="onPageChange"
-    />
+      <RisDatenbankTable
+        v-if="norms.length"
+        :items="norms"
+        :total="total"
+        :current-page="currentPage"
+        :page-size="pageSize"
+        @page="onPageChange"
+      />
 
-    <RisEmptyState v-else text-content="Keine Werke gefunden." />
+      <RisEmptyState v-else text-content="Keine Werke gefunden." />
+    </template>
   </RisViewLayout>
 </template>
