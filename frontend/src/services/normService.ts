947c6a5316ab00a4e54d74a0399e0de3ff68d3cd
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-07-07 15:41:22 +0200

    Remove unused methods from normService
    The once from the dokumentService are now used instead
    
    RISDEV-0000

--- frontend/src/services/normService.ts
+++ frontend/src/services/normService.ts
@@ -82,60 +82,6 @@ export function useGetNormHtml(
 }
 
 /**
- * Convenience shorthand for `useNormService` that sets the correct
- * configuration for getting the raw XML of the norm.
- *
- * @param eli ELI of the norm
- * @param options Optional additional filters and queries
- * @param [fetchOptions={}] Optional configuration for fetch behavior
- * @returns Reactive fetch wrapper
- */
-export function useGetNormXml(
-  eli: Parameters<typeof useNormService>["0"],
-  options?: Parameters<typeof useNormService>["1"],
-  fetchOptions?: Parameters<typeof useNormService>["2"],
-): UseFetchReturn<string> & PromiseLike<UseFetchReturn<string>> {
-  return useNormService(eli, options, {
-    refetch: true,
-    ...fetchOptions,
-    beforeFetch(c) {
-      c.options.headers = { ...c.options.headers, Accept: "application/xml" }
-    },
-  }).text()
-}
-
-/**
- * Convenience shorthand for `useNormService` that sets the correct
- * configuration for putting the raw XML of the norm.
- *
- * @param updateData the new xml of the norm
- * @param eli ELI of the norm
- * @param options Optional additional filters and queries
- * @param [fetchOptions={}] Optional configuration for fetch behavior
- * @returns Reactive fetch wrapper
- */
-export function usePutNormXml(
-  updateData: MaybeRefOrGetter<string | null | undefined>,
-  eli: Parameters<typeof useNormService>["0"],
-  options?: Parameters<typeof useNormService>["1"],
-  fetchOptions?: Parameters<typeof useNormService>["2"],
-): UseFetchReturn<string> {
-  return useNormService(eli, options, {
-    immediate: false,
-    ...fetchOptions,
-    beforeFetch(c) {
-      c.options.headers = {
-        ...c.options.headers,
-        "Content-Type": "application/xml",
-        Accept: "application/xml",
-      }
-    },
-  })
-    .text()
-    .put(updateData)
-}
-
-/**
  * Fetches a paginated list of all norm works from the API.
  * Refetches automatically when the page or size parameters change.
  *
