d44cb27114bb8e117c345fa3c5a0ab057922a8bd
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-07-08 15:14:56 +0200

    Remove no longer needed methods from DokumentExpressionController
    RISDEV-0000

--- backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/DokumentExpressionController.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/DokumentExpressionController.java
@@ -2,22 +2,15 @@ package de.bund.digitalservice.ris.norms.adapter.input.restapi.controller;
 
 import static org.springframework.http.MediaType.*;
 
-import de.bund.digitalservice.ris.norms.adapter.input.restapi.mapper.NormResponseMapper;
-import de.bund.digitalservice.ris.norms.adapter.input.restapi.schema.NormResponseSchema;
-import de.bund.digitalservice.ris.norms.application.port.input.LoadNormUseCase;
 import de.bund.digitalservice.ris.norms.application.port.input.LoadRegelungstextXmlUseCase;
-import de.bund.digitalservice.ris.norms.application.port.input.TransformLegalDocMlToHtmlUseCase;
 import de.bund.digitalservice.ris.norms.application.port.input.UpdateRegelungstextXmlUseCase;
 import de.bund.digitalservice.ris.norms.domain.entity.Dokument;
-import de.bund.digitalservice.ris.norms.domain.entity.DokumentType;
 import de.bund.digitalservice.ris.norms.domain.entity.eli.DokumentExpressionEli;
-import de.bund.digitalservice.ris.norms.domain.entity.eli.NormExpressionEli;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -31,37 +24,15 @@ import org.springframework.web.bind.annotation.RestController;
 )
 public class DokumentExpressionController {
 
-  private final LoadNormUseCase loadNormUseCase;
   private final LoadRegelungstextXmlUseCase loadRegelungstextXmlUseCase;
   private final UpdateRegelungstextXmlUseCase updateRegelungstextXmlUseCase;
-  private final TransformLegalDocMlToHtmlUseCase transformLegalDocMlToHtmlUseCase;
 
   public DokumentExpressionController(
-    LoadNormUseCase loadNormUseCase,
     LoadRegelungstextXmlUseCase loadRegelungstextXmlUseCase,
-    UpdateRegelungstextXmlUseCase updateRegelungstextXmlUseCase,
-    TransformLegalDocMlToHtmlUseCase transformLegalDocMlToHtmlUseCase
+    UpdateRegelungstextXmlUseCase updateRegelungstextXmlUseCase
   ) {
-    this.loadNormUseCase = loadNormUseCase;
     this.loadRegelungstextXmlUseCase = loadRegelungstextXmlUseCase;
     this.updateRegelungstextXmlUseCase = updateRegelungstextXmlUseCase;
-    this.transformLegalDocMlToHtmlUseCase = transformLegalDocMlToHtmlUseCase;
-  }
-
-  /**
-   * Retrieves a norm based on its expression ELI.
-   *
-   * @param eli Eli of the request
-   * @return A {@link ResponseEntity} containing the retrieved norm.
-   *     <p>Returns HTTP 200 (OK) and the norm if found.
-   *     <p>Returns HTTP 404 (Not Found) if the norm is not found.
-   * @deprecated use {@link NormExpressionController#getNorm(NormExpressionEli)} instead
-   */
-  @Deprecated(forRemoval = true)
-  @GetMapping(produces = { APPLICATION_JSON_VALUE })
-  public ResponseEntity<NormResponseSchema> getNorm(final NormExpressionEli eli) {
-    var norm = loadNormUseCase.loadNorm(new LoadNormUseCase.EliOptions(eli));
-    return ResponseEntity.ok(NormResponseMapper.fromUseCaseData(norm));
   }
 
   /**
@@ -77,33 +48,6 @@ public class DokumentExpressionController {
     return ResponseEntity.ok(
       loadRegelungstextXmlUseCase.loadRegelungstextXml(new LoadRegelungstextXmlUseCase.Options(eli))
     );
-  }
-
-  /**
-   * Retrieves a regelungstext's html render based on its expression ELI.
-   *
-   * @param eli Eli of the request
-   * @param showMetadata Boolean indicating whether to include metadata in the HTML response.
-   * @return A {@link ResponseEntity} containing the retrieved regelungstext as rendered html.
-   *     <p>Returns HTTP 200 (OK) and the regelungstext as rendered html.
-   *     <p>Returns HTTP 404 (Not Found) if the regelungstext is not found.
-   * @deprecated use {@link NormExpressionController#getNormRender(NormExpressionEli, boolean)} instead
-   */
-  @GetMapping(produces = { TEXT_HTML_VALUE })
-  public ResponseEntity<String> getNormRender(
-    final NormExpressionEli eli,
-    @RequestParam(defaultValue = "false") boolean showMetadata
-  ) {
-    var normXml = loadRegelungstextXmlUseCase.loadRegelungstextXml(
-      new LoadRegelungstextXmlUseCase.Options(
-        DokumentExpressionEli.fromNormEli(eli, DokumentType.REGELUNGSTEXT_VERKUENDUNG, 1)
-      )
-    );
-    var legalDocHtml = this.transformLegalDocMlToHtmlUseCase.transformLegalDocMlToHtml(
-      new TransformLegalDocMlToHtmlUseCase.Options(normXml, showMetadata, false)
-    );
-
-    return ResponseEntity.ok(legalDocHtml);
   }
 
   /**
--- backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/DokumentExpressionControllerTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/norms/adapter/input/restapi/controller/DokumentExpressionControllerTest.java
@@ -9,14 +9,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import de.bund.digitalservice.ris.norms.application.exception.InvalidUpdateException;
-import de.bund.digitalservice.ris.norms.application.exception.NormNotFoundException;
 import de.bund.digitalservice.ris.norms.application.port.input.LoadNormUseCase;
 import de.bund.digitalservice.ris.norms.application.port.input.LoadRegelungstextUseCase;
 import de.bund.digitalservice.ris.norms.application.port.input.LoadRegelungstextXmlUseCase;
 import de.bund.digitalservice.ris.norms.application.port.input.TransformLegalDocMlToHtmlUseCase;
 import de.bund.digitalservice.ris.norms.application.port.input.UpdateRegelungstextXmlUseCase;
-import de.bund.digitalservice.ris.norms.domain.entity.Fixtures;
-import de.bund.digitalservice.ris.norms.domain.entity.eli.NormExpressionEli;
 import de.bund.digitalservice.ris.norms.utils.exceptions.MandatoryNodeNotFoundException;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -47,82 +44,6 @@ class DokumentExpressionControllerTest {
   private TransformLegalDocMlToHtmlUseCase transformLegalDocMlToHtmlUseCase;
 
   @Nested
-  class getNorms {
-
-    @Test
-    void itReturnsNorm() throws Exception {
-      // Given
-      var norm = Fixtures.loadNormFromDisk("eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/1964-08-05");
-
-      // When
-      when(loadNormUseCase.loadNorm(any())).thenReturn(norm);
-
-      // When // Then
-      mockMvc
-        .perform(
-          get(
-            "/api/v1/norms/eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/regelungstext-verkuendung-1"
-          ).accept(MediaType.APPLICATION_JSON)
-        )
-        .andExpect(status().isOk())
-        .andExpect(
-          jsonPath("eli").value(
-            equalTo("eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/regelungstext-verkuendung-1")
-          )
-        )
-        .andExpect(
-          jsonPath("title").value(equalTo("Gesetz zur Regelung des öffentlichen Vereinsrechts"))
-        )
-        .andExpect(jsonPath("frbrNumber").value(equalTo("s593")))
-        .andExpect(jsonPath("frbrName").value(equalTo("BGBl. I")))
-        .andExpect(jsonPath("frbrDateVerkuendung").value(equalTo("1964-08-05")));
-
-      verify(loadNormUseCase, times(1)).loadNorm(
-        new LoadNormUseCase.EliOptions(
-          NormExpressionEli.fromString("eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu")
-        )
-      );
-    }
-
-    @Test
-    void itReturnsErrorResponse() throws Exception {
-      // Given
-
-      // When
-      when(loadNormUseCase.loadNorm(any())).thenThrow(
-        new NormNotFoundException(
-          NormExpressionEli.fromString("eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu")
-        )
-      );
-
-      // When // Then
-      mockMvc
-        .perform(
-          get(
-            "/api/v1/norms/eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/regelungstext-verkuendung-1"
-          ).accept(MediaType.APPLICATION_JSON)
-        )
-        .andExpect(status().isNotFound())
-        .andExpect(jsonPath("type").value(equalTo("/errors/norm-not-found")))
-        .andExpect(jsonPath("title").value(equalTo("Norm not found")))
-        .andExpect(jsonPath("status").value(equalTo(404)))
-        .andExpect(
-          jsonPath("detail").value(
-            equalTo("Norm with eli eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu does not exist")
-          )
-        )
-        .andExpect(
-          jsonPath("instance").value(
-            equalTo(
-              "/api/v1/norms/eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu/regelungstext-verkuendung-1"
-            )
-          )
-        )
-        .andExpect(jsonPath("eli").value(equalTo("eli/bund/bgbl-1/1964/s593/1964-08-05/1/deu")));
-    }
-  }
-
-  @Nested
   class getNormXml {
 
     @Test
@@ -139,54 +60,6 @@ class DokumentExpressionControllerTest {
         .perform(get("/api/v1/norms/{eli}", eli).accept(MediaType.APPLICATION_XML))
         .andExpect(status().isOk())
         .andExpect(content().string(xml));
-    }
-  }
-
-  @Nested
-  class getNormRender {
-
-    @Test
-    void itCallsNormServiceAndReturnsNormRender() throws Exception {
-      // Given
-      final String eli = "eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-verkuendung-1";
-      final String xml = "<akn:doc></akn:doc>";
-      final String html = "<div></div>";
-
-      when(loadRegelungstextXmlUseCase.loadRegelungstextXml(any())).thenReturn(xml);
-      when(transformLegalDocMlToHtmlUseCase.transformLegalDocMlToHtml(any())).thenReturn(html);
-
-      // When // Then
-      mockMvc
-        .perform(get("/api/v1/norms/{eli}", eli).accept(MediaType.TEXT_HTML))
-        .andExpect(status().isOk())
-        .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-        .andExpect(content().string(html));
-
-      verify(transformLegalDocMlToHtmlUseCase, times(1)).transformLegalDocMlToHtml(
-        argThat(query -> query.xml().equals(xml) && !query.showMetadata())
-      );
-    }
-
-    @Test
-    void itCallsNormServiceAndReturnsNormRenderWithMetadata() throws Exception {
-      // Given
-      final String eli = "eli/bund/bgbl-1/1990/s2954/2022-12-19/1/deu/regelungstext-verkuendung-1";
-      final String xml = "<akn:doc></akn:doc>";
-      final String html = "<div></div>";
-
-      when(loadRegelungstextXmlUseCase.loadRegelungstextXml(any())).thenReturn(xml);
-      when(transformLegalDocMlToHtmlUseCase.transformLegalDocMlToHtml(any())).thenReturn(html);
-
-      // When // Then
-      mockMvc
-        .perform(get("/api/v1/norms/{eli}?showMetadata=true", eli).accept(MediaType.TEXT_HTML))
-        .andExpect(status().isOk())
-        .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-        .andExpect(content().string(html));
-
-      verify(transformLegalDocMlToHtmlUseCase, times(1)).transformLegalDocMlToHtml(
-        argThat(query -> query.xml().equals(xml) && query.showMetadata())
-      );
     }
   }
 
