c4dcd4872e109ffbc77ae0c0009d630d022d8203
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-07-10 12:34:53 +0200

    Extract components from metadata editor views
    RISDEV-8470

---
+++ frontend/src/components/metadata-editor/outline-element/RisMetadataEditorOutlineElement.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+import RisLawPreview from "@/components/RisLawPreview.vue"
+import RisLoadingSpinner from "@/components/RisLoadingSpinner.vue"
+import RisErrorCallout from "@/components/RisErrorCallout.vue"
+import { useGetElement, useGetElementHtml } from "@/services/elementService"
+import Message from "primevue/message"
+import type { DokumentExpressionEli } from "@/lib/eli/DokumentExpressionEli"
+
+const props = defineProps<{
+  dokumentExpressionEli: DokumentExpressionEli
+  eId: string
+}>()
+
+const {
+  data: element,
+  isFetching: elementIsLoading,
+  error: elementError,
+} = useGetElement(
+  () => props.dokumentExpressionEli,
+  () => props.eId,
+)
+
+const {
+  data: render,
+  isFetching: renderIsLoading,
+  error: renderError,
+} = useGetElementHtml(
+  () => props.dokumentExpressionEli,
+  () => props.eId,
+)
+</script>
+
+<template>
+  <div class="flex h-full flex-col overflow-hidden p-24">
+    <div
+      v-if="elementIsLoading"
+      class="flex h-full items-center justify-center p-24"
+    >
+      <RisLoadingSpinner />
+    </div>
+
+    <div v-else-if="elementError" class="p-24">
+      <RisErrorCallout :error="elementError" />
+    </div>
+
+    <div v-else class="flex flex-col overflow-hidden p-24">
+      <div class="flex gap-16">
+        <div class="grow">
+          <h2 class="ris-label2-bold">
+            {{ element?.title }}
+          </h2>
+        </div>
+      </div>
+
+      <div class="gap grid min-h-0 grow grid-cols-2 grid-rows-1 gap-16">
+        <section
+          class="mt-16 flex flex-col gap-8 overflow-hidden"
+          aria-label="Vorschau"
+        >
+          <div v-if="renderIsLoading" class="my-16 flex justify-center">
+            <RisLoadingSpinner />
+          </div>
+
+          <RisErrorCallout v-else-if="renderError" :error="renderError" />
+
+          <RisLawPreview
+            v-else
+            class="h-full grow overflow-auto p-2"
+            :content="render ?? ''"
+            :arrow-focus="false"
+          />
+        </section>
+
+        <section
+          class="mt-32 flex flex-col gap-8"
+          aria-label="Metadaten dokumentieren"
+        >
+          <Message severity="warn">
+            Aktuell sind keine Metadaten auf Gliederungsebene implementiert.
+          </Message>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
--- frontend/src/views/expression/metadata-editor/outline-element/ExpressionMetadataEditorOutlineElement.view.vue
+++ frontend/src/views/expression/metadata-editor/outline-element/ExpressionMetadataEditorOutlineElement.view.vue
@@ -1,78 +1,16 @@
 <script setup lang="ts">
-import RisLawPreview from "@/components/RisLawPreview.vue"
-import RisLoadingSpinner from "@/components/RisLoadingSpinner.vue"
-import RisErrorCallout from "@/components/RisErrorCallout.vue"
 import { useEidPathParameter } from "@/composables/useEidPathParameter"
 import { useDokumentExpressionEliPathParameter } from "@/composables/useDokumentExpressionEliPathParameter"
-import { useGetElement, useGetElementHtml } from "@/services/elementService"
-import Message from "primevue/message"
+import RisMetadataEditorOutlineElement from "@/components/metadata-editor/outline-element/RisMetadataEditorOutlineElement.vue"
 
 const dokumentExpressionEli = useDokumentExpressionEliPathParameter()
 const elementEid = useEidPathParameter()
-
-const {
-  data: element,
-  isFetching: elementIsLoading,
-  error: elementError,
-} = useGetElement(dokumentExpressionEli, elementEid)
-
-const {
-  data: render,
-  isFetching: renderIsLoading,
-  error: renderError,
-} = useGetElementHtml(dokumentExpressionEli, elementEid)
 </script>
 
 <template>
-  <div class="flex h-full flex-col overflow-hidden p-24">
-    <div
-      v-if="elementIsLoading"
-      class="flex h-full items-center justify-center p-24"
-    >
-      <RisLoadingSpinner />
-    </div>
-
-    <div v-else-if="elementError" class="p-24">
-      <RisErrorCallout :error="elementError" />
-    </div>
-
-    <div v-else class="flex flex-col overflow-hidden p-24">
-      <div class="flex gap-16">
-        <div class="grow">
-          <h2 class="ris-label2-bold">
-            {{ element?.title }}
-          </h2>
-        </div>
-      </div>
-
-      <div class="gap grid min-h-0 grow grid-cols-2 grid-rows-1 gap-16">
-        <section
-          class="mt-16 flex flex-col gap-8 overflow-hidden"
-          aria-label="Vorschau"
-        >
-          <div v-if="renderIsLoading" class="my-16 flex justify-center">
-            <RisLoadingSpinner />
-          </div>
-
-          <RisErrorCallout v-else-if="renderError" :error="renderError" />
-
-          <RisLawPreview
-            v-else
-            class="h-full grow overflow-auto p-2"
-            :content="render ?? ''"
-            :arrow-focus="false"
-          />
-        </section>
-
-        <section
-          class="mt-32 flex flex-col gap-8"
-          aria-label="Metadaten dokumentieren"
-        >
-          <Message severity="warn">
-            Aktuell sind keine Metadaten auf Gliederungsebene implementiert.
-          </Message>
-        </section>
-      </div>
-    </div>
-  </div>
+  <RisMetadataEditorOutlineElement
+    v-if="elementEid"
+    :dokument-expression-eli="dokumentExpressionEli"
+    :e-id="elementEid"
+  />
 </template>
