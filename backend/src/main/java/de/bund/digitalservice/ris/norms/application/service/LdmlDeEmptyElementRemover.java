976c6ec4e9d79236ed63dc8d62137f5dd22a35ed
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-07-17 15:54:10 +0200

    Simplify list of elements to remove if empty
    RISDEV-0000

--- backend/src/main/java/de/bund/digitalservice/ris/norms/application/service/LdmlDeEmptyElementRemover.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/application/service/LdmlDeEmptyElementRemover.java
@@ -6,10 +6,8 @@ import de.bund.digitalservice.ris.norms.domain.entity.Geltungszeiten;
 import de.bund.digitalservice.ris.norms.domain.entity.Namespace;
 import de.bund.digitalservice.ris.norms.domain.entity.ZielnormReferences;
 import de.bund.digitalservice.ris.norms.utils.NodeParser;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -21,38 +19,29 @@ import org.w3c.dom.Node;
 @Service
 public class LdmlDeEmptyElementRemover {
 
-  private static final Map<Namespace, Set<String>> ELEMENTS_TO_REMOVE_IF_EMPTY = Set.of(
-    new ElementType(Namespace.INHALTSDATEN, "proprietary"),
-    new ElementType(Namespace.INHALTSDATEN, "analysis"),
-    new ElementType(Namespace.INHALTSDATEN, "activeModifications"),
-    new ElementType(Namespace.INHALTSDATEN, "passiveModifications"),
-    new ElementType(Namespace.INHALTSDATEN, "notes"),
-    new ElementType(Namespace.INHALTSDATEN, "lifecycle"),
-    new ElementType(Namespace.INHALTSDATEN, "components"),
-    new ElementType(Namespace.METADATEN_BUNDESREGIERUNG, "legalDocML.de_metadaten"),
-    new ElementType(Namespace.METADATEN_BUNDESREGIERUNG, "federfuehrung"),
-    new ElementType(Namespace.METADATEN_RIS, "legalDocML.de_metadaten"),
-    new ElementType(Namespace.METADATEN_RIS, "einzelelement"),
-    new ElementType(CustomModsMetadata.NAMESPACE, CustomModsMetadata.TAG_NAME),
-    new ElementType(ZielnormReferences.NAMESPACE, ZielnormReferences.TAG_NAME),
-    new ElementType(Geltungszeiten.NAMESPACE, Geltungszeiten.TAG_NAME),
-    new ElementType(AmendedNormExpressions.NAMESPACE, AmendedNormExpressions.TAG_NAME)
-  )
-    .stream()
-    .collect(
-      Collectors.toMap(
-        ElementType::namespace,
-        a -> {
-          var set = new HashSet<String>();
-          set.add(a.tagName);
-          return set;
-        },
-        (a, b) -> {
-          a.addAll(b);
-          return a;
-        }
-      )
-    );
+  private static final Map<Namespace, Set<String>> ELEMENTS_TO_REMOVE_IF_EMPTY = Map.of(
+    Namespace.INHALTSDATEN,
+    Set.of(
+      "proprietary",
+      "analysis",
+      "activeModifications",
+      "passiveModifications",
+      "notes",
+      "lifecycle",
+      "components"
+    ),
+    Namespace.METADATEN_BUNDESREGIERUNG,
+    Set.of("legalDocML.de_metadaten", "federfuehrung"),
+    Namespace.METADATEN_RIS,
+    Set.of("legalDocML.de_metadaten", "einzelelement"),
+    Namespace.METADATEN_NORMS_APPLICATION_MODS,
+    Set.of(
+      CustomModsMetadata.TAG_NAME,
+      ZielnormReferences.TAG_NAME,
+      Geltungszeiten.TAG_NAME,
+      AmendedNormExpressions.TAG_NAME
+    )
+  );
 
   /**
    * Removes empty elements.
@@ -88,6 +77,4 @@ public class LdmlDeEmptyElementRemover {
 
     return false;
   }
-
-  record ElementType(Namespace namespace, String tagName) {}
 }
