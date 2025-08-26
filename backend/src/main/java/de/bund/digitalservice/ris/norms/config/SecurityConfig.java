c4f1ce969f654d75fb29ce093ad626bbd5c03c60
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2025-08-26 13:22:17 +0200

    Add csp reporting
    RISDEV-8974

--- backend/src/main/java/de/bund/digitalservice/ris/norms/config/SecurityConfig.java
+++ backend/src/main/java/de/bund/digitalservice/ris/norms/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package de.bund.digitalservice.ris.norms.config;
 import de.bund.digitalservice.ris.norms.domain.entity.Roles;
 import java.util.*;
 import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
@@ -20,6 +21,7 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
 import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 
 /**
@@ -30,6 +32,9 @@ import org.springframework.security.web.servlet.util.matcher.PathPatternRequestM
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+  @Value("${csp.report-uri}")
+  private String cspReportUri;
 
   /**
    * Configures security settings for specific HTTP requests.
@@ -78,7 +83,30 @@ public class SecurityConfig {
         httpSecurityHeadersConfigurer
           .contentSecurityPolicy(contentSecurityPolicyConfig ->
             contentSecurityPolicyConfig.policyDirectives(
-              "default-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-eval'; connect-src 'self' *.sentry.io neuris.login.bare.id data:"
+              """
+              default-src 'self';
+              img-src 'self' data:;
+              style-src 'self' 'unsafe-inline';
+              script-src 'self' 'unsafe-eval';
+              connect-src 'self' *.sentry.io neuris.login.bare.id data:;
+              report-uri %s;
+              report-to csp-endpoint
+              """.formatted(cspReportUri)
+                .replaceAll("[\\n\\r]+", " ")
+            )
+          )
+          .addHeaderWriter(
+            new StaticHeadersWriter(
+              "Report-To",
+              "{\"group\":\"csp-endpoint\",\"max_age\":10886400,\"endpoints\":[{\"url\":\"%s\"}],\"include_subdomains\":true}".formatted(
+                cspReportUri
+              )
+            )
+          )
+          .addHeaderWriter(
+            new StaticHeadersWriter(
+              "Reporting-Endpoints",
+              "csp-endpoint=\"%s\"".formatted(cspReportUri)
             )
           )
           .contentTypeOptions(contentTypeOptionsConfig -> {})
